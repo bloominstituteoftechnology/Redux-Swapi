8ac5d0a757f4005ba95afea911eb3724f87068bb
Sean Chen <skypemaster007@gmail.com>
2018-02-21 15:08:01 -0800

    Fix inconsistent action types specified in instructions

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -6,7 +6,7 @@ const initialState = {
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
     // Fill me in with the important reducers
-    // action types should be FETCHING_CHARS, CHARS_RECEIVED, ERROR_FETCHING_CHARS
+    // action types should be FETCHING, FETCHED, and ERROR
     // your switch statement should handle all of these cases.
     default:
       return state;
