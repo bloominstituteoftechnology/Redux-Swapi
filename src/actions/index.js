81cb0af66a4a3eb500243e14247f1cfd7a2d2fec
Charletta <charlettabullard12@outlook.com>
2018-03-22 19:04:05 -0400

    Completed most of this project/couldn't see chars on monitor

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,32 @@
 // we'll need axios
-
+import axios from 'axios';
 // we'll need to create 3 different action types here.
 // one for fetching, one for fetched and one for errors
 
 // our action creator will be a function that returns a promise
 // we'll have to be sure to make our promise resolve within our new "thunk based middlware"
 // the url to fetch charicters from is `https://swapi.co/api/people`
-// remember that now we have controll over our thunk-based
+// remember that now we have controll over our thunk-based (thunkified)
+
+// function getData() {
+//   return function (dispatch) { 
+
+//   }
+// }
+
+export const FETCHING = 'FETCHING'
+export const FETCHED = 'FETCHED'
+export const ERROR = 'ERROR'
+
+export const getChars = () => (dispatch) => {
+
+  dispatch({ type: FETCHING }); 
+
+  axios.get('https://swapi.co/api/people')
+  .then(response => {
+    dispatch ({ type: 'FETCHED', chars: response.data.results });
+  })
+  .catch(err => {
+    dispatch ({ type: 'ERROR', errorMessage: 'Error fetching the data' });
+  });
+};
