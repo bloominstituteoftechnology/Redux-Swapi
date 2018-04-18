8690eaf0e1b7140a239955f8155a97b96e6449b5
Natalie Schennum <natalieschennum@gmail.com>
2018-04-18 14:30:51 -0700

    installed dependencies, index root, app w CDM & map; actions(3); reduce(initial state Pbj & swtich statement with initial state and actions

--- src/components/App.js
+++ src/components/App.js
@@ -3,31 +3,38 @@ import { connect } from 'react-redux';
 
 import logo from '../logo.svg';
 import '../styles/App.css';
+import { getChars } from '../actions';
 // pull in actions from action/index
 
 class App extends Component {
   componentDidMount() {
-    // call our action
+    this.props.getChars();// call our action
   }
   render() {
     return (
       <div className="App">
+      <h1> Star Wars Characters </h1>
         {this.props.fetching ? (
           <img src={logo} className="App-logo" alt="logo" />
         ) : (
-          <ul>
+          <div>
             {this.props.chars.map(char => {
               return <li key={char.name}>{char.name}</li>;
             })}
-          </ul>
+          </div>
         )}
       </div>
     );
   }
 }
 
-// our mapDispatchToProps needs to have two properties inherited from state
+const mapStateToProps = state => {
+  return { 
+    chars: state.chars, 
+    fetching: state.fetching //boolean?
+  };
+};// our mapDispatchToProps needs to have two properties inherited from state
 // the chars and the fetching boolean
-export default connect(null, {
+export default connect(mapStateToProps, {getChars})(App);
   /* actions go here */
-})(App);
+
