5582b8ab567373dd2dbdbf5a612d7401b8acc71b
Harrison Brock <harrison@harrisonbrock.com>
2018-04-18 16:23:11 -0400

    Import actions MapDispatchToProps

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,15 @@ import { connect } from 'react-redux';
 
 import logo from '../logo.svg';
 import '../styles/App.css';
+import {getChars} from "../actions";
+
+'../actions/index'
 // pull in actions from action/index
 
 class App extends Component {
   componentDidMount() {
     // call our action
+      this.props.getChars();
   }
   render() {
     return (
@@ -28,6 +32,10 @@ class App extends Component {
 
 // our mapDispatchToProps needs to have two properties inherited from state
 // the chars and the fetching boolean
-export default connect(null, {
-  /* actions go here */
-})(App);
+const mapDispatchToProps = state => {
+  return {
+    chars: state.chars,
+    fetching: state.fetching
+  };
+};
+export default connect(mapDispatchToProps(), {getChars})(App);
