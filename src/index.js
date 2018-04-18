9b8254d8d72de0ec2224ccc6bafa4604472d3894
totsuya <supertatortots@gmail.com>
2018-04-18 17:56:29 -0400

    added devtools extention

--- src/index.js
+++ src/index.js
@@ -15,7 +15,8 @@ import reducers from './reducers';
 // logger from redux-logger
 // rootReducer from ./reducers
 
-const store = createStore(reducers, applyMiddleware(thunk, logger));
+// remove redux dev tools extention tools for final pr
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <Provider store={store}>
