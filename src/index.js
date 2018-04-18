589994bc114abf31463414b7b35ea5fd9a10314b
David Alexander Morales <damorales89@gmail.com>
2018-04-18 18:09:21 -0400

    index.js redux & thunk imports

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import { App } from './components';
+import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import thunk from 'redux-thunk';
+
+import './index.css';
+import { App } from './components';
+import { starWarsReducer } from './reducers/starWarsReducer';
+
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
 // logger from redux-logger
 // rootReducer from ./reducers
 
-const store = createStore(/* rootReducer */, /* applyMiddleware goes here */);
+const store = createStore(starWarsReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
