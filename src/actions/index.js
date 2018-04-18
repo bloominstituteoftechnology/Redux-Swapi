2845ea2bb035847420ed6052936a28e03370c527
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-04-18 13:11:38 -0700

    fixing errors

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,21 +9,13 @@ export const ERROR = 'ERROR';
 // we'll have to be sure to make our promise resolve within our new "thunk based middlware"
 // the url to fetch characters from is `https://swapi.co/api/people`
 // remember that now we have controll over our thunk-based
-function getChars() {
-    return function action(dispatch) {
-        dispatch({ type: FETCHING })
-
-        const request = axios({
-          method: "GET",
-          url: "https://swapi.co/api/people",
-          headers: []
-        });
-
-        return request.then(
-            response => dispatch({ type: FETCHED, chars: response.data.results }),
-            err => dispatch({ type: ERROR, errorMessage: 'Oops!' })
-        );
+export const getChars = () => {
+    const chars = axios.get("https://swapi.co/api/people/");
+    return dispatch => {
+        dispatch({ type: 'FETCHING'});
+        chars
+            .then(({ data }) => {
+                dispatch({ type: 'FETCHED', payload: data.results })
+            });
     }
 }
-
-export default getChars;
