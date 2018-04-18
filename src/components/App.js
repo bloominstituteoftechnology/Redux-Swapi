c4159d540171e588a1d868e6ffaa3e040d7bfc8f
Mark Wong <fmarkwong@gmail.com>
2018-04-18 15:59:05 -0700

    set up getChar action and api call

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,12 @@ import { connect } from 'react-redux';
 import logo from '../logo.svg';
 import '../styles/App.css';
 // pull in actions from action/index
+import { getChars } from '../actions/'; 
 
 class App extends Component {
   componentDidMount() {
     // call our action
+    this.props.getChars();
   }
   render() {
     return (
@@ -16,9 +18,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
         ) : (
           <ul>
-            {this.props.chars.map(char => {
-              return <li key={char.name}>{char.name}</li>;
-            })}
+            {/* {this.props.chars.map(char => { */}
+            {/*   return <li key={char.name}>{char.name}</li>; */}
+            {/* })} */}
           </ul>
         )}
       </div>
@@ -29,5 +31,5 @@ class App extends Component {
 // our mapDispatchToProps needs to have two properties inherited from state
 // the chars and the fetching boolean
 export default connect(null, {
-  /* actions go here */
+  getChars
 })(App);
