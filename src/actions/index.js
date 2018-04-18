3d6d1d288cdf1e44c471050e2b4b3934e53fa07f
Jared Cuffe <jcuffe@gmail.com>
2018-04-18 10:24:28 -0700

    Created asynchronous action for fetching data

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-// we'll need axios
+import axios from 'axios'
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for fetched and one for errors
@@ -7,3 +7,16 @@
 // we'll have to be sure to make our promise resolve within our new "thunk based middlware"
 // the url to fetch charicters from is `https://swapi.co/api/people`
 // remember that now we have controll over our thunk-based
+export const FETCH_STARTED = 'FETCH_STARTED'
+export const FETCH_SUCCESS = 'FETCH_SUCCESS'
+export const FETCH_ERROR = 'FETCH_ERROR'
+
+export default () => {
+  const request = axios.get('https://swapi.co/api/people')
+  return (dispatch) => {
+    dispatch({ type: FETCH_STARTED })
+    request
+      .then(response => dispatch({ type: FETCH_SUCCESS, payload: response.data.results }))
+      .catch(error => dispatch({ type: FETCH_ERROR, payload: error }))
+  }
+}
