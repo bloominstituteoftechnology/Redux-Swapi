0113afaa924623142fb0881e3b2853bd8d498a37
Justin Hong <jhong2009@gmail.com>
2018-04-18 15:20:03 -0700

    in index.js: imported roote reducer

--- src/index.js
+++ src/index.js
@@ -7,13 +7,9 @@ import logger from "redux-logger";
 
 import "./index.css";
 import { App } from "./components";
-import charsReducer from "./reducers";
+import rootReducer from "./reducers";
 
-const store = createStore(
-	charsReducer,
-	applyMiddleware(thunk, logger),
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
 	<Provider store={store}>
