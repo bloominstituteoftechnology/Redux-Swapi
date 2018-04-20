502079059b45bb06e032d92771eaec4d699232c2
markstez05 <36493764+markstez05@users.noreply.github.com>
2018-04-20 11:40:26 -0500

    not quite working yet

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import { get } from '../actions/index';
 import logo from '../logo.svg';
 import '../styles/App.css';
 // pull in actions from action/index
@@ -8,6 +8,7 @@ import '../styles/App.css';
 class App extends Component {
   componentDidMount() {
     // call our action
+    this.props.get();
   }
   render() {
     return (
@@ -16,8 +17,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
         ) : (
           <ul>
-            {this.props.chars.map(char => {
-              return <li key={char.name}>{char.name}</li>;
+            {this.props.starChars.map(starChars => {
+              return <li key={starChars.name}>{starChars.name}</li>;
             })}
           </ul>
         )}
@@ -25,9 +26,12 @@ class App extends Component {
     );
   }
 }
-
+const mapStateToProps = state => {
+  return {
+    starChars: state.starChars,
+    fetching: state.fetching
+  };
+};
 // our mapDispatchToProps needs to have two properties inherited from state
 // the chars and the fetching boolean
-export default connect(null, {
-  /* actions go here */
-})(App);
+export default connect(mapStateToProps, { get })(App);
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,13 +1,28 @@
-import /* we need our action types here*/ '../actions';
+import { GET, FETCHING, ERROR_FETCHING } from '../actions';
 const initialState = {
   // define a few properties here.
   // Array chars, Boolean fetching, Boolean fetched, null error.
+  fetching: false,
+  fetched: false,
+  starChars: [],
+  error: null
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
     // Fill me in with the important reducers
     // action types should be FETCHING, FETCHED, and ERROR
     // your switch statement should handle all of these cases.
+    case FETCHING:
+      return {...state, fetching: true };
+      case ERROR_FETCHING:
+      return {...state, fetching: false, error: action.payload };
+      case GET:
+      return {
+        ...state,
+        chars: action.payload,
+        fetching: false,
+        fetched: true
+      };
     default:
       return state;
   }
