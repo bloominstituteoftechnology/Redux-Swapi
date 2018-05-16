8de2e276f6b17db657c560df9c10ac5fff38cf5f
jaklundun <govi.escudero@gmail.com>
2018-05-16 13:48:49 -0700

    Added the appropriate packages in `src/index.js`

--- src/index.js
+++ src/index.js
@@ -6,9 +6,13 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 // needed dependancies
 // applyMiddleware from redux
+import { applyMiddleware, createStore } from 'redux';
 // thunk from redux-thunk
+import thunk from 'redux-thunk';
 // logger from redux-logger
+import logger from 'redux-logger'
 // rootReducer from ./reducers
+import combineReducers from './reducers'
 
 const store = createStore(/* rootReducer */, /* applyMiddleware goes here */);
 
