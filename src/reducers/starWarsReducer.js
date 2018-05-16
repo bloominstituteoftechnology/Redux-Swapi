d15c7ec5479c74fa3dac617f5b483a3631a18e4e
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-05-16 12:47:37 -0700

    dependencies installed

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,7 +1,6 @@
-import /* we need our action types here*/ '../actions';
+import {FETCHING, FETCHED, ERROR} '../actions';
 const initialState = {
-  // define a few properties here.
-  // Array chars, Boolean fetching, Boolean fetched, null error.
+  
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
