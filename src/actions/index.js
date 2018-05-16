80732a2f0195c6cf548637ef481b7cf6f696920f
masontmorrow <masontmorrow@gmail.com>
2018-05-17 00:43:59 +0200

    Initial Commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,21 @@
-// we'll need axios
+import axios from "axios";
+import thunk from "redux-thunk";
 
-// we'll need to create 3 different action types here.
-// one for fetching, one for fetched and one for errors
 
-// our action creator will be a function that returns a promise
-// we'll have to be sure to make our promise resolve within our new "thunk based middlware"
-// the url to fetch charicters from is `https://swapi.co/api/people/`
-// remember that now we have controll over our thunk-based
+export const FETCHING = 'FETCHING';
+export const FETCHED = 'FETCHED';
+export const ERROR = 'ERROR';
+
+export const fetching = () => {
+    const fetching = axios.get(`https://swapi.co/api/people/`)
+    return function(dispatch) {
+        dispatch({type: FETCHING});
+        fetching
+            .then(characterData =>{
+                dispatch({type: FETCHED, payload: characterData.data})
+            })
+            .catch(err => {
+                dispatch({type: ERROR});
+            })
+    }
+}
