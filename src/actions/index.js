ab454809ac53ec68fc5eeb3e8ce8c9f6b32edafc
Brandon Benefield <bsquared18@gmail.com>
2018-05-16 16:21:20 -0400

    feat(actions/index): uses `axios` and `redux-thunk` to request data from API

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,3 +7,29 @@
 // we'll have to be sure to make our promise resolve within our new "thunk based middlware"
 // the url to fetch charicters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based
+import axios from 'axios';
+
+// action types
+// export const FETCHING = 'FETCHING';
+export const FETCHED = 'FETCHED';
+export const ERROR = 'ERROR';
+
+/************************
+** ACTION DESCRIPTIONS **
+************************/
+// fetching
+export const fetched = () => {
+  const getCharacters = axios.get('https://swapi.co/api/people');
+  
+  return dispatch => {
+    getCharacters
+      .then(data => {
+        const { results } = data.data;
+        return dispatch({ type: FETCHED, payload: results })
+      })
+      .catch(err => {
+        console.log('ERR', err);
+        return dispatch({ type: ERROR, payload: err });
+      });
+  }
+}
