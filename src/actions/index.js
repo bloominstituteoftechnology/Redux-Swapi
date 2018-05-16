d13cfdfe970e1b0c2987271a03d159242f021268
jesuarva <jean.ariza@jesuarva.com>
2018-05-16 23:15:34 +0200

    END all project

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,32 @@
 // we'll need axios
-
+import axios from 'axios'
 // we'll need to create 3 different action types here.
 // one for fetching, one for fetched and one for errors
+export const FETCHING = "FETCHING";
+export const FETCHED = "FETCHED";
+export const ERRORS = "ERRORS";
 
 // our action creator will be a function that returns a promise
 // we'll have to be sure to make our promise resolve within our new "thunk based middlware"
 // the url to fetch charicters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based
+
+export const getChars = () => {
+    return function (dispatch) {
+          axios.get('https://swapi.co/api/people').
+          then( response => {
+              console.log(response);
+              dispatch({
+                  type: FETCHED,
+                  chars: response.data.results
+              })
+          }).
+          catch( e => {
+              console.log(e.name);
+              dispatch({
+                  type: ERRORS,
+                  error: e.name
+              })
+          });
+    }
+} 
--- src/index.js
+++ src/index.js
@@ -3,18 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './components';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 // needed dependancies
 // applyMiddleware from redux
+import { createStore, applyMiddleware} from 'redux';
 // thunk from redux-thunk
+import think from 'redux-thunk';
 // logger from redux-logger
+import logger from 'redux-logger';
 // rootReducer from ./reducers
+import combineReducers from './reducers';
 
-const store = createStore(/* rootReducer */, /* applyMiddleware goes here */);
-
+const store = createStore(combineReducers, applyMiddleware(think, logger));
+console.log("combineReducers",combineReducers)
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
 );
+ 
