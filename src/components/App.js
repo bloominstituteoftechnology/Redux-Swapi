5ad87ae3ad0ece8a67b839e89adc60248bf13717
AmyShackles <amyshackles@gmail.com>
2018-05-16 13:52:12 -0700

    Everything works.

--- src/components/App.js
+++ src/components/App.js
@@ -4,19 +4,22 @@ import { connect } from 'react-redux';
 import logo from '../logo.svg';
 import '../styles/App.css';
 // pull in actions from action/index
+import { fetchCharacters } from '../actions'
 
 class App extends Component {
   componentDidMount() {
+    this.props.fetchCharacters()
     // call our action
   }
   render() {
+    console.log('Props for App', this.props)
     return (
       <div className="App">
         {this.props.fetching ? (
           <img src={logo} className="App-logo" alt="logo" />
         ) : (
           <ul>
-            {this.props.chars.map(char => {
+            {this.props.starwarsCharacters.map(char => {
               return <li key={char.name}>{char.name}</li>;
             })}
           </ul>
@@ -25,9 +28,13 @@ class App extends Component {
     );
   }
 }
-
+const mapStateToProps = state => {
+  console.log('State', state)
+  return {
+    fetching: state.charsReducer.fetching,
+    starwarsCharacters: state.charsReducer.starwarsCharacters,
+  }
+}
 // our mapDispatchToProps needs to have two properties inherited from state
 // the chars and the fetching boolean
-export default connect(null, {
-  /* actions go here */
-})(App);
+export default connect(mapStateToProps, { fetchCharacters })(App);
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,13 +1,24 @@
-import /* we need our action types here*/ '../actions';
-const initialState = {
+import { FETCH_CHARS, FETCHING_CHARS, ERROR } from '../actions'
+
+const initialState = { starwarsCharacters: [], fetching: false, fetched: false, error: null
   // define a few properties here.
   // Array chars, Boolean fetching, Boolean fetched, null error.
 };
-export const charsReducer = (state = initialState, action) => {
-  switch (action.type) {
     // Fill me in with the important reducers
     // action types should be FETCHING, FETCHED, and ERROR
     // your switch statement should handle all of these cases.
+
+export const charsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_CHARS:
+    return Object.assign({}, state, {
+      starwarsCharacters: action.payload,
+      fetching: false,
+    });
+    case FETCHING_CHARS:
+      return Object.assign({}, state, { fetching: true });
+    case ERROR:
+      return Object.assign({}, state, { error: action.payload})
     default:
       return state;
   }
