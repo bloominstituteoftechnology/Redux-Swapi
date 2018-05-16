22fd66757f7391b6b9e148ed0926cc1ed5502d10
Matthias Bakken <matthias.bakken@gmail.com>
2018-05-17 03:19:25 +0700

    added exports to actions as well as Fetching action functionality.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,24 @@
 // we'll need axios
-
+import axios from 'axios';
 // we'll need to create 3 different action types here.
 // one for fetching, one for fetched and one for errors
-
+export const FETCHING = 'FETCHING';
+export const FETCHED = 'FETCHED';
+export const ERROR = 'ERROR';
 // our action creator will be a function that returns a promise
 // we'll have to be sure to make our promise resolve within our new "thunk based middlware"
 // the url to fetch charicters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based
+export const fetchChars = () => {
+    const getChars = axios.get('https://swapi.co/api/people/');
+    return function (dispatch) {
+        dispatch({ type: FETCHING });
+        getChars
+            .then(charsData => {
+                dispatch({ type: FETCHED, payload: charsData.data });
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    };
+};
