fc42eac849b2eb1c8ee5d91d817e93ba8bd69e85
Matthias Bakken <matthias.bakken@gmail.com>
2018-05-17 02:47:18 +0700

    fixed typo in reducers

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,13 +1,24 @@
-import /* we need our action types here*/ '../actions';
+import { FETCHING, FETCHED, ERROR } from '../actions';
 const initialState = {
   // define a few properties here.
   // Array chars, Boolean fetching, Boolean fetched, null error.
+  chars: [],
+  fetching: true,
+  fetched: false,
+  error: null
 };
+
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
     // Fill me in with the important reducers
     // action types should be FETCHING, FETCHED, and ERROR
     // your switch statement should handle all of these cases.
+    case FETCHING:
+      return null;
+    case FETCHED:
+      return null;
+    case ERROR:
+      return null;
     default:
       return state;
   }
