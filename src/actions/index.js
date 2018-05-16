476b56b1d1c00b4a1551b93c0cb845d0045c6dcb
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-05-16 15:37:42 -0400

    Finished actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,35 @@
-// we'll need axios
+import axios from 'axios'; // we'll need axios
 
-// we'll need to create 3 different action types here.
-// one for fetching, one for fetched and one for errors
+// we'll need to create 3 different action types here: fetching, fetched, errors
+export const FETCH_CHARACTERS = 'FETCH_CHARACTERS';
+export const FETCHING_CHARACTERS = 'FETCHING_CHARACTERS';
+export const ERROR = 'ERROR';
 
 // our action creator will be a function that returns a promise
 // we'll have to be sure to make our promise resolve within our new "thunk based middlware"
 // the url to fetch charicters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based
+
+export const fetchCharacters = () => {
+  // create promise
+  const getCharacters = axios.get('https://swapi.co/api/people/');
+
+  // return dispatch function
+  // handles all 3 action types since they are tied to promise
+  return (dispatch) => {
+    dispatch({type: FETCH_CHARACTERS}); // fetching
+
+    getCharacters
+      .then(response => {
+        dispatch({ // fetched
+          type: FETCH_CHARACTERS,
+          payload: response.data
+        });
+      })
+      .catch(err => { // error
+        dispatch({type: ERROR})
+      });
+
+    // error
+  };
+};
