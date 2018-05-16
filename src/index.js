1e059564dd8bd6b7d1d12926598b5a20efbe3d9e
Obo Agboghidi <oagboghi2@gmail.com>
2018-05-16 15:25:35 -0700

    Obo Agboghidi 2nd commit

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
 // needed dependancies
 // applyMiddleware from redux
+import { thunk, logger } from 'redux-thunk';
 // thunk from redux-thunk
 // logger from redux-logger
+import { rootReducer } from './reducers'
 // rootReducer from ./reducers
 
-const store = createStore(/* rootReducer */, /* applyMiddleware goes here */);
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <Provider store={store}>
