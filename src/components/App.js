0860ee763dc644abac1d52705858102a0370dd65
mister-corn <code@ronaldlibago.com>
2018-05-16 13:26:25 -0700

    1:25 PM PDT: Mini-project complete.

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import { fetching, fetched } from '../actions';
 import logo from '../logo.svg';
 import '../styles/App.css';
 // pull in actions from action/index
 
 class App extends Component {
   componentDidMount() {
-    // call our action
+    this.props.fetched();
   }
   render() {
+    console.log("App.js props",this.props);
     return (
       <div className="App">
-        {this.props.fetching ? (
+        {this.props.status ? (
           <img src={logo} className="App-logo" alt="logo" />
         ) : (
-          <ul>
-            {this.props.chars.map(char => {
-              return <li key={char.name}>{char.name}</li>;
-            })}
-          </ul>
-        )}
+            <ul>
+              {this.props.chars.map(char => {
+                return <li key={char.name}>{char.name}</li>;
+              })}
+            </ul>
+          )}
       </div>
     );
   }
 }
 
+const mapStateToProps = (state) => {
+  console.log("App.js mapStateToProps", state);
+  return {
+    chars: state.charsReducer.chars,
+    status: state.charsReducer.fetching,
+  }
+}
 // our mapDispatchToProps needs to have two properties inherited from state
 // the chars and the fetching boolean
-export default connect(null, {
-  /* actions go here */
+export default connect(mapStateToProps, {
+  fetching,
+  fetched
 })(App);
