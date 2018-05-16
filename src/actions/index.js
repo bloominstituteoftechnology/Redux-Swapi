d8c0b08abcdbae24c54e5519467b570119e222e9
Zach Campbell <beazlebubbles@gmail.com>
2018-05-16 14:51:28 -0600

    Did the whole project

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 // we'll need axios
+import axios from 'axios';
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for fetched and one for errors
@@ -7,3 +8,25 @@
 // we'll have to be sure to make our promise resolve within our new "thunk based middlware"
 // the url to fetch charicters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based
+
+
+export const FETCHING = "FETCHING";
+export const FETCHED = "FETCHED";
+export const ERROR = "ERROR";
+
+
+
+const myFetcher = () => {
+  const request = axios.get('https://swapi.co/api/people/');
+  return (dispatch) => {
+    dispatch({type: FETCHING});
+    request.then(({data}) => {
+      dispatch({type: FETCHED, payload: data.results});
+    })
+    .catch(err => {
+      dispatch({type: ERROR, payload: err});
+    });
+  };
+};
+
+export default myFetcher;
