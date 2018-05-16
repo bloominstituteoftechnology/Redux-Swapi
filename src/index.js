1f377ae1a8512ca74986ebf48fc8545c174ad795
Matthias Bakken <matthias.bakken@gmail.com>
2018-05-17 02:27:07 +0700

    attached dependencies in index.js and created store

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 // needed dependancies
 // applyMiddleware from redux
-import { applyMiddleware } from 'redux';
+import applyMiddleware from 'redux';
 // thunk from redux-thunk
-import { thunk } from 'redux-thunk';
+import thunk from 'redux-thunk';
 // logger from redux-logger
-import { logger } from 'redux-logger';
+import logger from 'redux-logger';
 // rootReducer from ./reducers
-import rootReducer from './reducers';
+import { rootReducer } from './reducers';
 
 const store = createStore( rootReducer, applyMiddleware(thunk, logger));
 
