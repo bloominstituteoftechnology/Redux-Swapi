4909781fdbb5f6f6a815b140a439a4ea468f904d
maxwashington <mnwashington18@gmail.com>
2018-05-16 15:01:00 -0500

    -First Commit -NPM installed -Imported middleware,createStore,thunk and somecoolReducers in index.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './components';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-// needed dependancies
-// applyMiddleware from redux
-// thunk from redux-thunk
-// logger from redux-logger
-// rootReducer from ./reducers
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { someCoolReducer } from './reducers';
 
-const store = createStore(/* rootReducer */, /* applyMiddleware goes here */);
+const store = createStore( rootReducer ,  applyMiddleware);
 
 ReactDOM.render(
   <Provider store={store}>
