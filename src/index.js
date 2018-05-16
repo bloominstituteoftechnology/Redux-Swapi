8126e41d8fc4b5d00a45e645e1ed31a74da8b565
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-05-16 13:53:33 -0500

    imported applyMiddleware, thunk, logger

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './components';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 // needed dependancies
-// applyMiddleware from redux
-// thunk from redux-thunk
-// logger from redux-logger
+// applyMiddleware from redux DONE
+// thunk from redux-thunk DONE
+// logger from redux-logger DONE
 // rootReducer from ./reducers
 
 const store = createStore(/* rootReducer */, /* applyMiddleware goes here */);
