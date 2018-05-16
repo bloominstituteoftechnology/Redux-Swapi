27fe4d48e31b299d6f43bf1f6b8deccf00135d9c
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-05-16 13:06:34 -0700

    starwarsreducer fixed

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,12 +1,22 @@
-import {FETCHING, FETCHED, ERROR} '../actions';
+import {FETCHING, FETCHED, ERROR} from '../actions';
+
 const initialState = {
-  
+  chars: [],
+  fetching: true,
+  fetched: false,
+  error: null
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
     // Fill me in with the important reducers
     // action types should be FETCHING, FETCHED, and ERROR
     // your switch statement should handle all of these cases.
+    case FETCHING:
+      return null;
+    case FETCHED:
+      return null;
+    case ERROR:
+      return null;
     default:
       return state;
   }
