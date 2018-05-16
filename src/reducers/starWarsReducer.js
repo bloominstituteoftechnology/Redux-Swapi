1c788481586a24628eaa08419eff3dd613f42602
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-05-16 15:44:30 -0400

    Block out charsReducer: not changing state yet

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,13 +1,20 @@
-import /* we need our action types here*/ '../actions';
+import { FETCHING_CHARACTERS, FETCH_CHARACTERS, ERROR } from '../actions';
+
 const initialState = {
   // define a few properties here.
   // Array chars, Boolean fetching, Boolean fetched, null error.
+  characters: [],
+  fetched: false,
+  error: null
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
-    // Fill me in with the important reducers
-    // action types should be FETCHING, FETCHED, and ERROR
-    // your switch statement should handle all of these cases.
+    case FETCHING_CHARACTERS:
+      return state;
+    case FETCH_CHARACTERS:
+      return state;
+    case ERROR:
+      return state;
     default:
       return state;
   }
