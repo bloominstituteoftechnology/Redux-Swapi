b0de4fb4d152942dd2f09db91be8649f77fe497d
Brandon Benefield <bsquared18@gmail.com>
2018-05-16 16:18:19 -0400

    feat(src/index): import `applyMiddleware`, `createStore` from `redux` import `thunk` from `redux-thunk` pass the `charsReducer` and `thunk` middleware to the redux store

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import { App } from './components';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+// import logger from 'redux-logger';
+import { charsReducer } from './reducers/starWarsReducer';
+
+import { App } from './components';
+
+import './index.css';
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
 // logger from redux-logger
 // rootReducer from ./reducers
 
-const store = createStore(/* rootReducer */, /* applyMiddleware goes here */);
+const store = createStore(charsReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
