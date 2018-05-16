7d57aaf14379c518621e1cf0e121dad303b46cc3
Bee C <b.choeung@gmail.com>
2018-05-16 18:39:59 -0400

    added depemdencies and store information on indes.js

--- src/index.js
+++ src/index.js
@@ -6,11 +6,15 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 // needed dependancies
 // applyMiddleware from redux
+import { applyMiddleware, createStore } from 'redux';
 // thunk from redux-thunk
+import thunk from 'redux-thunk';
 // logger from redux-logger
+import logger from 'redux-logger';
 // rootReducer from ./reducers
+import rootReducer from './reducers';
 
-const store = createStore(/* rootReducer */, /* applyMiddleware goes here */);
+const store = createStore(/* rootReducer */, /* applyMiddleware goes here */ rootReducer, applyMiddlewear);
 
 ReactDOM.render(
   <Provider store={store}>
