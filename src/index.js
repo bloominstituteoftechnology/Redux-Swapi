8117577abd5aca722e2fb58748879d4018805a6c
Marco A Guzman <marcoguzman16@gmail.com>
2018-05-17 15:08:15 -0700

    Marco Guzman pull request swapi

--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './components';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
 // logger from redux-logger
 // rootReducer from ./reducers
 
-const store = createStore(/* rootReducer */, /* applyMiddleware goes here */);
+const store = createStore(rootReducer, applyMiddleware);
 
 ReactDOM.render(
   <Provider store={store}>
@@ -18,3 +20,6 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+
+//PullReq note for Swapi project
