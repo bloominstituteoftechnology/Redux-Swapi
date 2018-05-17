8a11ed6fe35421b0ea003f6c771e021241f05b33
Bee C <b.choeung@gmail.com>
2018-05-17 16:59:48 -0400

    adding actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 // we'll need axios
 
 // we'll need to create 3 different action types here.
@@ -7,3 +8,11 @@
 // we'll have to be sure to make our promise resolve within our new "thunk based middlware"
 // the url to fetch charicters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based
+
+export const fetchStarwars = () => {
+    const getStarwars = axios.get( `https://swapi.co/api/people/`);
+    return function(dispatch) {
+        dispatch({ type: FETCHING_STARWARS });
+        get
+    }
+}
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 // needed dependancies
 // applyMiddleware from redux
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware } from 'redux';
 // thunk from redux-thunk
 import thunk from 'redux-thunk';
 // logger from redux-logger
@@ -14,7 +14,7 @@ import logger from 'redux-logger';
 // rootReducer from ./reducers
 import rootReducer from './reducers';
 
-const store = createStore(/* rootReducer */, /* applyMiddleware goes here */ rootReducer, applyMiddlewear);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
