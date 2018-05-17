b3c054e7b04d36064eb4cf14a932a1f4c9baac3b
Brandon Hopper <BrandonHopper0@gmail.com>
2018-05-17 17:07:06 -0400

    -edited root reducer and store

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger'
-import { charsReducer } from './reducers/starWarsReducer';
+import rootReducer  from './reducers';
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
 // logger from redux-logger
 // rootReducer from ./reducers
 
-const store = createStore( charsReducer, applyMiddleware(thunk, logger));
+const store = createStore( rootReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <Provider store={store}>
