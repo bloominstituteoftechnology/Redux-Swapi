f075eba43a6f6eff2d960ac3a338a216a209a4fd
Derric-Mei <derrick.mei.168@gmail.com>
2018-05-17 16:12:43 -0500

    finished src/index.js but I still need to import reducer for createStore

--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './components';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 // needed dependancies
-// applyMiddleware from redux
+// applyMiddleware from redux which I did above in conjunction with createStore
 // thunk from redux-thunk
+import thunk from "redux-thunk";
 // logger from redux-logger
+import logger from "redux-logger";
 // rootReducer from ./reducers
 
-const store = createStore(/* rootReducer */, /* applyMiddleware goes here */);
+const store = createStore( ()=> {}/* rootReducer */,  applyMiddleware(thunk, logger)/* applyMiddleware goes here */);
 
 ReactDOM.render(
   <Provider store={store}>
