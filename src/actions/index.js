087b3222a9b545a4e08e17d33c4940668fa6d60d
Niloc92 <prestonresult@github.com>
2018-05-17 07:47:37 -0500

    still debugging

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,27 @@
+import axios from 'axios';
+export const FETCHING = 'FETCHING_CHARS';
+export const FETCHED = 'CHARS_FETCHED';
+export const FETCHED_ERR = 'ERROR_FETCHING_CHARS';
+
 // we'll need axios
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for fetched and one for errors
 
-// our action creator will be a function that returns a promise
-// we'll have to be sure to make our promise resolve within our new "thunk based middlware"
-// the url to fetch charicters from is `https://swapi.co/api/people/`
-// remember that now we have controll over our thunk-based
+export const fetch = () => {
+    const Chars = axios.get(`https://swapi.co/api/people/`);
+    return dispatch => {
+        dispatch({ type: FETCHING });
+        Chars
+            .then(response => {
+                dispatch({ type: FETCHED, payload: response.data.results });
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch({
+                    type: FETCHED_ERR,
+                    payload: 'Error Fetching Chars'
+                });
+            });
+    };
+};
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+// import * as F_Options from '../actions/index';
+import { fetch,FETCHING, } from '../actions';
 import logo from '../logo.svg';
 import '../styles/App.css';
 // pull in actions from action/index
 
 class App extends Component {
   componentDidMount() {
-    // call our action
+    this.props.fetch().chars;
   }
   render() {
     return (
@@ -28,6 +29,11 @@ class App extends Component {
 
 // our mapDispatchToProps needs to have two properties inherited from state
 // the chars and the fetching boolean
-export default connect(null, {
-  /* actions go here */
-})(App);
+const mapDispatchToProps = state => {
+  return {
+    chars: state.charsReducer.chars,
+    fetching: state.charsReducer.fetching,
+    
+  };
+};
+export default connect(mapDispatchToProps, fetch )(App);
