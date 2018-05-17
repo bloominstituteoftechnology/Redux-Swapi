9dde017db566b4f53c6f5116ab87106422bf9d87
Brandon Hopper <BrandonHopper0@gmail.com>
2018-05-17 17:20:22 -0400

    updated app.js to component did mount and connect method

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,17 @@ import { connect } from 'react-redux';
 
 import logo from '../logo.svg';
 import '../styles/App.css';
+import { fetchChars } from '../actions';
 // pull in actions from action/index
+// import {fetchChars} from '../actions/index';
 
 class App extends Component {
   componentDidMount() {
     // call our action
+    this.props.fetchChars()
   }
   render() {
+    console.log(this.props)
     return (
       <div className="App">
         {this.props.fetching ? (
@@ -28,6 +32,13 @@ class App extends Component {
 
 // our mapDispatchToProps needs to have two properties inherited from state
 // the chars and the fetching boolean
-export default connect(null, {
-  /* actions go here */
-})(App);
+const mapDispatchToProps = (state) => {
+  console.log('STATE', state)
+  return {
+    chars:  state.charsReducer.chars,
+    fetching: state.charsReducer.fetching,
+    fetched: state.charsReducer.fetched
+  }
+}
+
+export default connect(mapDispatchToProps, {fetchChars}) (App);
