dae3c57c62acb96c0c6aada19712b3b0e0d0ad40
sagardesai90 <saagardesai@gmail.com>
2018-05-17 17:49:55 -0500

    get closer

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './components';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
 // needed dependancies
 import { applyMiddleware, createStore } from 'redux';// applyMiddleware from redux
-import { thunk } from 'redux-thunk';// thunk from redux-thunk
-import { logger } from 'redux-logger';// logger from redux-logger
-import { rootReducer } from './reducers';// rootReducer from ./reducers
+import thunk from 'redux-thunk';// thunk from redux-thunk
+import logger from 'redux-logger';// logger from redux-logger
+import combineReducers from './reducers';// rootReducer from ./reducers
 
-const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+const store = createStore(combineReducers, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <Provider store={store}>
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,4 +1,4 @@
-import {FETCHING, FETCHED, ERROR} '../actions';
+import {FETCHING, FETCHED, ERROR} from '../actions';
 const initialState = {
   // define a few properties here.
   // Array chars, Boolean fetching, Boolean fetched, null error.
@@ -10,15 +10,15 @@ export const charsReducer = (state = initialState, action) => {
     // Fill me in with the important reducers
     // action types should be FETCHING, FETCHED, and ERROR
     // your switch statement should handle all of these cases.
-    case FETCHING {
-      return Object.assign({}, state, {fetching: true})
-    }
-    case FETCHED {
-      return Object.assign({}, state, {fetched: true})
-    }
-    case ERROR {
-      return Object.assign({}, state, {error: action.payload})
-    }
+    case FETCHING: 
+      return Object.assign({}, state, {fetching: true});
+    
+    case FETCHED: 
+      return Object.assign({}, state, {fetched: true});
+    
+    case ERROR: 
+      return Object.assign({}, state, {error: action.payload});
+    
     default:
       return state;
   }
