b4e36772e1056326d700123073d96c9c629a77fc
David <AquilaVirtual>
2018-05-17 21:59:29 -0700

    Initial commit: work in progress

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,26 @@
 // we'll need axios
+import axios from 'axios';
+import thunk from 'redux-thunk';
+ export const FETCH_PEOPLE = "FETCH_PEOPLE";
+
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for fetched and one for errors
 
+export const fetchPeople = () => {
+    const people = axios.get(`https://swapi.co/api/people/`);
+    return function (dispatch) {
+        people.then((response) => {
+        // console.log(response);
+            dispatch({type: FETCH_PEOPLE, payload: response.data });
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    }
+}
 // our action creator will be a function that returns a promise
 // we'll have to be sure to make our promise resolve within our new "thunk based middlware"
 // the url to fetch charicters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based
+
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,10 +1,13 @@
-import /* we need our action types here*/ '../actions';
+import {FETCH_PEOPLE} from '../actions';
 const initialState = {
   // define a few properties here.
   // Array chars, Boolean fetching, Boolean fetched, null error.
+
 };
-export const charsReducer = (state = initialState, action) => {
+export const charsReducer = (state = [], action) => {
   switch (action.type) {
+    case FETCH_PEOPLE:
+    return state.concat(action.payload);
     // Fill me in with the important reducers
     // action types should be FETCHING, FETCHED, and ERROR
     // your switch statement should handle all of these cases.
