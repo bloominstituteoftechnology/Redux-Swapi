a09929769b485e5283202020eeb5a07d092c29fd
Derric-Mei <derrick.mei.168@gmail.com>
2018-05-18 01:23:30 -0500

    added error message

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        {this.props.error ? <h1>{this.props.error}</h1> : null}
         {this.props.fetching ? (
           <img src={logo} className="App-logo" alt="logo" />
         ) : (
