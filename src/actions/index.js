0eaa62dd20057ddf6b8c58467539310dee69bcdb
Simon <Simon_Freelance@hotmail.com>
2018-07-11 18:52:34 -0400

    Axios fetches the starwars data.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,8 @@
+import axios from 'axios';
 // we'll need axios
-
+export const FETCHING_DATA = "FETCHING_DATA";
+export const FETCHED_DATA = "FETCHED_DATA";
+export const ERROR = "ERROR";
 // we'll need to create 3 different action types here.
 // one for fetching, one for fetched and one for errors
 
@@ -7,3 +10,17 @@
 // we'll have to be sure to make our promise resolve within our new "thunk based middlware"
 // the url to fetch charicters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based
+
+export const fetchData = () => {
+    const request = axios.get('https://swapi.co/api/people/');
+    return (dispatch) => {
+        dispatch( {type: FETCHING_DATA });
+        request.then(response => {
+            console.log(response.data);
+            dispatch({type: FETCHED_DATA, payload: response.data.results });
+        })
+        .catch(err => {
+            dispatch({ type: ERROR, payload: err })
+        });
+    }
+}
