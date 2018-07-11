3f1536b1608624c850415fa913162be04a34d28a
Sam Khaled <samkhaled@outlook.com>
2018-07-11 17:28:52 -0400

    Completed assignment

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,34 @@
 // we'll need axios
+import axios from 'axios'
 
-// we'll need to create 3 different action types here.
-// one for fetching, one for fetched and one for errors
+export const FETCHING_DATA = 'FETCHING_DATA'
+export const DATA_FETCHED = 'DATA_FETCHED'
+export const FETCH_ERROR = 'FETCH_ERROR'
 
-// our action creator will be a function that returns a promise
-// we'll have to be sure to make our promise resolve within our new "thunk based middlware"
-// the url to fetch charicters from is `https://swapi.co/api/people/`
-// remember that now we have controll over our thunk-based
+export const fetchingData = () => ({
+  type: FETCHING_DATA,
+})
+
+export const fetchError = error => ({ type: FETCH_ERROR, error })
+ 	 
+ // we'll need to create 3 different action types here.	 // we'll need to create 3 different action types here.
+ // one for fetching, one for fetched and one for errors	 // one for fetching, one for fetched and one for errors
+export const fetchData = () => {
+  return dispatch => {
+    // Let reducers know we're fetching:
+    dispatch(fetchingData())
+
+    axios.get('https://swapi.co/api/people/')
+      .then(({ data }) => {
+	console.log(data)
+	dispatch({
+	  type: DATA_FETCHED,
+	  payload: data.results,
+	})
+      })
+      .catch(e => fetchError(e))
+  }
+}
+ 	 
+ // our action creator will be a function that returns a promise	 // our action creator will be a function that returns a promise
+ // we'll have to be sure to make our promise resolve within our new "thunk based middlware"	 // we'll have to be sure to make our promise resolve within our new "thunk based middlware"
--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { App } from './components';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-// needed dependancies
-// applyMiddleware from redux
-// thunk from redux-thunk
-// logger from redux-logger
-// rootReducer from ./reducers
+import { App } from './components';	 
+import { Provider } from 'react-redux';	 
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
+import rootReducer from './reducers/'
 
-const store = createStore(/* rootReducer */, /* applyMiddleware goes here */);
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+ // needed dependancies	 
+ // applyMiddleware from redux	 
+ // thunk from redux-thunk
+ // logger from redux-logger
+ // rootReducer from ./reducers	
+ 	 
+const store = createStore(rootReducer, applyMiddleware(logger, thunk));
+ 	 
+ ReactDOM.render(	 
+   <Provider store={store}>	   
+   <App />
+   </Provider>,	  
+    document.getElementById('root')
+	 );
