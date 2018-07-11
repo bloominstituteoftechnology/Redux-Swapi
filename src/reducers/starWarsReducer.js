72d2829ff2bfac7e24802c923e5c5577d2447ae3
JJ Ashcraft <jj@jjashcraft.com>
2018-07-11 19:05:46 -0400

    updated actions

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,4 +1,4 @@
-import { FETCH, FETCHING, FETCH_ERROR } from '../actions';
+import { FETCHED, FETCHING, FETCH_ERROR } from '../actions';
 const initialState = {
   // define a few properties here.
   chars: [],
@@ -9,6 +9,13 @@ const initialState = {
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCHED: 
+    return console.log('fetched');
+    case FETCHING:
+    return console.log('fetching');
+    case FETCH_ERROR:
+    return console.log('fetch error');
+
     // Fill me in with the important reducers
     // action types should be FETCHING, FETCHED, and ERROR
     // your switch statement should handle all of these cases.
