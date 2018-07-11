b14764c77d2ad58e85985a5a04b8a77584f908f2
jeffyjkang <jeffyjkang@gmail.com>
2018-07-11 14:20:59 -0700

    jeff Kang redux swapi

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import { App } from './components';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-// needed dependancies
-// applyMiddleware from redux
-// thunk from redux-thunk
-// logger from redux-logger
-// rootReducer from ./reducers
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { App } from "./components";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import { charsReducer } from "./reducers/starWarsReducer";
+// needed dependancies done
+// applyMiddleware from redux done
+// thunk from redux-thunk done
+// logger from redux-logger done
+// rootReducer from ./reducers done
 
-const store = createStore(/* rootReducer */, /* applyMiddleware goes here */);
+const store = createStore(charsReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,13 +1,41 @@
-import /* we need our action types here*/ '../actions';
+import {
+  /* we need our action types here*/
+  FETCHING,
+  FETCHED,
+  ERROR
+} from "../actions";
 const initialState = {
   // define a few properties here.
   // Array chars, Boolean fetching, Boolean fetched, null error.
+  chars: [],
+  fetching: false,
+  fetched: false,
+  error: null
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
     // Fill me in with the important reducers
     // action types should be FETCHING, FETCHED, and ERROR
     // your switch statement should handle all of these cases.
+    case FETCHING:
+      return Object.assign({}, state, {
+        fetching: true
+        // fetched: false,
+        // error: null
+      });
+    case FETCHED:
+      return Object.assign({}, state, {
+        chars: action.payload,
+        fetching: false,
+        fetched: true
+        // error: null
+      });
+    case ERROR:
+      return Object.assign({}, state, {
+        fetching: false,
+        fetched: false,
+        error: action.payload
+      });
     default:
       return state;
   }
