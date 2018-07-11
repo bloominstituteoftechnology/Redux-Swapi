2eb464de0846ea5678cfc60e0ffd3f703c5c4934
Simon <Simon_Freelance@hotmail.com>
2018-07-11 18:53:29 -0400

    Fetching commands are setup except the ERROR and schema of intiailState setup

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,10 +1,20 @@
-import /* we need our action types here*/ '../actions';
+import { FETCHING_DATA, FETCHED_DATA, ERROR } from '../actions';
 const initialState = {
   // define a few properties here.
   // Array chars, Boolean fetching, Boolean fetched, null error.
+  chars: [],
+  fetching: false,
+  fetched: false,
+  error: null
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCHING_DATA:
+    return Object.assign({}, state, { fetching: true });
+    case FETCHED_DATA:
+    return Object.assign({}, state, { fetching: false, fetched: true, chars: action.payload });
+    case ERROR:
+    return;
     // Fill me in with the important reducers
     // action types should be FETCHING, FETCHED, and ERROR
     // your switch statement should handle all of these cases.
