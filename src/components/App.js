051e773129c82f3b15b61a7577be56bf7d6c6280
Libby Hart <libbjo@gmail.com>
2018-07-11 15:19:39 -0600

    just some updates

--- src/components/App.js
+++ src/components/App.js
@@ -28,7 +28,12 @@ class App extends Component {
 
 // our mapDispatchToProps needs to have two properties inherited from state
 // the chars and the fetching boolean
-const mapDispatchToProps = (dispatch,)
+const mapStateToProps = state => {
+  return {
+    chars: state.charsReducer.chars,
+    fetching: state.charsReducer.fetching
+  }
+}
 export default connect(null, {
   /* actions go here */
 })(App);
--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './components';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { charsReducer } from './reducers';
+import logger from 'redux-logger';
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
 // logger from redux-logger
 // rootReducer from ./reducers
 
-const store = createStore(/* rootReducer */, /* applyMiddleware goes here */);
+const store = createStore(charsReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <Provider store={store}>
