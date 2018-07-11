57eb35bde168d5a76a4039769a003f221418223a
Simon <Simon_Freelance@hotmail.com>
2018-07-11 19:02:54 -0400

    Error action works when it cannot get the proper data

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -14,7 +14,7 @@ export const charsReducer = (state = initialState, action) => {
     case FETCHED_DATA:
     return Object.assign({}, state, { fetching: false, fetched: true, chars: action.payload });
     case ERROR:
-    return;
+    return Object.assign({}, state, { error: action.payload });
     // Fill me in with the important reducers
     // action types should be FETCHING, FETCHED, and ERROR
     // your switch statement should handle all of these cases.
