f8bc119c87d371aaa8a6cd2617f0248a4c9ae885
CookieMonsta89 <jlstan06@gmail.com>
2018-07-11 16:10:19 -0400

    finished with actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,3 +7,22 @@
 // we'll have to be sure to make our promise resolve within our new "thunk based middlware"
 // the url to fetch charicters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based
+import axios from "axios";
+
+export const FETCHING = "FETCHING";
+export const FETCHED = "FETCHED";
+export const ERROR = "ERROR";
+
+export const gettingThis = () => {
+    const getData = axios.get(`https://swapi.co/api/people/`);
+    return function(dispatch) {
+        dispatch({type: FETCHING});
+        getData
+            .then((characterData) => {
+                dispatch({ type: FETCHED, payload: characterData.data.results});
+            })
+            .catch(err => {
+                dispatch({type: ERROR, payload: console.log(err)});
+            })
+    };
+};
