d8033333413aab58d57e488d46cc14bd992dad7d
raymondgrc <raymond_grc@yahoo.com>
2018-07-11 16:24:15 -0500

    Near completion, problem displaying data

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,23 @@
 // we'll need axios
-
-// we'll need to create 3 different action types here.
-// one for fetching, one for fetched and one for errors
+import axios from "axios";
+export const FETCHING = "FETCHING";
+export const FETCHED = "FETCHED";
+export const ERROR = "ERROR";
 
 // our action creator will be a function that returns a promise
 // we'll have to be sure to make our promise resolve within our new "thunk based middlware"
 // the url to fetch charicters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based
+
+export const getCharacters = () => {
+  return function(dispatch) {
+        dispatch({type: FETCHING});
+        axios.get(`https://swapi.co/api/people/`)
+          .then((chars) => {
+              dispatch({ type: FETCHED, payload: chars.data.results});
+            })
+          .catch(error => {
+              dispatch({type: ERROR, payload: console.log(error)});
+            })
+    };
+}
