f215651f853e47a769be6b7327ee21114b329f93
Kevin <brackkevin@gmail.com>
2018-07-12 15:33:33 -0500

    fixed thunk and logger imports

--- src/index.js
+++ src/index.js
@@ -9,13 +9,13 @@ import { createStore } from "redux";
 // applyMiddleware from redux
 import { applyMiddleware } from "redux";
 // thunk from redux-thunk
-import { thunk } from "redux-thunk";
+import thunk from "redux-thunk";
 // logger from redux-logger
-import { logger } from "redux-logger";
+import logger from "redux-logger";
 // rootReducer from ./reducers
 import rootReducer from "./reducers";
 
-const store = createStore(rootReducer, applyMiddleware);
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <Provider store={store}>
