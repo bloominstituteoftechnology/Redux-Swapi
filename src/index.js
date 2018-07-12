8b09e79dfe45f408b67eda13e65d2126a70e6467
chainchompa <chainchompa@gmail.com>
2018-07-12 14:40:49 -0400

    imported packages

--- src/index.js
+++ src/index.js
@@ -4,12 +4,10 @@ import './index.css';
 import { App } from './components';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-// needed dependancies
-// applyMiddleware from redux
-// thunk from redux-thunk
-// logger from redux-logger
-// rootReducer from ./reducers
+import logger from 'redux-logger';
+import rootReducer from './reducers';
 
 const store = createStore(/* rootReducer */, /* applyMiddleware goes here */);
 
