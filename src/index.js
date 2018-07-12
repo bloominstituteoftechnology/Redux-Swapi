1873b225dc7f6802dc305f3da731e8ea0ff73b81
Kevin <brackkevin@gmail.com>
2018-07-12 14:43:20 -0500

    fixed imports

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { applyMiddleware } from "redux";
 // thunk from redux-thunk
 import { thunk } from "redux-thunk";
 // logger from redux-logger
-import { loger } from "redux-logger";
+import { logger } from "redux-logger";
 // rootReducer from ./reducers
 import rootReducer from "./reducers";
 
