78515229d26b22ba0a658bce36b8d0c45133e66b
chainchompa <chainchompa@gmail.com>
2018-07-12 15:49:58 -0400

    completed mvp

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,27 @@
 // we'll need axios
-
+import axios from 'axios';
 // we'll need to create 3 different action types here.
 // one for fetching, one for fetched and one for errors
-
+export const FETCHING = 'FETCHING';
+export const FETCHED = 'FETCHED';
+export const ERROR = 'ERROR';
 // our action creator will be a function that returns a promise
 // we'll have to be sure to make our promise resolve within our new "thunk based middlware"
 // the url to fetch charicters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based
+export const fetchCharacters = () => {
+  const characters = `https://swapi.co/api/people/`;
+  return dispatch => {
+    dispatch({
+      type: FETCHING,
+    })
+    axios
+      .get(characters)
+      .then(({ data }) => {
+        dispatch({ type: FETCHED, payload: data.results})
+      })
+      .catch(error => {
+        dispatch({type: ERROR})
+      })
+  }
+};
--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,11 @@ import { connect } from 'react-redux';
 import logo from '../logo.svg';
 import '../styles/App.css';
 // pull in actions from action/index
+import { fetchCharacters } from '../actions';
 
 class App extends Component {
   componentDidMount() {
-    // call our action
+    this.props.fetchCharacters();
   }
   render() {
     return (
@@ -28,6 +29,10 @@ class App extends Component {
 
 // our mapDispatchToProps needs to have two properties inherited from state
 // the chars and the fetching boolean
-export default connect(null, {
-  /* actions go here */
-})(App);
+const mapDispatchToProps = state => {
+  return {
+    chars: state.charsReducer.chars,
+    fetching: state.charsReducer.fetching
+  }
+}
+export default connect(mapDispatchToProps, { fetchCharacters })(App);
