2f8302c9e31959cf1c210ca4917f16c29c5f63e9
sean chavez <seanchavez@seanchavezsMBP.home>
2018-07-18 14:55:13 -0700

    Sean Chavez--Redux-Swapi Initialize project Apply thunk and logger to Redux store Implement starWarsReducer

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,5 +1,10 @@
-import /* we need our action types here*/ '../actions';
+import { FETCHING, FETCHED, ERROR } from '../actions/index';
+import { Object } from 'core-js';
 const initialState = {
+  chars: [],
+  fetching: false,
+  fetched: false,
+  error: null,
   // define a few properties here.
   // Array chars, Boolean fetching, Boolean fetched, null error.
 };
@@ -8,6 +13,14 @@ export const charsReducer = (state = initialState, action) => {
     // Fill me in with the important reducers
     // action types should be FETCHING, FETCHED, and ERROR
     // your switch statement should handle all of these cases.
+    case FETCHING:
+      return Object.assign({}, state, {fetching: true});
+    case FETCHED: 
+      return Object.assign(
+        {}, state, {fetching: false, fetched: true, chars: action.payload}
+      );
+    case ERROR:
+      return Object.assign({}, state, {error: action.payload, fetching: false});  
     default:
       return state;
   }
