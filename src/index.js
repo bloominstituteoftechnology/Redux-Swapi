8ac371a19f0ffdd29480ae8628e4fa54338c4652
Sumayyah <sumayyah@zubairs-air.attlocal.net>
2018-07-18 13:16:11 -0700

    PR

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './components';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import rootReducer from './reducers'
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
