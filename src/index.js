39489b029a661cd9b217dbdfab4561a475fd0b4b
Adrian Adames <adrianadames@gmail.com>
2018-07-20 21:42:44 -0400

    Imported dependencies to add asynchronous functionality to redux..

--- src/index.js
+++ src/index.js
@@ -3,14 +3,34 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './components';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
 // logger from redux-logger
 // rootReducer from ./reducers
 
+
+// Simply put, middleware is software that sits between 
+// our action creators and the reducer stack. Every action 
+// will first go through all middleware sequentially before 
+// it is sent to the reducers.
+
+// redux-thunk allows us to turn our action creators into 
+// async functions by granting them the ability to return 
+// 'functions' instead of plain objects.
+
 const store = createStore(/* rootReducer */, /* applyMiddleware goes here */);
+
+// Instead of passing our initial state directly to our 
+// createStore() function, we can pass it applyMiddleware
+// as a second argument.
+
+// Then, anything we add to applyMiddleware we'll have 
+// access to once we pass it to our store and set it as 
+// a property on the <Provider /> component.
 
 ReactDOM.render(
   <Provider store={store}>
