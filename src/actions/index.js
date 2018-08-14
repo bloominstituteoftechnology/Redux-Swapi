f725352ef918969e1d7139ccfd0cb199de0b13f7
Ashwin <aswinjeganath@gmail.com>
2018-08-15 03:39:54 +0530

    Implement reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,12 @@
 // we'll need axios
-
+import axios from 'axios';
 // we'll need to create 3 different action types here.
 // one for fetching, one for fetched and one for errors
+export const FETCH_CHARS_REQUEST = 'FETCH_CHARS_REQUEST';
+export const FETCH_CHARS_SUCCESS = 'FETCH_CHARS_SUCCESS';
+export const FETCH_CHARS_FAILURE = 'FETCH_CHARS_FAILURE';
 
+const API_URL = 'https://swapi.co/api/people/';
 // our action creator will be a function that returns a promise
 // we'll have to be sure to make our promise resolve within our new "thunk based middlware"
 // the url to fetch charicters from is `https://swapi.co/api/people/`
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
-import { charsReducer } from './starWarsReducer';
+import { charsReducer, isFetching } from './starWarsReducer';
 
 export default combineReducers({
-  charsReducer
+  charsReducer,
+  isFetching
 });
