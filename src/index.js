de6a74bf772fe6b1748d475ada23eefa27b980b3
Trevor <trevorfehrman@gmail.com>
2018-08-14 15:46:32 -0700

    MVP complete

--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './components';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-// needed dependancies
-// applyMiddleware from redux
-// thunk from redux-thunk
-// logger from redux-logger
-// rootReducer from ./reducers
+import { applyMiddleware, createStore } from 'redux';
+import rootReducer from './reducers'
+import thunk from 'redux-thunk';
+import logger from 'redux-logger'
 
-const store = createStore(/* rootReducer */, /* applyMiddleware goes here */);
+
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <Provider store={store}>
