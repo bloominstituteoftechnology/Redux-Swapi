afadfaaa39778f52a3b83b052d437157039ceb4c
jaymzsocool <james.mcraejr@gmail.com>
2018-08-15 16:17:39 -0400

    initial wiring

--- src/index.js
+++ src/index.js
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './components';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
 // logger from redux-logger
 // rootReducer from ./reducers
+import logger from 'redux-logger';
+import rootReducer from './reducers';
+import thunk from 'redux-thunk';
 
-const store = createStore(/* rootReducer */, /* applyMiddleware goes here */);
+
+
+const store = createStore( rootReducer , applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <Provider store={store}>
