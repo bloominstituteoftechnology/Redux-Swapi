258879d79d2d414fd44636af187ec00bb78f2a9c
Grant Reighard <grant.reighard@gmail.com>
2018-08-15 17:00:36 -0400

    Initial commit.

--- src/index.js
+++ src/index.js
@@ -9,8 +9,12 @@ import { createStore } from 'redux';
 // thunk from redux-thunk
 // logger from redux-logger
 // rootReducer from ./reducers
+import { applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import rootReducer from './reducers';
 
-const store = createStore(/* rootReducer */, /* applyMiddleware goes here */);
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <Provider store={store}>
