ecdb01aea01b1edbbb079190d21ca2b4fbe71501
Jurgen Kela <jurgenkela@Jurgens-MacBook-Pro.local>
2018-08-15 18:37:47 -0400

    opening PR

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import './index.css';
 import { App } from './components';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+
+
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
 // logger from redux-logger
 // rootReducer from ./reducers
 
-const store = createStore(/* rootReducer */, /* applyMiddleware goes here */);
+const store = createStore(/* rootReducer *//* applyMiddleware goes here */);
 
 ReactDOM.render(
   <Provider store={store}>
