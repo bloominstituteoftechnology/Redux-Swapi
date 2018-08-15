91d7d934125a11605cdc4ed0121a23e01de8f769
mkerbleski <mkerbles@gmail.com>
2018-08-15 13:42:48 -0600

    init. also started filling in prompts.

--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ import { App } from './components';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 // needed dependancies
+import {applyMiddleware} from 'redux';
 // applyMiddleware from redux
+import thunk from 'redux-thunk';
 // thunk from redux-thunk
+import logger from 'redux-logger';
 // logger from redux-logger
+import rootReducer from './reducers'
 // rootReducer from ./reducers
 
-const store = createStore(/* rootReducer */, /* applyMiddleware goes here */);
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <Provider store={store}>
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,10 +1,32 @@
-import /* we need our action types here*/ '../actions';
+/* we need our action types here*/ '../actions';
+import {FETCHING, FETCHED, ERRORS} from '../actions'
+
 const initialState = {
   // define a few properties here.
   // Array chars, Boolean fetching, Boolean fetched, null error.
+  chars: {},
+  isFetching: false,
+  isFetched: false,
+  errors: [],
+  hasErrors: false
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCHING:
+      return Object.assign({}, state, {
+        isFetching: true,
+      })
+    case FETCHED:
+      return Object.assign({}, state, {
+        isFetching: false,
+        isFetched: true,
+      })
+    case ERRORS:
+      return Object.assign({}, state, {
+        isFetching: false,
+        errors: [...state.errors],
+        hasErrors: true,
+      })
     // Fill me in with the important reducers
     // action types should be FETCHING, FETCHED, and ERROR
     // your switch statement should handle all of these cases.
