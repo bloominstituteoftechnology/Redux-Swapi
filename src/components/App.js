0f1278b5ae794a846927e2ff024d453497ed2d8a
szincone <sawyerzincone@gmail.com>
2018-08-15 15:18:26 -0600

    fetching call looks like it maybe working

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-
-import logo from '../logo.svg';
-import '../styles/App.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "../styles/App.css";
 // pull in actions from action/index
-
+import { fetchCharData } from "../actions";
 class App extends Component {
   componentDidMount() {
     // call our action
+    console.log("COMPONENT DID MOUNT", this.props.fetchCharData);
+    this.props.fetchCharData();
   }
   render() {
     return (
       <div className="App">
-        {this.props.fetching ? (
+        {/* {this.props.fetching ? (
           <img src={logo} className="App-logo" alt="logo" />
         ) : (
           <ul>
@@ -20,14 +20,26 @@ class App extends Component {
               return <li key={char.name}>{char.name}</li>;
             })}
           </ul>
-        )}
+        )} */}
+        <p>Hey</p>
       </div>
     );
   }
 }
 
 // our mapDispatchToProps needs to have two properties inherited from state
+const mapStateToProps = state => {
+  console.log("MapsStatetoProps state CHARS", state.charsReducer.chars);
+  console.log("MapsStatetoProps state FETCHED", state.charsReducer.isFetching);
+  return {
+    chars: state.charsReducer.chars,
+    isFetching: state.charsReducer.isFetching
+  };
+};
 // the chars and the fetching boolean
-export default connect(null, {
-  /* actions go here */
-})(App);
+export default connect(
+  mapStateToProps,
+  {
+    fetchCharData
+  }
+)(App);
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,13 +1,29 @@
-import /* we need our action types here*/ '../actions';
+import { FETCHING } from "../actions";
 const initialState = {
   // define a few properties here.
   // Array chars, Boolean fetching, Boolean fetched, null error.
+  chars: [],
+  isFetching: false,
+  isFetched: false,
+  hasError: false
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
     // Fill me in with the important reducers
     // action types should be FETCHING, FETCHED, and ERROR
     // your switch statement should handle all of these cases.
+    case FETCHING:
+      // return Object.assign({}, state, {
+      //   ...state,
+      //   isFetching: true,
+      //   isFetched: false
+      // });
+      //same as above, using object spread syntax
+      return {
+        ...state,
+        isFetching: true,
+        isFetched: false
+      };
     default:
       return state;
   }
