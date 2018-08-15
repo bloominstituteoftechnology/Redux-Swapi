a628f384e18ca4cc27bee390c7790320054a24e7
johnoro <officialjohnorourke@gmail.com>
2018-08-15 15:11:48 -0400

    Installed dependiences and set up the master index.js
    (src/index.js)

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import reducers from './reducers';
 import './index.css';
 import { App } from './components';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
 // logger from redux-logger
 // rootReducer from ./reducers
 
-const store = createStore(/* rootReducer */, /* applyMiddleware goes here */);
+const store = createStore(reducers, applyMiddleware(logger, thunk));
 
 ReactDOM.render(
   <Provider store={store}>
