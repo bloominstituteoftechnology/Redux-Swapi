fae77e7fca1ef8b533f14332166761d912f33d34
codejoncode <jonathanjholloway@gmail.com>
2018-08-15 14:17:43 -0500

    Started the action firt. Action performs the request on the api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 // we'll need axios
 
 // we'll need to create 3 different action types here.
@@ -7,3 +9,20 @@
 // we'll have to be sure to make our promise resolve within our new "thunk based middlware"
 // the url to fetch charicters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based
+export const FETCHING = 'FETCHING';
+export const FETCHED = 'FETCHED';
+export const ERRORS = 'ERRORS'; 
+
+
+export const reducer = () => {
+    const request = axios.get('https://swapi.co/api/people/');
+    return(dispatch){
+        request.then(({response}) => {
+            dispatch({type: FETCHED, payload: response.data})
+        })
+        .catch(error => {
+            dispatch({type:ERROR, payload: error})
+        });
+    };
+};
+
