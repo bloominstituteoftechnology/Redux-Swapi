80aa72b63c439b080b06b0965d0ed53f2740b149
francistse23 <wtse.ucsd@gmail.com>
2018-08-15 12:05:08 -0700

    Francis Tse - Redux Swapi

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
-import logo from '../logo.svg';
 import '../styles/App.css';
 // pull in actions from action/index
 
