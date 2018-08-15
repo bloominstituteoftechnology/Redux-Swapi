add2dbdadcbbcffbbf0131453c4064443dced66a
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-08-15 15:31:07 -0400

    Cleanup -removed some comments -mapped fetching in app.js to load logo

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,10 +4,6 @@ export const FETCHING = 'FETCHING';
 export const FETCHED = 'FETCHED';
 export const ERROR = 'ERROR';
 
-// our action creator will be a function that returns a promise
-// we'll have to be sure to make our promise resolve within our new "thunk based middlware"
-// the url to fetch charicters from is `https://swapi.co/api/people/`
-// remember that now we have controll over our thunk-based
 export const fetchChars = () => {
   return function(dispatch) {
     dispatch({ type: FETCHING });
