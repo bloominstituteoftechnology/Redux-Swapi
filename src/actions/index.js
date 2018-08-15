f8559e570525a9b10e0312cd9c069168b5a509be
rodacoram <rodacoram8@gmail.com>
2018-08-15 15:12:11 -0600

    Redux-Swapi--Rodrigo-Acosta-Ramos
    setup almost done

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,28 @@
-// we'll need axios
+import {axios} from 'axios';
 
-// we'll need to create 3 different action types here.
-// one for fetching, one for fetched and one for errors
+// // we'll need to create 3 different action types here.
+export const REQUEST_FETCHING = 'REQUEST_FETCHING'; // this means a request is going out (requestInFlight)
+export const REQUEST_SUCCESS = 'REQUEST_SUCCESS'; //fetched
+export const REQUEST_FAILURE = 'REQUEST_FAILURE'; //errors
 
-// our action creator will be a function that returns a promise
-// we'll have to be sure to make our promise resolve within our new "thunk based middlware"
-// the url to fetch charicters from is `https://swapi.co/api/people/`
-// remember that now we have controll over our thunk-based
+// // our action creator will be a function that returns a promise
+// // we'll have to be sure to make our promise resolve within our new "thunk based middlware"
+// // the url to fetch charicters from is `https://swapi.co/api/people/`
+// // remember that now we have controll over our thunk-based
+
+const url = `https://swapi.co/api/people/`;
+
+
+export const userREQUEST = () => {
+    return function(dispatch) {
+      dispatch({ type: REQUEST_FETCHING });
+      axios.get(url)
+        .then(function(response){
+             dispatch({type: REQUEST_SUCCESS, payload: {response}})
+        })
+        .catch(function(error){
+                dispatch({type: REQUEST_FAILURE, error});
+        })
+    }
+}
+  
--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
 import logo from '../logo.svg';
 import '../styles/App.css';
-// pull in actions from action/index
+import { userREQUEST } from '../actions';
 
 class App extends Component {
   componentDidMount() {
     // call our action
   }
   render() {
+    console.log(this.props);
     return (
       <div className="App">
+      <button onClick={()=> this.userREQUEST()}>Strong with you, the Force is</button>
         {this.props.fetching ? (
           <img src={logo} className="App-logo" alt="logo" />
         ) : (
@@ -26,8 +27,14 @@ class App extends Component {
   }
 }
 
+const mapStateToProps= (state)=> {
+  return({
+    chars: state.chars,
+    isFetching: state.isFetching
+  })
+}
 // our mapDispatchToProps needs to have two properties inherited from state
 // the chars and the fetching boolean
-export default connect(null, {
+export default connect(mapStateToProps, {
   /* actions go here */
 })(App);
