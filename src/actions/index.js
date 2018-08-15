5bace493c99b81fb869eebafa0e092cb9955dad3
kamalnrf <kamalnrf@hotmail.com>
2018-08-16 01:51:06 +0530

    added axios in action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,23 @@
-// we'll need axios
+import axios from 'axios' 
 
-// we'll need to create 3 different action types here.
-// one for fetching, one for fetched and one for errors
+export const FETCHING_DATA = 'FETCHING_DATA'
+export const FETCHED_DATA = 'FETCHED_DATA'
+export const ERROR = 'ERROR'
 
 // our action creator will be a function that returns a promise
 // we'll have to be sure to make our promise resolve within our new "thunk based middlware"
 // the url to fetch charicters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based
+export const fetchCharacters = () => {
+    return dispatch => {
+        dispatch({ type: FETCHING_DATA})
+        axios.get(`https://swapi.co/api/people/`)
+            .then(res => {
+                console.log(res)
+            })
+            .catch(err => {
+                console.error(err);
+                dispatch({ type: ERROR })
+            })
+    }
+}
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,13 +1,18 @@
-import /* we need our action types here*/ '../actions';
+import { FETCHING_DATA, FETCHED_DATA } from '../actions';
 const initialState = {
-  // define a few properties here.
-  // Array chars, Boolean fetching, Boolean fetched, null error.
+  chars: [],
+  fetching: false,
+  fetched: false
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
     // Fill me in with the important reducers
     // action types should be FETCHING, FETCHED, and ERROR
     // your switch statement should handle all of these cases.
+    case FETCHING_DATA: 
+      console.log('^^^^^^^^^^^^^^FETCHING DATA^^^^^^^^^^^^^^^^')
+    case FETCHED_DATA:
+      console.log('##############FETCHED DATA#################')
     default:
       return state;
   }
