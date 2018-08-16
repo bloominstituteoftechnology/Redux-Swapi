c1dcfc6832f15d6d2852cd0c1c4fce96661a54ba
luis salazar <luissalazar@luiss-MacBook-Pro.local>
2018-08-15 21:00:55 -0700

    Major changes - Actions and Reducers
    Finished setting up my actions and reducers.

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,3 +7,22 @@
 // we'll have to be sure to make our promise resolve within our new "thunk based middlware"
 // the url to fetch charicters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based
+
+export const FETCHING = 'FETCHING';
+export const FETCHED = 'FETCHED';
+export const ERROR = 'ERROR';
+
+export const fetchingData = () => {
+    const request = axios.get('https://swapi.co/api/people');
+    return (dispatch) => {
+        request.then(({data}) => {
+            dispatch({ type: FETCHING });
+
+            if (response.error) {
+                dispatch({ type: ERROR, payload: response.error })
+            } else {
+                dispatch({ type: FETCHED, payload: data })
+            }
+        });
+    }
+}
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,13 +1,40 @@
-import /* we need our action types here*/ '../actions';
+import {
+  FETCHING,
+  FETCHED,
+  ERROR
+} from '../actions';
+
 const initialState = {
-  // define a few properties here.
-  // Array chars, Boolean fetching, Boolean fetched, null error.
+  isFetched: false,
+  isFetching: false,
+  // user: null;
+  errors: [],
+  hasErrors: false,
 };
+
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
     // Fill me in with the important reducers
     // action types should be FETCHING, FETCHED, and ERROR
     // your switch statement should handle all of these cases.
+    case FETCHING:
+      return Object.assign({}, state, {
+        isFetching: true,
+      });
+
+    case FETCHED:
+      return Object.assign({}, state, {
+        isFetching: false,
+        isFetched: true,
+      });
+
+    case ERROR:
+      return Object.assign({}, state, {
+        isFetching: false,
+        errors: [...state.errors, ...action.payload.errors],
+        hasErrors: true;
+      });
+
     default:
       return state;
   }
