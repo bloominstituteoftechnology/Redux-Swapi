cffef4db7538338305a90cbe759f3dcbb79ba7c4
MichaelBoegner <cmikeb@gmail.com>
2018-08-16 15:47:57 -0400

    Reducers built out
    Moving on to Actions

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,10 +1,27 @@
-import /* we need our action types here*/ '../actions';
+import {FETCHING, FETCHED, ERROR} from '../actions';
+
 const initialState = {
+  chars: ['Dan Abramov', 'Michael Boegner', 'Chewie', 'Luke Skywalker', 'r2d2(the DJ)'],
+  isFetching: false,
+  isFetched: false,
+  error: null
   // define a few properties here.
   // Array chars, Boolean fetching, Boolean fetched, null error.
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCHING:
+      return Object.assign({}, state, {
+        isFetching: true 
+      });
+    case FETCHED:
+      return Object.assign({}, state, {
+        isFetched: true
+      });
+    case ERROR:
+      return Object.assign({}, state, {
+        error: "Incorrect Login. Please try again." 
+      });
     // Fill me in with the important reducers
     // action types should be FETCHING, FETCHED, and ERROR
     // your switch statement should handle all of these cases.
