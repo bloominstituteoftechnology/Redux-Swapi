88a967c4afe45528d535ee20b604a895955d32b2
Charles Cortinas <crcortinas@gmail.com>
2018-08-16 09:47:52 -0500

    Get info
    Added functionality to request character data from Star Wars API

--- src/components/App.js
+++ src/components/App.js
@@ -3,31 +3,39 @@ import { connect } from 'react-redux';
 
 import logo from '../logo.svg';
 import '../styles/App.css';
+import { getChars } from '../actions';
 // pull in actions from action/index
 
 class App extends Component {
   componentDidMount() {
-    // call our action
+    console.log('props ', this.props);
+    this.props.getChars();
   }
   render() {
     return (
       <div className="App">
-        {this.props.fetching ? (
-          <img src={logo} className="App-logo" alt="logo" />
-        ) : (
-          <ul>
+        {
+          this.props.fetching ? (
+            <img src={logo} className="App-logo" alt="logo" />
+          ) : (
+            <ul>
             {this.props.chars.map(char => {
               return <li key={char.name}>{char.name}</li>;
             })}
-          </ul>
-        )}
+            </ul>
+          )
+        }
       </div>
     );
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    chars: state.chars,
+    fetching: state.fetching
+  };
+};
 // our mapDispatchToProps needs to have two properties inherited from state
 // the chars and the fetching boolean
-export default connect(null, {
-  /* actions go here */
-})(App);
+export default connect(mapStateToProps, { getChars })(App);
