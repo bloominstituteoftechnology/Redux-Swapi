b9edfdd45df2b14ae33582afdb3662c07352de37
keegn <keegan.t.burkett@gmail.com>
2018-08-16 17:45:39 -0500

    finishing up mvp

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,32 @@
 // we'll need axios
-
+import axios from "axios";
+let url = "https://swapi.co/api/people/";
 // we'll need to create 3 different action types here.
 // one for fetching, one for fetched and one for errors
+export const FETCHING = "FETCHING";
+export const FETCHED = "FETCHED";
+export const ERRORS = "ERRORS";
 
 // our action creator will be a function that returns a promise
 // we'll have to be sure to make our promise resolve within our new "thunk based middlware"
 // the url to fetch charicters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based
+export const getData = () => {
+  console.log("getData function was invoked");
+  return function(dispatch) {
+    dispatch({ type: FETCHING });
+    // axios call would go here with a .then()
+    setTimeout(() => {
+      // this would normally be wrapped in a .then
+      axios
+        .get(url)
+        .then(res => {
+          dispatch({ type: FETCHED, payload: res.data });
+        })
+        .catch(err => {
+          console.log("err");
+          dispatch({ type: ERRORS, payload: err });
+        });
+    }, 2500);
+  };
+};
--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import './index.css';
 import { App } from './components';
 import { Provider } from 'react-redux';
 // applyMiddleware from redux
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, createStore, combineReducers } from 'redux';
 // thunk from redux-thunk
 import thunk from 'redux-thunk';
 // logger from redux-logger
 import logger from 'redux-logger';
 // rootReducer from ./reducers
-import { someCoolReducer } from './reducers';
+import reducer from './reducers';
 
-const store = createStore(someCoolReducer, applyMiddleware(thunk, logger));
+const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <Provider store={store}>
