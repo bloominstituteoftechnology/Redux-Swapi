dac044139437c4bae1c601ff4ed755b8014ffaca
Arthur Bates <aabatesj@gmail.com>
2018-08-22 15:52:41 -0400

    StarWars Reducer
    imported actions
    
    will refactor state

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,10 +1,36 @@
-import /* we need our action types here*/ '../actions';
+import { FETCHING, FETCHED, ERROR } from '../actions';
 const initialState = {
   // define a few properties here.
   // Array chars, Boolean fetching, Boolean fetched, null error.
+  chars: [],
+  fetching: true,
+  fetched: true,
+  error: null
 };
+
+
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
+
+    case FETCHING:
+
+      return {
+        ...state,
+        fetching: true,
+      };
+    case FETCHED:
+
+      return {
+        ...state,
+        fetched: true,
+      };
+
+    case ERROR:
+
+      return {
+        ...state,
+        error: null,
+      };
     // Fill me in with the important reducers
     // action types should be FETCHING, FETCHED, and ERROR
     // your switch statement should handle all of these cases.
