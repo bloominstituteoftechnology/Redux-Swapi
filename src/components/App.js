286223897a63eff52bd6bdfaf5fd4219524de038
Arthur Bates <aabatesj@gmail.com>
2018-08-22 16:37:27 -0400

    App.js
    calling my action

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,31 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
-import logo from '../logo.svg';
 import '../styles/App.css';
+import { fetching, fetched, error } from '../actions';
+
 // pull in actions from action/index
 
 class App extends Component {
   componentDidMount() {
     // call our action
+    const fetchedItems = JSON.parse(localStorage.getItem('fetched'));
+    if (fetchedItems !== null) {
+      this.props.fetched(fetchedItems);
+    }
   }
+
   render() {
     return (
       <div className="App">
         {this.props.fetching ? (
           <img src={logo} className="App-logo" alt="logo" />
         ) : (
-          <ul>
-            {this.props.chars.map(char => {
-              return <li key={char.name}>{char.name}</li>;
-            })}
-          </ul>
-        )}
+            <ul>
+              {this.props.chars.map(char => {
+                return <li key={char.name}>{char.name}</li>;
+              })}
+            </ul>
+          )}
       </div>
     );
   }
@@ -28,6 +33,16 @@ class App extends Component {
 
 // our mapDispatchToProps needs to have two properties inherited from state
 // the chars and the fetching boolean
-export default connect(null, {
+
+const mapDispatchToProps = state => {
+  return {
+    chars: state.fetchedItems
+  };
+};
+
+export default connect(mapDispatchToProps, {
   /* actions go here */
+  fetching,
+  fetched,
+  error
 })(App);
