53046fbf9da27bbc7cfdbaf1877c35cf3a9eb4a5
Fred <sohnf90@yahoo.com>
2018-08-22 15:45:09 -0700

    First commit for pull request

--- src/index.js
+++ src/index.js
@@ -6,11 +6,16 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 // needed dependancies
 // applyMiddleware from redux
+import { applyMiddleware } from 'redux';
 // thunk from redux-thunk
+import thunk from 'redux-thunk';
 // logger from redux-logger
+import logger from 'redux-logger';
 // rootReducer from ./reducers
+import rootReducer from './reducers';
 
-const store = createStore(/* rootReducer */, /* applyMiddleware goes here */);
+const store = createStore(/* rootReducer */rootReducer, 
+  /* applyMiddleware goes here */applyMiddleware(logger, thunk));
 
 ReactDOM.render(
   <Provider store={store}>
