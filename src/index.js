4b8c18189c40fdb2e04db1e8ef0a5b6c676405e3
vanceleon <vanceleon44@gmail.com>
2018-08-23 17:20:52 -0400

    all dependancies are added

--- src/index.js
+++ src/index.js
@@ -10,7 +10,10 @@ import { createStore } from 'redux';
 // logger from redux-logger
 // rootReducer from ./reducers
 
-const store = createStore(/* rootReducer */, /* applyMiddleware goes here */);
+const store = createStore(
+  reducer, /* preloadedState, */
+ window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
