e2a48b891fbe40c136de2327b826a49b226b246c
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-09-19 17:08:35 -0400

    Huthman King - React-Swapi Initial Commit yarn installed imported into src index Setup reducers

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,5 +1,9 @@
-import /* we need our action types here*/ '../actions';
-const initialState = {
+import {
+  FETCH_CHARS_REQUEST,
+  FETCH_CHARS_SUCCESS,
+  FETCH_CHARS_FAILURE 
+} from  '../actions';
+ const initialState = {
   // define a few properties here.
   // Array chars, Boolean fetching, Boolean fetched, null error.
 };
@@ -8,6 +12,21 @@ export const charsReducer = (state = initialState, action) => {
     // Fill me in with the important reducers
     // action types should be FETCHING, FETCHED, and ERROR
     // your switch statement should handle all of these cases.
+    case FETCH_CHARS_SUCCESS:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+ export const isFetching = (state = false, action) => {
+  switch (action.type) {
+    case FETCH_CHARS_REQUEST:
+      return true;
+    case FETCH_CHARS_SUCCESS:
+    case FETCH_CHARS_FAILURE:
+      return false;
+
     default:
       return state;
   }
