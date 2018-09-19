4552231f01930bdd27c8c5ffa9d01251473fc212
Michael Trevino <preince560m@gmail.com>
2018-09-19 17:50:26 -0500

    finished/MT

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 // we'll need axios
+import axios from 'axios';
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for fetched and one for errors
@@ -6,4 +7,26 @@
 // our action creator will be a function that returns a promise
 // we'll have to be sure to make our promise resolve within our new "thunk based middlware"
 // the url to fetch charicters from is `https://swapi.co/api/people/`
-// remember that now we have controll over our thunk-based
+// remember that now we have control over our thunk-based
+
+
+
+export const FETCHING_CHARS = 'FETCHING_CHARS';
+export const CHAR_FETCHED_SUCCESS = 'CHAR_FETCHED_SUCCESS';
+export const CHAR_FETCH_ERRROR = 'CHAR_FETCH_ERRROR';
+
+export const fetchChars = () => {
+    return dispatch => {
+        dispatch({type: FETCHING_CHARS});
+        axios
+        .get('https://swapi.co/api/people/')
+
+        .then(response =>{
+            dispatch({type:CHAR_FETCHED_SUCCESS, payload: response.data.results});
+        })
+        .catch(err =>{
+            console.log(err);
+            dispatch({type:CHAR_FETCH_ERRROR});
+        });
+    };
+};
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,14 +1,33 @@
-import /* we need our action types here*/ '../actions';
-const initialState = {
-  // define a few properties here.
-  // Array chars, Boolean fetching, Boolean fetched, null error.
-};
+import {FETCHING_CHARS,CHAR_FETCHED_SUCCESS,CHAR_FETCH_ERRROR} from '../actions';
+
+const initialState = {chars:[], fetching: false, error: null };
+
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
-    // Fill me in with the important reducers
-    // action types should be FETCHING, FETCHED, and ERROR
-    // your switch statement should handle all of these cases.
+
+    case FETCHING_CHARS:
+    return Object.assign({}, state, {fetchingchars:true});
+
+    case CHAR_FETCHED_SUCCESS:
+    return Object.assign({}, state, {
+      chars : [...state.chars, ...action.payload],
+      fetchingchars:false
+
+    });
+
+    case CHAR_FETCH_ERRROR:
+    return Object.assign({}, state,{
+      fetchingchars:false,
+      error: 'Error Fetching Characters'
+    })
     default:
       return state;
   }
 };
+
+// define a few properties here.
+  // Array chars, Boolean fetching, Boolean fetched, null error.
+
+   // Fill me in with the important reducers
+    // action types should be FETCHING, FETCHED, and ERROR
+    // your switch statement should handle all of these cases.
