07e143c93a3c932cc74878420f59540e60c52bfd
Tristen Neu <tristenneu@Tristens-iMac.local>
2018-09-19 17:38:29 -0500

    Fetch functionality

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,13 +1,22 @@
-import /* we need our action types here*/ '../actions';
+import { FETCHING_CHARS, FETCHED_CHARS, ERROR_CHARS } from '../actions';
 const initialState = {
   // define a few properties here.
   // Array chars, Boolean fetching, Boolean fetched, null error.
+  chars: [],
+  fetching: false,
+  error: ""
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
     // Fill me in with the important reducers
     // action types should be FETCHING, FETCHED, and ERROR
     // your switch statement should handle all of these cases.
+    case FETCHING_CHARS:
+      return Object.assign({}, state, {fetching: true});
+    case FETCHED_CHARS:
+      return Object.assign({}, state, {chars: [...state.chars, ...action.payload], fetching: false});
+    case ERROR_CHARS:
+      return Object.assign({}, state, {fetching: false, error: "Error fetching characters"});
     default:
       return state;
   }
