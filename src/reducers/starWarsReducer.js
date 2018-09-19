064a3e68db992ad6f3a5264afef68949b55d4d70
mparedes003 <michelle.g.paredes@gmail.com>
2018-09-19 14:42:42 -0700

    First STab. Added dependencies, applied middleware and imported combinedReducers in index.js defined initialState and created reducers in switch statements in starWarsReducer.js

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,13 +1,29 @@
-import /* we need our action types here*/ '../actions';
+import { FETCHING_CHAR, CHAR_FETCH_SUCCESS, CHAR_FETCH_ERROR } '../actions';
 const initialState = {
   // define a few properties here.
   // Array chars, Boolean fetching, Boolean fetched, null error.
+  chars: [], 
+  fetchingChars: false,
+   error: ""
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
     // Fill me in with the important reducers
     // action types should be FETCHING, FETCHED, and ERROR
     // your switch statement should handle all of these cases.
+    case FETCHING_CHAR:
+      return Object.assign({}, state, {fecthingChars: true}); //if we are fetching simply trigger the boolean.
+    case CHAR_FETCH_SUCCESS:
+      return Object.assign({}, state, { 
+        chars: [...state.chars, ...action.payload], //if our promise was successful, build out the chars array.
+      fetchingChars: false //also, set our boolean to false, because we're no longer fetching
+      });
+    case CHAR_FETCH_ERROR:
+      return Object.assign({}, state, {
+        fetchingChars: false, //we're also no longer fetching here so set the boolean to false
+        error: "Error occured fetching Chars" // now we're getting an error back, set the error message
+      });
+  
     default:
       return state;
   }
