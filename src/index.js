9e1f0f15f38bf618caf1898ac07e45f23c725cea
Timothy Hoang <timh1203@gmail.com>
2018-09-19 12:32:40 -0400

    Add store setup in root

--- src/index.js
+++ src/index.js
@@ -1,20 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import { App } from './components';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { applyMiddleware, compose, createStore } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
+import rootReducer from './reducers'
+import { App } from './components'
+import './index.css'
+
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
 // logger from redux-logger
 // rootReducer from ./reducers
 
-const store = createStore(/* rootReducer */, /* applyMiddleware goes here */);
+const middlewares = compose(
+  applyMiddleware(thunk, logger),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+const store = createStore(
+  /* rootReducer */
+  /* applyMiddleware goes here */
+  rootReducer,
+  middlewares
+)
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
-);
+)
