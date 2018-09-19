a60494f6ea98222be2846c120b22a05e92625cc6
Benny Oseguera <oseguerabenny27@gmail.com>
2018-09-19 14:07:52 -0700

    Not quite done - Benny Oseguera

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import logo from '../logo.svg';
-import '../styles/App.css';
+import logo from "../logo.svg";
+import "../styles/App.css";
 // pull in actions from action/index
+import { fetchChars } from "../actions/index";
 
 class App extends Component {
   componentDidMount() {
     // call our action
+    this.props.fetchChars();
   }
   render() {
     return (
@@ -28,6 +30,19 @@ class App extends Component {
 
 // our mapDispatchToProps needs to have two properties inherited from state
 // the chars and the fetching boolean
-export default connect(null, {
-  /* actions go here */
-})(App);
+
+const mapStateToProps = state => {
+  return {
+    // our state machine is working for us based on fetching, success, and error. lets make sure our component knows about the state machine
+    chars: state.chars, /
+    fetchingChars: state.fetchingChars 
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  {
+    /* actions go here */
+    fetchChars
+  }
+)(App);
