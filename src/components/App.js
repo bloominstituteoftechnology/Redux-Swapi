5eecf5021c3907c5a1b3d2e49084949d1ae45799
unknown <cornielleandres@gmail.com>
2018-09-19 16:21:17 -0400

    Add PropTypes to App component.

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+// Dependencies
+import PropTypes from 'prop-types';
+
 // Actions
 import { fetchSwapi } from '../actions';
 
@@ -17,21 +20,45 @@ class App extends Component {
 	render() {
 		console.log('app props', this.props)
 		return (
-			<div className="App">
-				{this.props.fetching ? (
-					<img src={logo} className="App-logo" alt="logo" />
+			<div className = 'App'>
+				{ this.props.fetching ? (
+					<img src = { logo } className = 'App-logo' alt = 'logo' />
 				) : (
 					<ul>
-					{this.props.chars.map(char => {
-						return <li key={char.name}>{char.name}</li>;
-					})}
+						{ this.props.chars.map(char => {
+							return <li key = { char.name }>{ char.name }</li>;
+						}) }
 					</ul>
 				)}
 
-				{this.props.error !== '' ? <h4>{this.props.error}</h4> : null}
+				{ this.props.error !== '' ? <h4>{ this.props.error }</h4> : null}
 			</div>
 		);
 	}
+}
+
+App.propTypes = {
+	chars: PropTypes.arrayOf(PropTypes.shape({
+		birth_year: PropTypes.string,
+		created: PropTypes.string,
+		edited: PropTypes.string,
+		eye_color: PropTypes.string,
+		films: PropTypes.arrayOf(PropTypes.string),
+		gender: PropTypes.string,
+		hair_color: PropTypes.string,
+		height: PropTypes.string,
+		homeworld: PropTypes.string,
+		mass: PropTypes.string,
+		name: PropTypes.string,
+		skin_color: PropTypes.string,
+		species: PropTypes.arrayOf(PropTypes.string),
+		starships: PropTypes.arrayOf(PropTypes.string),
+		url: PropTypes.string,
+		vehicles: PropTypes.arrayOf(PropTypes.string)
+	})),
+	error: PropTypes.string,
+	fetchSwapi: PropTypes.func,
+	fetching: PropTypes.bool
 }
 
 const mapStateToProps = state => {
