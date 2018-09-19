e14c1fa8d2dd12589233a6744ba6a00176135c38
m-j-hacker <mjhacker@gmail.com>
2018-09-19 16:09:25 -0500

    filled out reducer cases

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -13,8 +13,17 @@ export const charsReducer = (state = initialState, action) => {
     // action types should be FETCHING, FETCHED, and ERROR
     // your switch statement should handle all of these cases.
     case FETCHING:
+    return Object.assign({}, state, { fetching: true });
     case FETCHED:
+    return Object.assign({}, state, {
+      chars: [...state.chars, ...action.payload],
+      fetching: false
+    });
     case ERROR:
+    return Object.assign({}, state, {
+      fetching: false,
+      error: "Error in fetching!!"
+    });
     default:
       return state;
   }
