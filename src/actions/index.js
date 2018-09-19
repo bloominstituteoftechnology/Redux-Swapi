cc9fde980318b75e5bca294ea1ff3c67bc8ba134
JeanetteFernandez <jeanfern555@gmail.com>
2018-09-19 17:11:07 -0500

    Refactored Redux-Swapi - 1)Added dependencies to src/index.js, 2)made changes to starWarsReducers.js, 3)created actions to be sent off to axios and to be used in reducers, 4)Added mapStateToProps to wire everything inside of components/App.js. - MVP Complete.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,28 @@
 // we'll need axios
+import axios from 'axios';
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for fetched and one for errors
+export const FETCHING_CHARS = 'FETCHING_CHARS';
+export const CHARS_FETCHED_SUCCESS = 'CHARS_FETCHED_SUCCESS';
+export const CHARS_FETCHED_ERROR = 'CHARS_FETCHED_ERROR';
+
 
 // our action creator will be a function that returns a promise
-// we'll have to be sure to make our promise resolve within our new "thunk based middlware"
-// the url to fetch charicters from is `https://swapi.co/api/people/`
+// we'll have to be sure to make our promise resolve within our new "thunk based middleware"
+// the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based
+
+export const fetchChars = () => dispatch => {
+    dispatch({ type: FETCHING_CHARS });
+    axios
+        .get(`https://swapi.co/api/people/`)
+        .then(response => {
+            console.log('AXIOS RESPONSE:', response)
+            // dispatch({type: CHARS_FETCHED_SUCCESS, payload: response.data.results})
+        })
+        .catch(error => {
+            console.log(error)
+            dispatch({type:CHARS_FETCHED_ERROR})
+        })
+}
