11748224dfacf2f138423fa57644efb30d1fcfed
Akshay Gadkari <akshay.gadkari@gmail.com>
2018-09-19 15:19:44 -0700

    runs but doesn't fetch

--- src/index.js
+++ src/index.js
@@ -6,15 +6,21 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 // needed dependancies
 // applyMiddleware from redux
+import { applyMiddleware } from 'redux';
 // thunk from redux-thunk
+import thunk from 'redux-thunk';
 // logger from redux-logger
+import logger from 'redux-logger';
 // rootReducer from ./reducers
+//import { rootReducer } from './reducers';
+import { charsReducer } from './reducers/starWarsReducer.js';
 
-const store = createStore(/* rootReducer */, /* applyMiddleware goes here */);
+const store = createStore(charsReducer, applyMiddleware(thunk, logger));
+//const store = createStore(/* rootReducer */, /* applyMiddleware goes here */);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
 );
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,14 +1,31 @@
-import /* we need our action types here*/ '../actions';
+import {FETCHING,FETCHED,ERROR} from '../actions';
+
 const initialState = {
-  // define a few properties here.
-  // Array chars, Boolean fetching, Boolean fetched, null error.
+    // define a few properties here.
+    // Array chars, Boolean fetching, Boolean fetched, null error.
+    chars: [],
+    fetchingChars: false,
+    error: ''
 };
+
 export const charsReducer = (state = initialState, action) => {
-  switch (action.type) {
-    // Fill me in with the important reducers
-    // action types should be FETCHING, FETCHED, and ERROR
-    // your switch statement should handle all of these cases.
+    switch (action.type) {
+	// Fill me in with the important reducers
+	// action types should be FETCHING, FETCHED, and ERROR
+	// your switch statement should handle all of these cases.
+    case FETCHING:
+	return Object.assign({}, state, {fetchingChars: true});
+    case FETCHED:
+	return Object.assign({}, state, {
+	    chars: [...state.chars, ...action.payload],
+	    fetchingChars: false
+	});
+    case ERROR:
+	return Object.assign({}, state, {
+	    fetchingChars: false,
+	    error: 'Error in the reducer!'
+	});
     default:
-      return state;
-  }
+	return state;
+    }
 };
