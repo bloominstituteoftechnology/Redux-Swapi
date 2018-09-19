3a0215e23a36c00c9a25e2fdc138badc2dc1823e
unknown <cornielleandres@gmail.com>
2018-09-19 15:56:47 -0400

    Set up project to successfully apply middleware.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,36 @@
+// React
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import { App } from './components';
+
+// Redux
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+
+// Dependencies
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+// Components
+import { App } from './components';
+
+// Reducer
+import rootReducer from './reducers';
+
+// Styles
+import './index.css';
+
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
 // logger from redux-logger
 // rootReducer from ./reducers
 
-const store = createStore(/* rootReducer */, /* applyMiddleware goes here */);
+// const store = createStore(/* rootReducer */, /* applyMiddleware goes here */);
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
 );
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,14 +1,29 @@
-import /* we need our action types here*/ '../actions';
+// Actions
+import { FETCHING, FETCH_SUCCESS, FETCH_ERROR } from '../actions';
+
 const initialState = {
-  // define a few properties here.
-  // Array chars, Boolean fetching, Boolean fetched, null error.
+	chars: [],
+	fetching: false,
+	error: ''
+	// define a few properties here.
+	// Array chars, Boolean fetching, Boolean fetched, null error.
 };
+
 export const charsReducer = (state = initialState, action) => {
-  switch (action.type) {
-    // Fill me in with the important reducers
-    // action types should be FETCHING, FETCHED, and ERROR
-    // your switch statement should handle all of these cases.
-    default:
-      return state;
-  }
+	switch (action.type) {
+	// Fill me in with the important reducers
+	// action types should be FETCHING, FETCHED, and ERROR
+	// your switch statement should handle all of these cases.
+	case FETCHING: {
+		return {...state, chars: [...state.chars], fetching: true};
+	}
+	case FETCH_SUCCESS: {
+		return {...state, chars: action.payload, fetching: false, error: ''};
+	}
+	case FETCH_ERROR: {
+		return {...state, chars: [...state.chars], fetching: false, error: 'Error fetching swapi people.'};
+	}
+	default:
+		return state;
+	}
 };
