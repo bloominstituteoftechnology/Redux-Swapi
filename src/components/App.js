e149fadac0ae5d106da9e4f9779046f3dbf7af64
PatrickTheCodeGuy <patrick.jthompson93@gmail.com>
2018-09-20 11:01:54 -0700

    finished mvp

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,49 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import logo from '../logo.svg';
-import '../styles/App.css';
+import logo from "../logo.svg";
+import "../styles/App.css";
 // pull in actions from action/index
+import { fetchingChar } from "../actions";
 
 class App extends Component {
-  componentDidMount() {
-    // call our action
-  }
-  render() {
-    return (
-      <div className="App">
-        {this.props.fetching ? (
-          <img src={logo} className="App-logo" alt="logo" />
-        ) : (
-          <ul>
-            {this.props.chars.map(char => {
-              return <li key={char.name}>{char.name}</li>;
-            })}
-          </ul>
-        )}
-      </div>
-    );
-  }
+	componentDidMount() {
+		// call our action
+		this.props.fetchingChar();
+	}
+	render() {
+		return (
+			<div className="App">
+				{this.props.fetching ? (
+					<img src={logo} className="App-logo" alt="logo" />
+				) : (
+					<ul>
+						{this.props.chars.map(char => {
+							return <li key={char.name}>{char.name}</li>;
+						})}
+					</ul>
+				)}
+				{this.props.error !== "" ? <h4>{this.props.error}</h4> : null}
+			</div>
+		);
+	}
 }
 
 // our mapDispatchToProps needs to have two properties inherited from state
 // the chars and the fetching boolean
-export default connect(null, {
-  /* actions go here */
-})(App);
+const mapStateToProps = state => {
+	{
+		console.log(state);
+	}
+	return {
+		chars: state.charsReducer.char,
+		fetching: state.charsReducer.fetching
+	};
+};
+
+export default connect(
+	mapStateToProps,
+	{
+		fetchingChar
+	}
+)(App);
