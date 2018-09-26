9831e9947192e84dab6c4ac499de5ba8241b03c9
A2i <abdulfatah.a.ahmad@gmx.com>
2018-09-26 17:07:28 -0400

    Add dependancies to root

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './components';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from './reducers'
-// needed dependancies
-// applyMiddleware from redux
-// thunk from redux-thunk
-// logger from redux-logger
-// rootReducer from ./reducers
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducers from './reducers'
+import logger from 'redux-logger';
 
-const store = createStore(charsReducer);
+
+
+
+
+const store = createStore(rootReducer, middlewares);
 
 ReactDOM.render(
   <Provider store={store}>
