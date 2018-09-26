cb94be393e82393fe38cd3a5e17614747346a868
IlanL75 <anatoly.leytman@gmail.com>
2018-09-26 14:05:17 -0700

    Adding index.js, actions/index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,3 +7,25 @@
 // we'll have to be sure to make our promise resolve within our new "thunk based middlware"
 // the url to fetch charicters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based
+
+import axios from 'axios';
+
+export const FETCHING = "FETCHING";
+export const FETCH_SUCCESS = "FETCH_SUCCESS";
+export const FETCH_ERROR = "FETCH_ERROR";
+
+
+export const fetchSwapi = () => {
+    return dispatch => {
+        dispatch({ type: FETCHING });
+        axios
+          .get('https://dog.ceo/api/breed/husky/images')
+          .then(response => {
+            dispatch({ type: FETCH_SUCCESS, payload: response.data.message });
+          })
+          .catch(error => {
+            console.log(error);
+            dispatch({ type: FETCH_ERROR });
+          });
+      };
+  }
