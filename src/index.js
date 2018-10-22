8ee36b57cf6b20fc625b2ee4acda64d7ac58bbc8
Lee Len Formento <leebatef@gmail.com>
2018-10-22 18:55:29 -0400

    Imported applymiddlware, logger, thunk. Installed redux thunk

--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './components';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-// needed dependancies
-// applyMiddleware from redux
-// thunk from redux-thunk
-// logger from redux-logger
-// rootReducer from ./reducers
+import { createStore, applyMiddleware } from 'redux'; //  // make async functions
+import thunk from 'redux-thunk'; // make async functions
+import rootReducer from './reducers'
+import logger from 'redux-logger'; // print out actions and states
 
-const store = createStore(/* rootReducer */, /* applyMiddleware goes here */);
+const store = createStore( rootReducer, applyMiddleware(thunk,logger));
 
 ReactDOM.render(
   <Provider store={store}>
