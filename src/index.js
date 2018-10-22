55da54e6f2f292a2ebbd1d94c1e9492a0b7aa477
Rahul Desai <rd825@georgetown.edu>
2018-10-22 18:29:55 -0400

    Index.js kept causing errors, now fixed

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import { App } from './components';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-// needed dependancies
-// applyMiddleware from redux
-// thunk from redux-thunk
-// logger from redux-logger
-// rootReducer from ./reducers
 
-const store = createStore(/* rootReducer */, /* applyMiddleware goes here */);
+// needed dependencies
+
+import {applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+// import rootReducer from './reducers'; // This line of code made my props.chars undefined in App.js
+import { charsReducer } from './reducers/starWarsReducer'
+
+const store = createStore(charsReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <Provider store={store}>
