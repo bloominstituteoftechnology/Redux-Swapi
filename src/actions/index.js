249fdaa21df9a12df6be73acd6f9ba0fbc7dae17
Lee Len Formento <leebatef@gmail.com>
2018-10-22 21:16:39 -0400

    Setting up reducers and action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,3 +7,9 @@
 // we'll have to be sure to make our promise resolve within our new "thunk based middlware"
 // the url to fetch charicters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based
+import axios from 'axios'
+
+export const FETCHING = 'FETCHING';
+export const ERROR_FETCHING = 'ERROR_FETCHING';
+export const GET = 'GET';
+
