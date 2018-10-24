c709de1541e07d0bd60e5035ae721ac53a950f85
Shawn Antonucci <shawnantonucci2018@gmail.com>
2018-10-24 12:46:41 -0700

    added reducer info

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { App } from './App';
+import App from './App';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { rootReducer } from './reducers';
+import rootReducer from './reducers';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,13 +1,29 @@
-import /* we need our action types here*/ '../actions';
+import { FETCHING_CHARACTER, CHARACTER_FETCH_SUCCESS, CHARACTER_FETCH_FAILURE } from '../actions';
+import { Object } from 'core-js';
 const initialState = {
   // define a few properties here.
   // Array characters, Boolean fetching, null error.
+  characters: [],
+  fetchingCharacters: false,
+  error: null
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+      case FETCHING_CHARACTER:
+        return Object.assign({}, state, { fetchingCharacters: true });
+      case CHARACTER_FETCH_SUCCESS:
+        return Object.assign({}, state, {
+          characters: [...state.characters, ...action.payload],
+          fetchingCharacters: false
+        });
+      case CHARACTER_FETCH_FAILURE:
+        return Object.assign({}, state, {
+          fetchingCharacters: false,
+          error: "Error fetching characters"
+        });
     default:
       return state;
   }
