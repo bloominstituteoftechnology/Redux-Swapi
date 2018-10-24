756a2201380d0e4d30e387c46e0506298ae312f5
Daniel Ferrer <danfrrr13@gmail.com>
2018-10-24 09:26:26 -0400

    added dependencies and lines of code to index.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './components';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleWare } from 'redux';
+import { combineReducers } from './reducers';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
 // logger from redux-logger
 // rootReducer from ./reducers
 
-const store = createStore(/* rootReducer */, /* applyMiddleware goes here */);
+const store = createStore(combineReducers, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <Provider store={store}>
