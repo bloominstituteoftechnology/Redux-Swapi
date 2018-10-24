f859a09a8aa30aa7fd901ac0256ded8e5dd87f2b
Unknown <zachary.arney@gmail.com>
2018-10-24 17:57:01 -0500

    Removed unnecessary semicolon, added some others

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 
 import { CharacterList } from '../components';
 // import actions
-import { fetchCharacters } from '../actions'
+import { fetchCharacters } from '../actions';
 
 class CharacterListView extends React.Component {
   componentDidMount() {
@@ -12,14 +12,14 @@ class CharacterListView extends React.Component {
   }
 
   render() {
-    console.log(this.props)
+    console.log(this.props);
     if (this.props.isFetching) {
       // return something here to indicate that you are fetching data
-      return <h3>The Force is loading...</h3>
+      return <h3>The Force is loading...</h3>;
     }
     return (
       <div className="CharactersList_wrapper">
-        <CharacterList characters={this.props.characters} />;
+        <CharacterList characters={this.props.characters} />
       </div>
     );
   }
@@ -28,7 +28,7 @@ class CharacterListView extends React.Component {
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
 const mapStateToProps = (state) => {
-  console.log('maptstatesnhd', state)
+  console.log('maptstatesnhd', state);
   return {
     characters: state.charsReducer.characters,
     isFetching: state.charsReducer.isFetching,
