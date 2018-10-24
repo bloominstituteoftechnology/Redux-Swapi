db06ccea77371b751540ec9e1435fa02aa07f7b6
Hargobind Atwal <ethaipath@gmail.com>
2018-10-24 14:45:08 -0700

    set up reducer boiler plate

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,11 @@
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
-
-// our action creator will be a function that returns a function
+export const FETCHING = 'FETCHING'
+export const SUCCESS = 'SUCCESS'
+export const FAILURE = 'FAILURE'
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+// our action creator will be a function that returns a function
+
+
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,13 +1,22 @@
-import /* we need our action types here*/ '../actions';
+import { FETCHING, SUCCESS, FAILURE } from '../actions';
 const initialState = {
   // define a few properties here.
   // Array characters, Boolean fetching, null error.
+  characters: [],
+  fetching: false,
+  error: null
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+    case FETCHING:
+
+    case SUCCESS:
+
+    case FAILURE:
+
     default:
       return state;
   }
