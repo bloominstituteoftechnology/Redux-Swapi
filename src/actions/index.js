49f9a228eed28ff93faf90e981a3b34c7e062ecb
Daniel Ferrer <danfrrr13@gmail.com>
2018-10-24 15:26:09 -0400

    updated the changes that Dustin fixed. Starting over from the beginning.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,29 +1,8 @@
 // we'll need axios
-import axios from 'axios';
 
 // we'll need to create 3 different action types here.
-// one for fetching, one for fetched and one for errors
-export const FETCHING = 'FETCHING';
-export const SUCCESS = 'SUCCESS';
-export const FAILURE = 'FAILURE';
+// one for fetching, one for success and one for failure
 
-
-// our action creator will be a function that returns a promise
-// we'll have to be sure to make our promise resolve within our new "thunk based middlware"
-// the url to fetch charicters from is `https://swapi.co/api/people/`
-// remember that now we have controll over our thunk-based
-
-export const fetchData = () => {
-    const getData = axios.get('https://swapi.co/api/people');
-    
-    return function(dispatch) {
-        dispatch({ type: FETCHING });
-        getData
-            .then(response => {
-                dispatch({ type: SUCCESS, payload: response.data.results })
-            })
-            .catch(error => {
-                dispatch({ type: FAILURE, payload: error })
-            });
-    }
-}
+// our action creator will be a function that returns a function
+// the url to fetch characters from is `https://swapi.co/api/people/`
+// remember that now we have controll over our thunk-based action creator
--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import rootReducer from './reducers';
 import './index.css';
 import { App } from './App';
 import { Provider } from 'react-redux';
-import { applyMiddleWare, createStore } from 'redux';
-
+import { createStore } from 'redux';
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
 // logger from redux-logger
 // rootReducer from ./reducers
 
-const store = createStore(rootReducer, applyMiddleWare(thunk, logger));
+const store = createStore(/* rootReducer, */ /* applyMiddleware goes here */);
 
 ReactDOM.render(
   <Provider store={store}>
