71665c15cdcbcf5487b37f1e8d155ac5569017a8
Unknown <zachary.arney@gmail.com>
2018-10-24 17:47:28 -0500

    Imported actions into CharacterListView

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -3,19 +3,19 @@ import { connect } from 'react-redux';
 
 import { CharacterList } from '../components';
 // import actions
+import { fetchCharacters } from '../actions'
 
 class CharacterListView extends React.Component {
-  constructor() {
-    super();
-  }
-
   componentDidMount() {
     // call our action
+    this.props.fetchCharacters();
   }
 
   render() {
-    if (this.props.fetching) {
+    console.log(this.props)
+    if (this.props.isFetching) {
       // return something here to indicate that you are fetching data
+      return <h3>The Force is loading...</h3>
     }
     return (
       <div className="CharactersList_wrapper">
