a4a74f4a01af8672c2f1ebd0bfb517a68c345b3f
Brandon Strellis <bstrellis@gmail.com>
2018-10-24 17:21:26 -0400

    Actions added and exported.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,25 @@
 // ACTIONS ----------------------- ACTIONS
 import axios from 'axios';
-// we'll need to create 3 different action types here.
-// one for fetching, one for success and one for failure
 
-// our action creator will be a function that returns a function
-// the url to fetch characters from is `https://swapi.co/api/people/`
-// remember that now we have controll over our thunk-based action creator
+export const fetchChars = () => dispatch => {
+    dispatch({ type: FETCHING_CHARS });
+    // send for the images. then...
+    axios
+        .get('https://swapi.co/api/people/')
+        .then(response => {
+            dispatch({
+                type: FETCHING_CHARS_SUCCESS, 
+                payload: response.data.characters
+            })
+        })
+        .catch(error => {
+            dispatch({
+                type: FETCHING_CHARS_FAILURE,
+                payload: error
+            })
+        })
+}
+
+export const FETCHING_CHARS = 'FETCHING_CHARS';
+export const FETCHING_CHARS_SUCCESS = 'FETCHING_CHARS_SUCCESS';
+export const FETCHING_CHARS_FAILURE = 'FETCHING_CHARS_FAILURE';
