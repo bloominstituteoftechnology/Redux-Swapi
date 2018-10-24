cba36de5fac288c6d9ce411d503e181b604fe782
KHollobaugh <khollobaugh@gmail.com>
2018-10-24 16:08:45 -0400

    created actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,22 @@
+import axios from 'axios';
+
+export const FETCHING_DATA = 'FETCHING_DATA';
+export const FETCHING_DATA_SUCCESS = 'FETCHING_DATA_SUCCESS';
+export const FETCHING_DATA_FAILURE = 'FETCHING_DATA_FAILURE';
+
+export const fetchData = () => dispatch => {
+    dispatch({ type: FETCHING_DATA });
+    axios
+    .get('https://swapi.co/api/people/')
+    .then(response => {
+        dispatch({ tyoe: FETCHING_DATA_SUCCESS, payload: response.data.results});
+    })
+    .catch(error => {
+        dispatch({ type: FETCHING_DATA_FAILURE, payload: error})
+    })
+}
+
+
 // we'll need axios
 
 // we'll need to create 3 different action types here.
