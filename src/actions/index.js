fb53948eaebc71a73cd9e70fc98d4a40ec953956
rytwalker <rytwalker@gmail.com>
2018-10-24 15:13:03 -0400

    Build out actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,24 @@
 // we'll need axios
+import axios from 'axios';
+
+export const FETCHING = 'FETCHING';
+export const SUCCESS = 'SUCCESS';
+export const FAILURE = 'FAILURE';
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for fetched and one for errors
-
+export const fetchPeople = () => dispatch => {
+  dispatch({ type: FETCHING });
+  axios
+    .get('https://swapi.co/api/people/')
+    .then(res => {
+      console.log(res.data);
+      dispatch({ type: SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      dispatch({ type: FAILURE, payload: err });
+    });
+};
 // our action creator will be a function that returns a promise
 // we'll have to be sure to make our promise resolve within our new "thunk based middlware"
 // the url to fetch charicters from is `https://swapi.co/api/people/`
