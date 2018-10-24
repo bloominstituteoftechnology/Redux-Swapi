d1ca6e03eb41ed21fb180514fed65a5cdf63286f
unknown <brandongrych@gmail.com>
2018-10-24 14:49:56 -0500

    building out functionality

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { App } from './components';
+import App from './App';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleWare } from 'redux';
-import { combineReducers } from './reducers';
+import { createStore, applyMiddleware, compose } from 'redux';
+import rootReducer from './reducers';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
-// needed dependancies
-// applyMiddleware from redux
-// thunk from redux-thunk
-// logger from redux-logger
-// rootReducer from ./reducers
 
-const store = createStore(combineReducers, applyMiddleware(thunk, logger));
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      })
+    : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+const store = createStore(rootReducer, enhancer);
 
 ReactDOM.render(
   <Provider store={store}>
--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { connect } from "react-redux";
+import { fetchCHARS } from "../actions/index"
 
-import { CharacterList } from "./components";
+import CharacterList from "../components/CharacterList";
 // import actions
 
 class CharacterListView extends React.Component {
@@ -10,12 +11,15 @@ class CharacterListView extends React.Component {
   }
 
   componentDidMount() {
+    this.props.fetchCHARS();
     // call our action
   }
 
   render() {
     if (this.props.fetching) {
-      // return something here to indicate that you are fetching data
+      return (
+        <h1>Please wait, fetching Characters</h1>
+      );
     }
     return (
       <div className="CharactersList_wrapper">
@@ -25,11 +29,15 @@ class CharacterListView extends React.Component {
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    chars: state.chars,
+    isFetching: state.isFetching // is our data currently being fetched???
+  };
+};
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
 export default connect(
-  null /* mapStateToProps replaces null here */,
-  {
-    /* action creators go here */
-  }
+  mapStateToProps /* mapStateToProps replaces null here */,
+  { fetchCHARS }
 )(CharacterListView);
