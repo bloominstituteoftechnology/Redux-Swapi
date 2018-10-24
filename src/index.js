23d682fe006890ff755f65d9aec370768bb5414b
Riley-Brown <riley@riley.gg>
2018-10-24 16:32:11 -0400

    Set up index imports and set up redux actions file

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import { App } from './App';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
+import charsReducer from "./reducers/index";
+import logger from "redux-logger";
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
 // logger from redux-logger
 // rootReducer from ./reducers
 
-const store = createStore(/* rootReducer, */ /* applyMiddleware goes here */);
+const store = createStore(
+  /* rootReducer, */ charsReducer,
+  applyMiddleware(thunk)
+);
+/* applyMiddleware goes here */
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
