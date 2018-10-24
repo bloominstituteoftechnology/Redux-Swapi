404f3403939fcda01c89ea69c2feb80ee8c92bb2
Brandon Strellis <bstrellis@gmail.com>
2018-10-24 16:59:48 -0400

    Imported middleware. Built initialState and charsReducer.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
-// we'll need axios
-
+// ACTIONS ----------------------- ACTIONS
+import axios from 'axios';
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
 
