7c7678c6ad611d52367e69c2248abc56a6a9d460
ryanboris <ryan.boris@gmail.com>
2018-10-24 13:31:56 -0400

    Creates actions to call data from API

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,24 @@
-// we'll need axios
+import axios from "axios";
 
-// we'll need to create 3 different action types here.
-// one for fetching, one for fetched and one for errors
+export const GET_CHARACTERS = "GET_CHARACTERS";
+export const GET_CHARACTERS_SUCCESS = "GET_CHARACTERS_SUCCESS";
+export const ERROR_GETTING_CHARACTERS = "ERROR_GETTING_CHARACTERS";
 
-// our action creator will be a function that returns a promise
-// we'll have to be sure to make our promise resolve within our new "thunk based middlware"
-// the url to fetch charicters from is `https://swapi.co/api/people/`
-// remember that now we have controll over our thunk-based
+export const getCharacters = () => {
+  const promise = axios.get("https://swapi.co/api/people");
+  return dispatch => {
+    dispatch({ type: GET_CHARACTERS });
+
+    promise
+      .then(response => {
+        dispatch({
+          type: GET_CHARACTERS_SUCCESS,
+          payload: response.data.characters
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        dispatch({ type: ERROR_GETTING_CHARACTERS });
+      });
+  };
+};
--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,7 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import logo from '../logo.svg';
-import '../styles/App.css';
+import "../styles/App.css";
 // pull in actions from action/index
 
 class App extends Component {
@@ -12,15 +11,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.props.fetching ? (
-          <img src={logo} className="App-logo" alt="logo" />
-        ) : (
-          <ul>
-            {this.props.chars.map(char => {
-              return <li key={char.name}>{char.name}</li>;
-            })}
-          </ul>
-        )}
+        {/* <ul>
+          {this.props.chars.map(char => {
+            return <li key={char.name}>{char.name}</li>;
+          })}
+        </ul>
+        )} */}
       </div>
     );
   }
@@ -28,6 +24,9 @@ class App extends Component {
 
 // our mapDispatchToProps needs to have two properties inherited from state
 // the chars and the fetching boolean
-export default connect(null, {
-  /* actions go here */
-})(App);
+export default connect(
+  null,
+  {
+    /* actions go here */
+  }
+)(App);
