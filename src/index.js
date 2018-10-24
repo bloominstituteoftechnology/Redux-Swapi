6ad8a7bb52c143f342d63434a48e39f0321c0985
Edward J <edjeong99@gmail.com>
2018-10-24 18:25:01 -0400

    implemented action, reducers, etc.  still has bug

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { App } from './App';
+import  App  from './App';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose  } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import {rootReducer} from './reducers';
+import rootReducers from './reducers';
 
 
-const store = createStore(/* rootReducer, */ /* applyMiddleware goes here */);
+const composeEnhancers = 
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ }): compose;
+const enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+
+const store = createStore(rootReducers, enhancer);
+
 
 ReactDOM.render(
   <Provider store={store}>
