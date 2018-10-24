4dba55003f846355c35ac33ee76a87b12e7374c0
JacobABrennan <jacobabrennan@gmail.com>
2018-10-24 18:52:29 -0400

    Add all functionality for MVP

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,14 +1,33 @@
 import /* we need our action types here*/ '../actions';
+import { FETCH_CHARACTERS, SUCCESS, FAILURE } from '../actions';
 const initialState = {
   // define a few properties here.
   // Array characters, Boolean fetching, null error.
+  characters: [],
+  fetching: false,
+  error: null,
 };
 export const charsReducer = (state = initialState, action) => {
+  let newState = {...state};
   switch (action.type) {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
-    default:
-      return state;
+    case FETCH_CHARACTERS: {
+      newState.fetching = true;
+      break;
+    }
+    case SUCCESS: {
+      newState.characters = action.characters;
+      newState.fetching = false;
+      newState.error = null;
+      break;
+    }
+    case FAILURE: {
+      newState.fetching = false;
+      newState.error = action.error;
+      break;
+    }
   }
+  return newState;
 };
--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -1,7 +1,9 @@
+
 import React from "react";
 import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
+import { bobaFetch } from '../actions';
 // import actions
 
 class CharacterListView extends React.Component {
@@ -11,11 +13,13 @@ class CharacterListView extends React.Component {
 
   componentDidMount() {
     // call our action
+    this.props.bobaFetch()
   }
 
   render() {
     if (this.props.fetching) {
       // return something here to indicate that you are fetching data
+      <h1>Fetching Data</h1>
     }
     return (
       <div className="CharactersList_wrapper">
@@ -27,9 +31,17 @@ class CharacterListView extends React.Component {
 
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
+function foobarBanana(state) {
+  return {
+    characters: state.charsReducer.characters,
+    fetching: state.charsReducer.fetching,
+  }
+}
+
 export default connect(
-  null /* mapStateToProps replaces null here */,
+  foobarBanana /* mapStateToProps replaces null here */,
   {
     /* action creators go here */
+    bobaFetch,
   }
 )(CharacterListView);
