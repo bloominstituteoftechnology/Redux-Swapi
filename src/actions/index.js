40170bc1169d41c78a04fd39932f77e5b8d903ec
sarahtennis <sarahtennis@Sarahs-Air.fios-router.home>
2018-10-24 16:05:58 -0400

    initial commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,3 +6,23 @@
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+
+import axios from 'axios';
+
+export const FETCHING_CHARS = 'FETCHING_CHARS';
+export const FETCHING_CHARS_SUCCESS = 'FETCHING_CHARS_SUCCESS';
+export const FETCHING_CHARS_FAILURE = 'FETCHING_CHARS_FAILURE';
+
+export const fetchChars = () => dispatch => {
+    dispatch({ type: FETCHING_CHARS });
+
+    axios
+        .get('https://swapi.co/api/people/')
+        .then(response => {
+            console.log(response);
+            dispatch({ type: FETCHING_CHARS_SUCCESS, payload: response.data.message });
+        })
+        .catch(error => {
+            dispatch({ type: FETCHING_CHARS_FAILURE, payload: error });
+        });
+};
