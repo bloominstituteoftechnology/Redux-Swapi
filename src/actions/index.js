825b40f75f147c381eb30c81d81cfbd6d8af1d4c
Dustin Myers <dustinmyers@users.noreply.github.com>
2018-10-24 11:55:46 -0600

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,8 @@
 // we'll need axios
 
 // we'll need to create 3 different action types here.
-// one for fetching, one for fetched and one for errors
+// one for fetching, one for success and one for failure
 
-// our action creator will be a function that returns a promise
-// we'll have to be sure to make our promise resolve within our new "thunk based middlware"
-// the url to fetch charicters from is `https://swapi.co/api/people/`
-// remember that now we have controll over our thunk-based
+// our action creator will be a function that returns a function
+// the url to fetch characters from is `https://swapi.co/api/people/`
+// remember that now we have controll over our thunk-based action creator
