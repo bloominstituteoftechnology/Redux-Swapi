ffc5ddeebb7557953adcf77eb2e79553c49906fa
Matthew Wright <matthewbwright@gmail.com>
2018-10-24 12:32:51 -0700

    fixes from Dustin

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,11 @@
 // we'll need axios
+import axios from 'axios';
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
+export const FETCHING = 'FETCHING';
+export const FETCHED = 'FETCHED';
+export const FETCH_ERROR = 'FETCH_ERROR';
 
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
