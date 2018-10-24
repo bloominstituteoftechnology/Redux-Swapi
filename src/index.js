8c5143400fc3e2fcc72b266161a5a57f1cb5daed
Hunter315 <hunter.mckay.smith@gmail.com>
2018-10-24 14:25:53 -0600

    Imports in index.js in src, enhancer created, starwaresreducer.js cases of FETCHING, SUCCESS, and FAILURE

--- src/index.js
+++ src/index.js
@@ -3,14 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './App';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { combineReducers } from './reducers';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
 // logger from redux-logger
 // rootReducer from ./reducers
 
-const store = createStore(/* rootReducer, */ /* applyMiddleware goes here */);
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      })
+    : compose;
+ const enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+
+
+const store = createStore(combineReducers, enhancer);
 
 ReactDOM.render(
   <Provider store={store}>
