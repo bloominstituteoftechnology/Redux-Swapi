3833033c84bfb3103e0de1be908735d7effc704f
Dustin Myers <dustinmyers@users.noreply.github.com>
2018-10-24 11:54:36 -0600

    Update starWarsReducer.js

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -6,7 +6,7 @@ const initialState = {
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
     // Fill me in with the important reducers
-    // action types should be FETCHING, FETCHED, and ERROR
+    // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
     default:
       return state;
