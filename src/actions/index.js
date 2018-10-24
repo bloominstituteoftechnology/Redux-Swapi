940654627ff3e18a2365214a0f6e8ad98ee0c1dd
Sumayyah <sumayyah@zubairs-air.attlocal.net>
2018-10-24 13:48:54 -0700

    pr

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,3 +6,5 @@
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+
+import axios from 'axios';
