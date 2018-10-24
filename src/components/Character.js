0087370a2cdc5976b3a2d9c249cecca40c6efc78
SabrinaGear <sabrinagear.ft@gmail.com>
2018-10-24 18:03:49 -0500

    initial style

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Character = props => {
-  return <li>{props.character.name}</li>;
+  return (
+    <div className="card card-body">
+      <h2>{props.character.name}</h2>
+      <h3>Height: {props.character.height}</h3>
+      <h3>Hair Color: {props.character.hair_color}</h3>
+      <h3>Skin Color: {props.character.skin_color}</h3>
+      <h3>Eye Color: {props.character.eye_color}</h3>
+      <h3>Birthday: {props.character.birth_year}</h3>
+      <h3>Gender: {props.character.gender}</h3>
+    </div>
+  );
 };
 
 export default Character;
