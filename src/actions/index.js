f39d0f572966114181451e96dacdb4a24fe11a52
adamhinckley <adamhinckley@mac.com>
2018-10-24 13:46:35 -0600

    init commit - actions setup

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,21 @@
+import axios from "axios";
+export const FETCHING_CHARACTERS = "FETCHING_CHARACTERS";
+export const FETCHING_CHARACTERS_SUCCESS = "FETCHING_CHARACTERS_SUCCESS";
+export const FETCHING_CHARACTERS_FAILURE = "FETCHING_CHARACTERS_FAILURE";
+
 // we'll need axios
+
+export const fetchCharacters = () => dispatch => {
+  dispatch({ type: FETCHING_CHARACTERS });
+  axios
+    .get("https://swapi.co/api/people/")
+    .then(res => {
+      dispatch({ type: FETCHING_CHARACTERS_SUCCESS, payload: res.data.message }); //res.data.message might need to be adjusted
+    })
+    .catch(err => {
+      dispatch({ type: FETCHING_CHARACTERS_FAILURE, payload: err });
+    });
+};
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import { App } from './App';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
 // logger from redux-logger
 // rootReducer from ./reducers
 
-const store = createStore(/* rootReducer, */ /* applyMiddleware goes here */);
+const store = createStore(/* rootReducer, */
+/* applyMiddleware goes here */);
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
