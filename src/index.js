67dc5fbeb8c9785f2dbed1aa3039cd5c77df6b02
Katie Cleary <katiecleary@airmaster.lan>
2018-10-25 15:17:33 -0400

    defined actions, connected axios, fixed errors from source file in src/index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { App } from './App';
+import  App from './App';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from "redux-thunk";
 import logger from "redux-logger";
 import charsReducer from "./reducers"
