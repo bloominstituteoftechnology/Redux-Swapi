9d28f0f7c6ff1e6350f77f8526e5784cb9af10f1
Mindysueasaurus <mymindyyeager@gmail.com>
2018-10-24 19:04:53 -0500

    index finished

--- src/index.js
+++ src/index.js
@@ -3,14 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App  from './App';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import { rootReducer } from './reducers';
+import { charsReducer } from './reducers';
 
 
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
 
-const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+      })
+    : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+const store = createStore(charsReducer, enhancer);
+
 
 ReactDOM.render(
   <Provider store={store}>
