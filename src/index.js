1555a52c5557d2587695e3c778364f2ef4580756
Katie Cleary <katiecleary@airmaster.lan>
2018-10-25 15:11:10 -0400

    imported thunk, logger, reducers, and connected in const store

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import { App } from './App';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import charsReducer from "./reducers"
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
 // logger from redux-logger
 // rootReducer from ./reducers
 
-const store = createStore(/* rootReducer, */ /* applyMiddleware goes here */);
+const store = createStore(charsReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <Provider store={store}>
