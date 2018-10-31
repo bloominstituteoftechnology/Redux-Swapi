3661ed5ee738e00009631cb28c92aeef5ae1e56b
mm2079 <vayne3252@gmail.com>
2018-10-31 18:17:04 -0400

    MVP Done

--- src/index.js
+++ src/index.js
@@ -1,24 +1,37 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import rootReducer from "./reducers";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore, compose } from 'redux';
+//Compose - This is a functional programming utility, and is included in Redux as a convenience. You might want to use it to apply several store enhancers in a row.
 // needed dependancies
-// applyMiddleware from redux
+// applyMiddleware from redux - Addded to line 6
 // thunk from redux-thunk
+import thunk from 'redux-thunk';
 // logger from redux-logger
+import logger from 'redux-logger';
 // rootReducer from ./reducers
+import rootReducer from './reducers/index';
 
+// creating a Store and adding Middleware(thunk & logger) to do asynchronous calls and log our state changes respectively.
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(thunk, logger));
 const store = createStore(
-  rootReducer
-  /* applyMiddleware goes here */
+  /* rootReducer, */
+  /* applyMiddleware goes here */ rootReducer,
+  enhancer
 );
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
