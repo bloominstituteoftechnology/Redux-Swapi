8827cefa69781e51c8d44d57447c6ec66b2005d7
Phil Hart <phil@Phils-iMac.local>
2018-10-31 18:17:26 -0600

    Project is finished

--- src/index.js
+++ src/index.js
@@ -4,16 +4,12 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
-import rootReducer from "./reducers";
-// needed dependancies
-// applyMiddleware from redux
-// thunk from redux-thunk
-// logger from redux-logger
-// rootReducer from ./reducers
 import {applyMiddleware} from "redux" ;
 import thunk from "redux-thunk";
 import logger from "redux-logger";
 import rootReducer from "./reducers";
+
+
 
 const store = createStore(
   rootReducer, applyMiddleware(thunk, logger));
