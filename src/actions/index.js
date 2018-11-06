eef0a1d92fe3605b2aa4488cefa7e28876afacac
Robin U Familara <robin.familara@cgius.com>
2018-11-05 23:21:55 -0600

    Setup action creators and reducers -Resolved promise in thunk middleware -Provision Fetching, Error Fetching and Get for fetching characters from a url -Assigned the proerties and array characters -Fill in reducers with action types that switch statement will handle

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,3 +6,7 @@
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+import axios from 'axios'
+export const FETCHING = 'FETCHING';
+export const ERROR_FETCHING = 'ERROR_FETCHING';
+export const GET = 'GET';
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,13 +1,24 @@
-import /* we need our action types here*/ "../actions";
+import { FETCHING, ERROR_FETCHING, GET } from '../actions';
 const initialState = {
   characters: []
   // Array characters, Boolean fetching, null error.
+  chars: [],
+  fetching: false,
+  fetched: false,
+  error: null
+  
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+    case FETCHING:
+    return 
+    case ERROR_FETCHING:
+    return
+    case GET:
+    return 
     default:
       return state;
   }
