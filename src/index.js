7fcbede67085affe38158561f2fa2c8ed301d21f
Ben <bennybooftw@gmail.com>
2018-11-06 12:12:40 -0500

    Setup starWarsReducer

--- src/index.js
+++ src/index.js
@@ -7,15 +7,10 @@ import { createStore, applyMiddleware } from "redux";
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import rootReducer from "./reducers";
-// needed dependancies
-// applyMiddleware from redux
-// thunk from redux-thunk
-// logger from redux-logger
-// rootReducer from ./reducers
 
 const store = createStore(
   rootReducer,
-  apple
+  applyMiddleware(thunk, logger)
 );
 
 ReactDOM.render(
