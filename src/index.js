c4b47bdf904ac29214678fa86deebea041137313
Michael Trew <michaeltrew@Michaels-iMac.attlocal.net>
2018-11-06 06:36:10 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -5,11 +5,10 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import rootReducer from "./reducers";
-// needed dependancies
-// applyMiddleware from redux
-// thunk from redux-thunk
-// logger from redux-logger
-// rootReducer from ./reducers
+import applyMiddleware from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
 
 const store = createStore(
   rootReducer
