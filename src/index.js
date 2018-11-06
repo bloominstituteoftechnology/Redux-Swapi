8e59c3dbbc374b95d066953200ddbed0d06f7250
Willie Yturralde <willieino@hotmail.com>
2018-11-06 15:14:52 -0800

    modified js files

--- src/index.js
+++ src/index.js
@@ -5,16 +5,15 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import rootReducer from "./reducers";
+import store from './store';
+
+
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
 // logger from redux-logger
 // rootReducer from ./reducers
 
-const store = createStore(
-  rootReducer
-  /* applyMiddleware goes here */
-);
 
 ReactDOM.render(
   <Provider store={store}>
