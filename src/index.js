17828b8133906b0631622e41f4d456ee68af6b1a
Kendra Williams <kmonique.williams@gmail.com>
2018-11-06 17:50:28 -0500

    initial push

--- src/index.js
+++ src/index.js
@@ -2,17 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import rootReducer from "./reducers";
+
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
 // logger from redux-logger
 // rootReducer from ./reducers
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./reducers";
 
 const store = createStore(
-  rootReducer
+  rootReducer, applyMiddleware(thunk)
   /* applyMiddleware goes here */
 );
 
