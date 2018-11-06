1a7cefa558ee355fec27a41b21a609e6ae62fffd
Robin U Familara <robin.familara@cgius.com>
2018-11-05 23:49:49 -0600

    Setup App under component -Embed the App with mapDispatchToProps -Call actions from within componentDidMount -Trigger the request

---
+++ src/components/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import logo from '../logo.svg';
+import '../styles/App.css';
+// pull in actions from action/index
+import { fetch } from '../actions';
+
+class App extends Component {
+  componentDidMount() {
+    this.props.fetch();
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.props.fetching ? (
+          <img src={logo} className="App-logo" alt="logo" />
+        ) : (
+          <ul>
+            {this.props.chars.map(char => {
+              return <li key={char.name}>{char.name}</li>;
+            })}
+          </ul>
+        )}
+      </div>
+    );
+  }
+}
+
+// our mapDispatchToProps needs to have two properties inherited from state
+// the chars and the fetching boolean
+const mapStateToProps = state => {
+  return {
+    chars: state.chars,
+    fetching: state.fetching
+  };
+};
+export default connect(mapStateToProps, 
+  { fetch })(App);
