d4f34e7acac7badf559085dc57d5d96e8584d2cb
Emmanium <pradoe.ep@gmail.com>
2018-11-06 11:01:07 -0700

    added middleware to index.js

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Redux Swapi (initial commit)
+# Redux Swapi
 
 ---
 
--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+
+import App from "./App";
 import rootReducer from "./reducers";
+
+import "./index.css";
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
 // logger from redux-logger
 // rootReducer from ./reducers
 
+const middleware = applyMiddleware(logger, thunk);
+
 const store = createStore(
-  rootReducer
+  rootReducer,
   /* applyMiddleware goes here */
+  middleware
 );
 
 ReactDOM.render(
