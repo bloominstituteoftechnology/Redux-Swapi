d72f8a082429ed03c9dfc652e4641de2d87ce8fb
Andrés Rivera <andres.rivera.toro@gmail.com>
2018-11-07 18:33:46 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,24 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import rootReducer from "./reducers";
-// needed dependancies
-// applyMiddleware from redux
-// thunk from redux-thunk
-// logger from redux-logger
-// rootReducer from ./reducers
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import App from './App';
+import rootReducer from './reducers';
 
-const store = createStore(
-  rootReducer
-  /* applyMiddleware goes here */
-);
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,13 +1,28 @@
-import /* we need our action types here*/ "../actions";
+import { FETCHING, SUCCESS, FAILURE } from '../actions';
+
 const initialState = {
-  characters: []
-  // Array characters, Boolean fetching, null error.
+  characters: [],
+  isFetching: false,
+  error: '',
 };
+
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
-    // Fill me in with the important reducers
-    // action types should be FETCHING, SUCCESS and FAILURE
-    // your switch statement should handle all of these cases.
+    case FETCHING:
+      return Object.assign({}, state, {
+        isFetching: true,
+      });
+    case SUCCESS:
+      return Object.assign({}, state, {
+        characters: action.payload,
+        isFetching: false,
+        error: '',
+      });
+    case FAILURE:
+      return Object.assign({}, state, {
+        isFetching: false,
+        error: action.payload,
+      });
     default:
       return state;
   }
