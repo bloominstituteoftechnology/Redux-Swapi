db128ea446392dae44e79f6ed1ce9cfbf3d7ea7b
Henry Moland <henrymoland@gmail.com>
2018-11-06 20:08:12 -0800

    Created Store, Actions, Reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,18 @@
+import axios from "axios";
 // we'll need axios
+export const FETCHING_FRIENDS = 'FETCHING_FRIENDS';
+export const FRIENDS_SUCCESS = 'FRIENDS_SUCCESS';
+export const FRIENDS_ERR = 'FRIENDS_ERR';
 
-// we'll need to create 3 different action types here.
-// one for fetching, one for success and one for failure
-
-// our action creator will be a function that returns a function
-// the url to fetch characters from is `https://swapi.co/api/people/`
-// remember that now we have controll over our thunk-based action creator
+export const fetchCharacters = () => dispatch => {
+    const promise = axios.get('https://swapi.co/api/people')
+    dispatch({type: FETCHING_FRIENDS})
+    promise
+    .then(({data}) => {
+        dispatch({type: FRIENDS_SUCCESS, payload: data.results})
+    })
+    .catch(err => {
+        dispatch({type: FRIENDS_ERR, payload: err})
+    })
+    
+}
