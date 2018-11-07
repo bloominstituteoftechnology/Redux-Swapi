24dae223c29cf546276842843844e1ba450ecc7b
Pascale Pierre <pascaleee@gmail.com>
2018-11-06 21:07:43 -0600

    added actions

--- src/index.js
+++ src/index.js
@@ -3,20 +3,20 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore , combineReducers, applyMiddleware} from "redux";
 import rootReducer from "./reducers";
-import charsReducer from "./reducers/starWarsReducer";
+import charsReducer from "./reducers";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
 
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
 // logger from redux-logger
 // rootReducer from ./reducers
-
-const store = createStore(
-  rootReducer
-  /* applyMiddleware goes here */
-);
+const middleware = applyMiddleware(logger, thunk);
+const reducers = combineReducers({ charsReducer, rootReducer })
+const store = createStore(reducers, middleware);
 
 ReactDOM.render(
   <Provider store={store}>
