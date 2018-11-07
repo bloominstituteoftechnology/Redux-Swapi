25ac5a6e1127448c96b04006ec19ba51eee2326e
Bryce Evans <bryceevans@Bryces-MacBook-Pro.local>
2018-11-06 20:12:15 -0700

    Data coming through

--- src/index.js
+++ src/index.js
@@ -13,10 +13,10 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import charsReducer from "./reducers";
+import rootReducer from "./reducers";
 
 const store = createStore(
-  charsReducer,
+  rootReducer,
   /* applyMiddleware goes here */
   applyMiddleware(thunk, logger)
 );
