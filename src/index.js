a3ec2be1b3e518fc94fea7fccb03a8844c109b6f
tommycollison <tommy@collison.ie>
2018-11-06 18:54:46 -0800

    adding dependencies

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ import { Provider } from "react-redux";
 import { createStore } from "redux";
 import rootReducer from "./reducers";
 // needed dependancies
-// applyMiddleware from redux
-// thunk from redux-thunk
-// logger from redux-logger
-// rootReducer from ./reducers
+
+import applyMiddleware from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import rootReducer from './reducers'
 
 const store = createStore(
   rootReducer
