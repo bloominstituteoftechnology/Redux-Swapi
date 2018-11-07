8453f51c2b55545217ee364de22c760030ad207d
Kendra Williams <kmonique.williams@gmail.com>
2018-11-06 19:35:39 -0500

    added functionality to scroll through pages

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,11 +13,11 @@ export const FAILURE = "FAILURE"
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
 
-export const fetchCharacters = () => {
+export const fetchCharacters = (page) => {
    return dispatch => {
       dispatch({type: FETCHING})
       axios
-         .get(`https://swapi.co/api/people/`)
+         .get(`https://swapi.co/api/people/?page=${page}`)
          .then(response => {
             dispatch({
                type: SUCCESS, 
--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -6,10 +6,31 @@ import { CharacterList } from "../components";
 import {FETCHING, SUCCESS, FAILURE, fetchCharacters} from "../actions"
 
 class CharacterListView extends React.Component {
-  componentDidMount() {
-    return this.props.fetchCharacters();
+  constructor(){
+    super()
+    this.state = {
+      page: 1,
+    }
   }
-
+  componentDidMount() {
+    return this.props.fetchCharacters(this.state.page);
+  }
+  prevPage = () => {
+    if(this.state.page > 1){
+      this.props.fetchCharacters(this.state.page - 1)
+      this.setState(state => ({
+        page: state.page - 1
+      }))
+    }
+  }
+  nextPage = () => {
+    if(this.state.page < 9){
+      this.props.fetchCharacters(this.state.page + 1)
+      this.setState(state => ({
+        page: state.page + 1
+      }))
+    }
+  }
   render() {
     if (this.props.fetching) {
       // return something here to indicate that you are fetching data
@@ -17,7 +38,11 @@ class CharacterListView extends React.Component {
     }
     return (
       <div className="CharactersList_wrapper">
+        <h2> StarWars Characters </h2>
+        <h3>Page {this.state.page}</h3>
         <CharacterList characters={this.props.characters} />;
+        <button onClick={() => this.prevPage()}>Previous</button>
+        <button onClick={() => this.nextPage()}>Next</button>
       </div>
     );
   }
