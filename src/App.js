c2b8f8b26b922423142cb174b5b3b63c05bce114
Louis <7.louislee@gmail.com>
2018-11-06 19:21:16 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
 import { CharacterListView } from "./views";
 import "./styles/App.css";
 
