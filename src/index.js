c366b63649e3e78d80cbd3bddf71bf6e3f24a193
shannonhealy <sahealy525@gmail.com>
2018-11-06 21:38:18 -0500

    imported and applied middleware

--- src/index.js
+++ src/index.js
@@ -7,13 +7,18 @@ import { createStore } from "redux";
 import rootReducer from "./reducers";
 // needed dependancies
 // applyMiddleware from redux
+import { applyMiddleware } from "redux";
 // thunk from redux-thunk
+import { thunk } from "redux-thunk";
 // logger from redux-logger
+import { logger } from "redux-logger";
 // rootReducer from ./reducers
+import { rootReducer } from "./reducers";
 
 const store = createStore(
-  rootReducer
+  rootReducer,
   /* applyMiddleware goes here */
+  applyMiddleware(thunk, logger)
 );
 
 ReactDOM.render(
