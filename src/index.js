d146bd003326a042c83192ba013e569fe98e91b2
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-11-06 23:45:18 -0500

    changed order of middleware

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import { applyMiddleware, createStore } from "redux";
-import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import rootReducer from "./reducers";
 // needed dependancies
 // applyMiddleware from redux
@@ -15,7 +15,7 @@ import rootReducer from "./reducers";
 
 const store = createStore(
   rootReducer,
-  applyMiddleware(logger, thunk)
+  applyMiddleware(thunk, logger)
   /* applyMiddleware goes here */
 );
 
