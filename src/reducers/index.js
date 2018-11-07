9741065f71571707725342bab15fbeae67ac8b65
cferrell2 <cferrell12182@gmail.com>
2018-11-07 00:15:57 -0500

    Create action creator that utilizes thunk to grab data from star wars api. The dispatch method is called for states of Fetching, Success, and Failure.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import { charsReducer } from './starWarsReducer';
 
-export default combineReducers({
+const rootReducer = combineReducers({
   charsReducer
 });
+
+export default rootReducer;
