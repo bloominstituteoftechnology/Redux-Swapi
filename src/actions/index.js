aadff168f7e77b307e27d9b5e4cd04a73e34be6d
Marc de la Torre <mdelatorre1314@gmail.com>
2018-11-06 22:43:47 -0800

    Completed CharacterListView.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,13 +8,14 @@ export const FAILURE = 'FAILURE';
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
-export const fetchChars = () => {
+export const fetchChars = characters => {
     return dispatch => {
         dispatch({ type: FETCHING });
         axios
             .get('https://swapi.co/api/people/')
             .then(response => {
-                dispatch({ type: SUCCESS, payload: response.data.results })
+                dispatch({ type: SUCCESS,
+                           payload: response.data.results })
             })
             .catch(err => {
                 console.log(err);
--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -2,20 +2,23 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
+import { fetchChars } from '../actions/index'
 // import actions
 
 class CharacterListView extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
   }
 
   componentDidMount() {
     // call our action
+    this.props.fetchChars();
   }
 
   render() {
     if (this.props.fetching) {
       // return something here to indicate that you are fetching data
+      return (<h3>feel the force around you...</h3>)
     }
     return (
       <div className="CharactersList_wrapper">
@@ -25,11 +28,20 @@ class CharacterListView extends React.Component {
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    characters: state.charsReducer.characters,
+    error: state.charsReducer.error,
+    fetching: state.charsReducer.fetching
+  };
+};
+
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
 export default connect(
-  null /* mapStateToProps replaces null here */,
+  mapStateToProps /* mapStateToProps replaces null here */,
   {
+    fetchChars
     /* action creators go here */
   }
 )(CharacterListView);
