8ac554d21ae5880a15fed4c08a76239947a89cbe
hraya <hraya@lssu.edu>
2018-11-06 22:36:44 -0600

    Completed most of MVP
    set up my reducers and actions
    
    have a bug so not functional yet

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,28 @@
+import axios from 'axios';
+
+export const FETCHING = 'FETCHING';
+export const SUCCESS = 'SUCCESS';
+export const FAILURE = 'FAILURE';
+
+
+export const getCharacters = () => dispatch => {
+    dispatch({type: FETCHING});
+    axios
+        .get("https://swapi.co/api/people")
+        .then(({data}) => {
+            dispatch({
+                type:SUCCESS,
+                payload:data.results
+            });
+        })
+        .catch(error => {
+            dispatch({
+                type:FAILURE,
+                payload: error
+            });
+        });
+}
+
 // we'll need axios
 
 // we'll need to create 3 different action types here.
