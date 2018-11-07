b9ee1ead2b649e8b17bc4ae9848ec662fc5dde22
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-11-06 23:47:53 -0500

    imported action creator called action creator in componentDidMount added loading message added mapStateToProps function

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -2,7 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
-// import actions
+import { fetchChars } from "../actions/index.js" // import actions
 
 class CharacterListView extends React.Component {
   constructor() {
@@ -11,11 +11,13 @@ class CharacterListView extends React.Component {
 
   componentDidMount() {
     // call our action
+    this.props.fetchChars();
   }
 
   render() {
     if (this.props.fetching) {
       // return something here to indicate that you are fetching data
+      <h2>One moment please!</h2>
     }
     return (
       <div className="CharactersList_wrapper">
@@ -27,9 +29,17 @@ class CharacterListView extends React.Component {
 
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
+const mapStateToProps = state = {
+  return {
+    characters: this.props.characters,
+    loading: this.props.loading
+  }
+}
+
 export default connect(
-  null /* mapStateToProps replaces null here */,
+  mapStateToProps /* mapStateToProps replaces null here */,
   {
     /* action creators go here */
+    fetchChars
   }
 )(CharacterListView);
