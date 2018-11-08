8f19f7530ccf1d0b461589a9730d47e3e8365592
Louis <7.louislee@gmail.com>
2018-11-07 17:58:22 -0800

    added thunk

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from "react";
-import { applyMiddleware, createStore } from 'redux';
-import thunk from 'redux-thunk';
 import { CharacterListView } from "./views";
 import "./styles/App.css";
 
