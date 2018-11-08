9474a49bb83d5b960ad2f1fb1efd7f7d93a8f1f3
paulapivat <paul.apivat@gmail.com>
2018-11-08 13:08:14 +0700

    add axios, structure to starWarsReducer, structure to action index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,12 @@
-// we'll need axios
+import axios from 'axios'; // we'll need axios
 
+export const FETCHING = "FETCHING";
+export const SUCCESS = "SUCCESS";
+export const FAILURE = "FAILURE";
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
 
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,10 +1,20 @@
-import /* we need our action types here*/ "../actions";
+import { FETCHING, SUCCESS, FAILURE } from "../actions";
+/* we need our action types here*/
+
+
 const initialState = {
-  characters: []
+  characters: [],
+  fetching: false,
+  error: '',
   // Array characters, Boolean fetching, null error.
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCHING: 
+
+    case SUCCESS:
+
+    case FAILURE: 
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
