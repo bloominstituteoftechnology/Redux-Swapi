7e926d3d097ab5194109c5f53dfe4c13924a493e
Eric Whitcomb <eric.whitcomb@gmail.com>
2018-11-08 21:19:51 -0700

    Connected up everything, MVP done

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import { charsReducer } from './starWarsReducer';
 
-export default combineReducers({
-  charsReducer
-});
+// export default combineReducers({
+//   characters: charsReducer
+// });
+
+export default charsReducer;
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,13 +1,23 @@
-import /* we need our action types here*/ "../actions";
+import { FETCHING, SUCCESS, FAILURE } from "../actions";
 const initialState = {
-  characters: []
   // Array characters, Boolean fetching, null error.
+  characters: [],
+  fetching: false,
+  error: null
 };
+
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
-    // Fill me in with the important reducers
-    // action types should be FETCHING, SUCCESS and FAILURE
-    // your switch statement should handle all of these cases.
+    case FETCHING:
+      return { ...state, fetching: true };
+    case SUCCESS:
+      return Object.assign({}, state, {
+        characters: action.payload,
+        fetching: false,
+        error: null
+      })
+    case FAILURE:
+      return { ...state, loading: false, error: action.payload }
     default:
       return state;
   }
