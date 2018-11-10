850693d69e0e3a6bfbfce131d7a4eebcbc668218
JeffJohnson <jeff.jj.9494@gmail.com>
2018-11-10 09:01:07 -0700

    i dont know why i could not git a commit or a push to go through but i hope that with this commit ive finally fixed it

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import rootReducer from "./reducers";
-// needed dependancies
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import rootReducer from './reducers';
+// needed dependencies
 // applyMiddleware from redux
 // thunk from redux-thunk
 // logger from redux-logger
 // rootReducer from ./reducers
 
-const store = createStore(
-  rootReducer
-  /* applyMiddleware goes here */
-);
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,13 +1,24 @@
-import /* we need our action types here*/ "../actions";
+import { FETCHING_CHARS, FETCHING_CHARS_SUCCESS, FETCHING_CHARS_FAILURE } from '../actions';
+
 const initialState = {
-  characters: []
+  // define a few properties here.
   // Array characters, Boolean fetching, null error.
+  characters: [],
+  isFetching: false,
+  error: null
 };
+
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+    case FETCHING_CHARS:
+      return { ...state, isFetching: true };
+    case FETCHING_CHARS_SUCCESS:
+      return { ...state, isFetching: false, characters: [...state.characters, ...action.payload] };
+    case FETCHING_CHARS_FAILURE:
+      return { ...state, isFetching: false, error: action.payload };
     default:
       return state;
   }
