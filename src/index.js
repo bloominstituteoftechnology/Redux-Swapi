5567524ad9aa6da68e5984bf612760ea9dca7b88
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2018-11-14 15:19:39 -0600

    First commit, Store and middleware created

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import "./index.css";
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
+import App from "./App";
 import rootReducer from "./reducers";
 // needed dependancies
 // applyMiddleware from redux
-// thunk from redux-thunk
-// logger from redux-logger
-// rootReducer from ./reducers
+// + thunk from redux-thunk
+// + logger from redux-logger
+// + rootReducer from ./reducers
+
+const middleWare = applyMiddleware(logger, thunk);
 
 const store = createStore(
-  rootReducer
+  rootReducer,
+  middleWare
   /* applyMiddleware goes here */
 );
 
