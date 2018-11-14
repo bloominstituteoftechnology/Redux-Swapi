38b0c0833cc3a1c1d498ce1dbd43cc3181a4e229
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2018-11-14 16:19:06 -0600

    Actions done

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 // we'll need axios
 
 // we'll need to create 3 different action types here.
@@ -6,6 +7,23 @@
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+
 export const FETCHING = "FETCHING";
 export const SUCCESS = "SUCCESS";
 export const FAILURE = "FAILURE";
+
+export const fectchCharacters = dispatch => {
+  dispatch({type: FETCHING});
+  axios
+    .get('https://swapi.co/api/people/')
+    .then(response => {
+      console.log(response.data);
+      dispatch({
+        type: SUCCESS,
+        payload: response.data
+      })
+    })
+    .catch(error => {
+      dispatch({type: FAILURE, payload: error})
+    })
+}
