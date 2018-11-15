cf416acd62772c1661b16028283e183b3142cffa
Jesse <theiseja@gmail.com>
2018-11-14 20:24:05 -0600

    MVP: Implemented axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 // we'll need axios
 
 // we'll need to create 3 different action types here.
@@ -6,3 +8,25 @@
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+
+export const FETCHING_CHARACTERS = "FETCHING_CHARACTERS";
+export const FETCHING_CHARACTERS_SUCCESS = "FETCHING_CHARACTERS_SUCCESS";
+export const FETCHING_CHARACTERS_FAILURE = "FETCHING_CHARACTERS_FAILURE";
+
+export const fetchCharacters = () => {
+    dispatchEvent({ type: FETCHING_CHARACTERS });
+    axios
+      .get("https://swapi.co/api/people")
+      .then(({ data }) => {
+        dispatchEvent({
+            type: FETCHING_CHARACTERS_SUCCESS,
+            payload: data.results
+        });
+      })
+      .catch(err => {
+          dispatchEvent({
+              type: FETCHING_CHARACTERS_FAILURE,
+              payload: err
+          });
+      });
+};
