24c8df03e6eaef89b0bcd4b45a7a16966de6c0fe
Lorenzo Evans <lorenzo.evans94@gmail.com>
2018-11-14 20:54:29 -0500

    Doing some basic set up for async redux.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import rootReducer from "./reducers";
+import { logger } from 'redux-logger';
+import { thunk } from 'redux-thunk';
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
