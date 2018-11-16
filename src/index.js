b1c9b6fd84263eb025d2935f7c331339849c6f5f
AlexOstrander <alexanderostrander23@gmail.com>
2018-11-15 23:04:37 -0500

    Finished Swapi, followed steps and added thunk then addded my reducers and character list.:

--- src/index.js
+++ src/index.js
@@ -4,20 +4,20 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
-import rootReducer from "./reducers";
-import { applyMiddleware, createStore } from 'redux';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import { rootReducer } from './reducers';
 // needed dependancies
 // applyMiddleware from redux
+import { applyMiddleware } from 'redux';
 // thunk from redux-thunk
+import thunk from 'redux-thunk'
 // logger from redux-logger
+import logger from 'redux-logger'
 // rootReducer from ./reducers
+import rootReducer from './reducers'
+
+const middleware = applyMiddleware(logger, thunk)
 
 const store = createStore(
-  rootReducer,
-  applyMiddleware(logger, thunk)
+  rootReducer, middleware
   /* applyMiddleware goes here */
 );
 
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,13 +1,23 @@
-import /* we need our action types here*/ "../actions";
+import { FETCHING, SUCCESS, FAILURE } from "../actions";
+
 const initialState = {
-  characters: []
+  characters: [],
+  fetching: false,
+  error: ''
   // Array characters, Boolean fetching, null error.
 };
+
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+    case FETCHING:
+      return {...state, fetching: true};
+    case FAILURE:
+      return {...state, fetching: false, error: action.payload};
+    case SUCCESS:
+      return {...state, fetching: false, error: '', characters: action.payload};
     default:
       return state;
   }
