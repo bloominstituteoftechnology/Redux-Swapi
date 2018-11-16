0d64ea940875595027902a9b82d3183cb9016f2c
erscriber <erscriber@gmail.com>
2018-11-16 11:41:59 -0500

    Imported thunk, logger, and applyMiddleware; created 3 action types

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,10 @@
 // we'll need axios
-
+import axios from 'axios';
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
-
+export const FETCHING_CHARS = 'FETCHING_CHARS';
+export const ERROR_FETCHING_CHARS = 'ERROR_FETCHING_CHARS';
+export const CHARS_FETCHED = 'CHARS_FETCHED';
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
