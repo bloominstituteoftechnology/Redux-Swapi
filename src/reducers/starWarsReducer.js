5bc5adee52004a652d0c4dc7345b013334f07529
Oliver Abreu <oliverabreu@Olivers-MacBook.local>
2018-11-15 23:21:11 -0500

    (MBP : STRUCTURE) - created structure for starWarsReducer,js

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,13 +1,26 @@
-import /* we need our action types here*/ "../actions";
+import { 
+  FETCHING_CHARS, 
+  ERROR_FETCHING_CHARS, 
+  CHARS_FETCHED } from '../actions';
+
 const initialState = {
-  characters: []
-  // Array characters, Boolean fetching, null error.
+  characters: [],
+  error: null,
+  fetching: false,
+  fetched: false
 };
+
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
-    // Fill me in with the important reducers
-    // action types should be FETCHING, SUCCESS and FAILURE
-    // your switch statement should handle all of these cases.
+    case FETCHING_CHARS;
+      return Object.assign({}, state, { fetching: true });
+    case ERROR_FETCHING_CHARS
+      return Object.assign({}, state, { fetching: false, error: action.payload });
+    case CHARS_FETCHED
+      return Object.assign({}, state, { 
+        fetching: false, 
+        fetched: true, 
+        characters: characers.concat(action.payload) });
     default:
       return state;
   }
