e9f0296a21588afa4a5e3c51b3dafa494cbdd39e
Oliver Abreu <oliverabreu@Olivers-MacBook.local>
2018-11-15 23:02:20 -0500

    (MVP : STRUCTURE) - finished structure for actions file

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,3 +8,22 @@ export const CHARS_FETCHED = 'CHARS_FETCHED';
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+const url = 'https://swapi.co/api/people/';
+
+export const fetchChars = () => {
+    const starWarsChars = axios.get(url);
+    return (dispatch) => {
+        dispatch({type: FETCHING_CHARS});
+        starWarsChars
+            .then(response =? {
+                console.log(response);
+                dispatch({ type: CHARS_FETCHED, payload: [] });
+            })
+            .catch(err => {
+                dispatch({
+                    type: ERROR_FETCHING_CHARS,
+                    payload: 'Error fetching characters'
+                });
+            });
+    };
+};
