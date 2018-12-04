d166c4851ed7e21650b8dedd35eb1501e0b2ebfe
Holden B <holden.bucher.92@gmail.com>
2018-12-04 13:50:04 -0600

    compiler said the constructor wasn't necessary, so BYE FELICIA

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -5,10 +5,6 @@ import { CharacterList } from "../components";
 import { fetchCharacters } from "../actions";
 
 class CharacterListView extends React.Component {
-  constructor() {
-    super();
-  }
-
   componentDidMount() {
     this.props.fetchCharacters();
   }
