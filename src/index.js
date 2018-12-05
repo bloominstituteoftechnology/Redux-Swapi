fa2874bfc7628f7868cc3d3c7776f708ed8c09de
Civoremo <omerovic_n@hotmail.com>
2018-12-05 16:43:47 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers";
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
@@ -12,7 +15,8 @@ import rootReducer from "./reducers";
 // rootReducer from ./reducers
 
 const store = createStore(
-  rootReducer
+  rootReducer,
+  applyMiddleware(thunk, logger)
   /* applyMiddleware goes here */
 );
 
