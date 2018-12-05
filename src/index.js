4624682ea7826481286172fb3e78d26ae382e393
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2018-12-05 17:19:34 -0500

    imported appropriate pkgs

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { charsReducer } from './reducers/starWarsReducer';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import rootReducer from "./reducers";
 // needed dependancies
 // applyMiddleware from redux
@@ -11,7 +14,7 @@ import rootReducer from "./reducers";
 // logger from redux-logger
 // rootReducer from ./reducers
 
-const store = createStore(
+const store = createStore(charsReducer, applyMiddleware(logger, thunk)
   rootReducer
   /* applyMiddleware goes here */
 );
