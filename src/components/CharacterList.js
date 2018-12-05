d7584e50125bde60d657f15012d4048f9914296c
steelworx <eileencuevas62@gmail.com>
2018-12-05 17:02:25 -0500

    characters now appear in CharacterList.js

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,15 +1,34 @@
 import React from "react";
+import { connect } from 'react-redux';
+import { fetchCharacters } from '../actions';
 
 import Character from "./Character";
 
-const CharacterList = props => {
-  return (
-    <ul>
-      {props.characters.map(character => {
-        return <Character key={character.name} character={character} />;
-      })}
-    </ul>
-  );
+class CharacterList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.props.fetchCharacters();
+  }
+
+  render(){
+    return (
+      <ul>
+        {this.props.characters.map(character => {
+          return <Character key={character.name} character={character} />;
+        })}
+      </ul>
+    );
+  }
 };
 
-export default CharacterList;
+function mapStateToProps(state) {
+  return {
+    characters: state.charsReducer.characters,
+    fetching: state.charsReducer.fetching,
+  }
+}
+
+export default connect(mapStateToProps, { fetchCharacters })(CharacterList);
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,14 +1,37 @@
-import /* we need our action types here*/ "../actions";
+import { FETCHING, SUCCESS, FAILURE } from "../actions";
+
 const initialState = {
-  characters: []
+  characters: [],
+  fetching: false,
+  error: null,
   // Array characters, Boolean fetching, null error.
 };
+
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+    case FETCHING:
+      return {
+        ...state,
+        fetching: true,
+      };
+    case SUCCESS:
+      return {
+        ...state,
+        characters: action.payload,
+        error: null,
+        fetching: false,
+      };
+    case FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        fetching: false,
+      };
     default:
       return state;
   }
-};
+}
+
