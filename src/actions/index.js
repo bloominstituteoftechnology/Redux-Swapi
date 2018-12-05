6abbaa7a1c27029836c49d34724b8ab3d5ad11f2
jamespagedev <jamespagedev@outlook.com>
2018-12-05 15:47:53 -0700

    refactored links

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,10 +10,15 @@ export const FETCH_CHARACTERS_FAILURE = 'FETCH_CHARACTERS_FAILURE';
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+const urlLinks = {
+  server: `https://swapi.co/api/people`,
+  home: `/`
+};
+
 export const fetchCharacters = () => dispatch => {
   dispatch({ type: FETCH_CHARACTERS_LOADING });
   axios
-    .get('https://swapi.co/api/people/')
+    .get(`${urlLinks.server}${urlLinks.home}`)
     .then(res => {
       console.log(res.data);
       dispatch({ type: FETCH_CHARACTERS_SUCCESS, payload: res.data.results });
