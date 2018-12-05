ea831c040a0848c4c02661277976423b9a3d796f
Holden B <holden.bucher.92@gmail.com>
2018-12-05 16:28:52 -0600

    idk

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,11 +12,6 @@ export const fetchCharacters = () => {
       .then(res => {
         dispatch({ type: SUCCESS, payload: res.data.results });
       })
-      .catch(err =>
-        dispatch({
-          type: ERROR,
-          payload: `We've had ${err}`
-        })
-      );
+      .catch(err => dispatch({ type: ERROR, payload: `We've had ${err}` }));
   };
 };
