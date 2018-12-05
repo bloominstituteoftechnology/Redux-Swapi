f3e9963ace0dda29bf4ff89c4ea76ca64c0ff81f
Nick O'Ferrall <nickoferrall@gmail.com>
2018-12-05 15:23:03 -0500

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -4,16 +4,21 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
-import rootReducer from "./reducers";
+// import rootReducer from "./reducers";
 // needed dependancies
 // applyMiddleware from redux
+import { applyMiddleware, createStore } from 'redux';
 // thunk from redux-thunk
+import thunk from 'redux-thunk';
 // logger from redux-logger
+import logger from 'redux-logger';
 // rootReducer from ./reducers
+import rootReducer from './reducers';
 
 const store = createStore(
   rootReducer
   /* applyMiddleware goes here */
+  applyMiddleware(thunk, logger);
 );
 
 ReactDOM.render(
