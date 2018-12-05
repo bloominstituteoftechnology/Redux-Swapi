0a450665f224065eea0ebcc0029446440d6877fc
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2018-12-05 17:52:48 -0500

    Trying to get things to display

--- src/index.js
+++ src/index.js
@@ -3,19 +3,21 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import { charsReducer } from './reducers/starWarsReducer';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import rootReducer from "./reducers";
+import { createStore } from "redux";
+
 // needed dependancies
 // applyMiddleware from redux
+import { applyMiddleware } from "redux";
 // thunk from redux-thunk
+import thunk from "redux-thunk";
 // logger from redux-logger
+import logger from "redux-logger";
 // rootReducer from ./reducers
+import combineReducers from "./reducers";
 
-const store = createStore(charsReducer, applyMiddleware(logger, thunk)
-  rootReducer
+const store = createStore(
+  combineReducers,
+  applyMiddleware(thunk, logger)
   /* applyMiddleware goes here */
 );
 
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,6 +1,8 @@
-import /* we need our action types here*/ "../actions";
+import { FETCHING, SUCCESS, FAILURE } from "../actions";
 const initialState = {
-  characters: []
+  characters: [],
+  fetching: false,
+  Err: null
   // Array characters, Boolean fetching, null error.
 };
 export const charsReducer = (state = initialState, action) => {
@@ -8,6 +10,24 @@ export const charsReducer = (state = initialState, action) => {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+    case FETCHING:
+      return {
+        ...state,
+        fetching: true
+      };
+    case SUCCESS:
+      return {
+        ...state,
+        characters: [...state.characters, ...action.payload],
+        fetching: false
+      };
+    case FAILURE:
+      return {
+        ...state,
+        fetching: false,
+        Err: action.payload
+      };
+
     default:
       return state;
   }
