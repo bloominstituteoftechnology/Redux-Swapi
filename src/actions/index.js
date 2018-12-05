c3f54217cb792a16eb35b056e6e9a337ac795812
Lidii <lidiiany92@gmail.com>
2018-12-05 17:17:44 -0500

    Displaying the char list

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,3 +6,24 @@
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+
+
+import axios from 'axios';
+
+export const FETCHING = 'FETCHING';
+export const SUCCESS = 'SUCCESS';
+export const FAILURE = 'FAILURE';
+
+
+export const fetchPeople = () => dispatch => {
+    dispatch({ type: FETCHING });
+    axios
+      .get('https://swapi.co/api/people/')
+      .then(response => {
+        dispatch({ type: SUCCESS, payload: response.data.results });
+      })
+      .catch(err => {
+        dispatch({ type: FAILURE, payload: err });
+      });
+  };
+  
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,13 +1,38 @@
-import /* we need our action types here*/ "../actions";
+import {FETCHING, SUCCESS, FAILURE} from "../actions";
+
+// Array characters, Boolean fetching, null error.
 const initialState = {
-  characters: []
-  // Array characters, Boolean fetching, null error.
+  characters: [],
+  error: null,
+  fetching: false
+  
 };
+// action types should be FETCHING, SUCCESS and FAILURE
+// your switch statement should handle all of these cases.
+
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
-    // Fill me in with the important reducers
-    // action types should be FETCHING, SUCCESS and FAILURE
-    // your switch statement should handle all of these cases.
+    case FETCHING:
+      return {
+        ...state,
+        fetching: true
+    };
+
+    case SUCCESS:
+    return {
+        ...state,
+        error: null,
+        fetching: false,
+        characters: action.payload
+    };
+
+    case FAILURE:
+    return {
+      ...state,
+      fetching: false,
+      error: action.payload
+    };
+    
     default:
       return state;
   }
