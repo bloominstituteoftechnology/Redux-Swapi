99bf29dd3121c3170d51681fdf2f531da83d39bf
Mr. Metelus <Mdmetelus@gmail.com>
2018-12-05 16:50:05 -0500

    "reducer imports, case, andinitial state inplemented."

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,14 +1,27 @@
-import /* we need our action types here*/ "../actions";
+import { FETCH_DATA_START, FETCH_DATA_GOOD, FETCH_DATA_FAIL } from "../actions";
+
+
 const initialState = {
-  characters: []
-  // Array characters, Boolean fetching, null error.
+    characters: [],
+    error: null,
+    fetching: false,
+
 };
 export const charsReducer = (state = initialState, action) => {
-  switch (action.type) {
-    // Fill me in with the important reducers
-    // action types should be FETCHING, SUCCESS and FAILURE
-    // your switch statement should handle all of these cases.
-    default:
-      return state;
-  }
+    switch (action.type) {
+        // Fill me in with the important reducers
+        // action types should be FETCHING, SUCCESS and FAILURE
+        // your switch statement should handle all of these cases.
+        case FETCH_DATA_START:
+            return {};
+        case FETCH_DATA_GOOD:
+            return {};
+        case FETCH_DATA_FAIL:
+            return {};
+
+
+
+        default:
+            return state;
+    }
 };
