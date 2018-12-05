ab65bc88d4f8cbde1a00cf7601b282a3c2a825c3
kmarker1101 <kmarker1101@icloud.com>
2018-12-05 16:48:37 -0600

    complete

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import rootReducer from './reducers';
 
-
 const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -2,34 +2,36 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
-// import actions
+import {fetchChars} from "../actions";
 
 class CharacterListView extends React.Component {
-  constructor() {
-    super();
-  }
-
   componentDidMount() {
-    // call our action
+      this.props.fetchChars();
   }
 
   render() {
-    if (this.props.fetching) {
-      // return something here to indicate that you are fetching data
+      if (this.props.isFetching) {
+          return <h3>Loading Characters...</h3>;
     }
     return (
-      <div className="CharactersList_wrapper">
-        <CharacterList characters={this.props.characters} />;
-      </div>
+        <div className="CharactersList_wrapper">
+            <CharacterList characters={this.props.characters}/>
+            {/* <h1>hi</h1> */}
+        </div>
     );
   }
 }
 
-// our mapStateToProps needs to have two properties inherited from state
-// the characters and the fetching boolean
+const mapStateToProps = state => {
+    console.log('state', state.charsReducer.characters)
+    return {
+        characters: state.charsReducer.characters,
+        isFetching: state.charsReducer.isFetching
+    }
+}
 export default connect(
-  null /* mapStateToProps replaces null here */,
-  {
-    /* action creators go here */
-  }
+    mapStateToProps
+{
+    fetchChars
+}
 )(CharacterListView);
