6f9c5bede67e964d5146f8fdcba44287c5f0b5be
Trai Lynne <trailynne@gmail.com>
2018-12-06 09:20:45 -0700

    Added loading and error messages to be rendered conditionally

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -16,13 +16,16 @@ class CharacterListView extends React.Component {
 
   render() {
     if (this.props.fetching) {
-      // return something here to indicate that you are fetching data
+      return <h1>Characters Loading</h1>
+    } else if (this.props.error){
+      return <h1>There was an error loading the characters. Please refresh & try again.</h1>
+    } else {
+      return (
+        <div className="CharactersList_wrapper">
+          <CharacterList characters={this.props.characters} />;
+        </div>
+      );
     }
-    return (
-      <div className="CharactersList_wrapper">
-        <CharacterList characters={this.props.characters} />;
-      </div>
-    );
   }
 }
 
@@ -32,7 +35,8 @@ class CharacterListView extends React.Component {
 const mapStateToProps = state => {
   return {
     fetching: state.charsReducer.fetching,
-    characters: state.charsReducer.characters
+    characters: state.charsReducer.characters,
+    error: state.charsReducer.error
   }
 }
 export default connect(
