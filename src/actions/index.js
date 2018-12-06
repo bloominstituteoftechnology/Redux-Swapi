fef3e4af6454a3e9fddf2fdb4d1a69e25a3c8807
tryingtokeepup <kl.hawaii@gmail.com>
2018-12-05 16:13:13 -0800

    yo, got MVP thanks to Jon jon! whoo

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,9 +2,9 @@
 import axios from 'axios';
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
-export const FETCH_CHARS_REQUEST = 'FETCH_CHARS_REQUEST';
-export const FETCH_CHARS_SUCCESS = 'FETCH_CHARS_SUCCESS';
-export const FETCH_CHARS_FAILURE = 'FETCH_CHARS_FAILURE';
+export const FETCH_CHAR_REQUEST = 'FETCH_CHAR_REQUEST';
+export const FETCH_CHAR_SUCCESS = 'FETCH_CHAR_SUCCESS';
+export const FETCH_CHAR_FAILURE = 'FETCH_CHAR_FAILURE';
 //why do we need to explicitly state the variables in our actions file?
 
 // our action creator will be a function that returns a function
@@ -13,12 +13,14 @@ export const FETCH_CHARS_FAILURE = 'FETCH_CHARS_FAILURE';
 
 // what is the point of an anon function that returns a function that is named? 
 export const fetch_chars = () => dispatch => {
-    dispatch({ type: FETCH_CHARS_REQUEST });
+    dispatch({ type: FETCH_CHAR_REQUEST });
+    
     axios
     .get('https://swapi.co/api/people/')
-    .then(response => {dispatch({ type: FETCH_CHARS_SUCCESS, payload: response.data.message});}
+    .then(response => {console.log(response);
+        dispatch({ type: FETCH_CHAR_SUCCESS, payload: response.data.results});}
     )
-    .catch(err => {dispatch({ type: FETCH_CHARS_FAILURE, payload: err});
+    .catch(err => {dispatch({ type: FETCH_CHAR_FAILURE, payload: err});
 });
 
 };
