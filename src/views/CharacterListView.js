c1ff039f78c6a98000ab448ca0fff5cd88024d71
kevinmarker1101 <kmarker1101@icloud.com>
2018-12-07 09:00:11 -0600

    cleanup

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -1,37 +1,38 @@
 import React from "react";
-import { connect } from "react-redux";
+import {connect} from "react-redux";
 
-import { CharacterList } from "../components";
+import {CharacterList} from "../components";
 import {fetchChars} from "../actions";
 
 class CharacterListView extends React.Component {
-  componentDidMount() {
-      this.props.fetchChars();
-  }
-
-  render() {
-      if (this.props.isFetching) {
-          return <h3>Loading Characters...</h3>;
+    componentDidMount() {
+        this.props.fetchChars();
     }
-    return (
-        <div className="CharactersList_wrapper">
-            <CharacterList characters={this.props.characters}/>
-            {/* <h1>hi</h1> */}
-        </div>
-    );
-  }
+
+
+    render() {
+        if (this.props.isFetching) {
+            return <h3>Loading Characters...</h3>;
+        }
+        return (
+            <div className="CharactersList_wrapper">
+                <CharacterList characters={this.props.characters}/>
+                {/* <h1>hi</h1> */}
+            </div>
+        );
+    }
 }
 
 const mapStateToProps = state => {
-    console.log('state', state.charsReducer.characters)
+    console.log('state', state.charsReducer.characters);
     return {
         characters: state.charsReducer.characters,
         isFetching: state.charsReducer.isFetching
     }
-}
+};
 export default connect(
     mapStateToProps,
-{
-    fetchChars
-}
+    {
+        fetchChars
+    }
 )(CharacterListView);
