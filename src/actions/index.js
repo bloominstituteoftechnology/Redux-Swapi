eb05d7a3919e9e3c880c592af504e4721c7cae1b
Jarrad Miller <jm020801@gmail.com>
2018-12-12 16:37:06 -0700

    Swapi

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,25 @@
-// we'll need axios
+import axios from "axios";
+
+export const FETCHING_CHARS = "FETCHING_CHARS";
+export const FETCHING_CHARS_SUCCESS = "FETCHING_CHARS_SUCCESS";
+export const FETCHING_CHARS_FAILURE = "FETCHING_CHARS_FAILURE";
+
+ export const getChars = () => dispatch => {
+  dispatch({ type: FETCHING_CHARS });
+  axios
+    // .get("https://cors-anywhere.herokuapp.com/https://swapi.co/api/people")
+    .get("https://swapi.co/api/people/")
+    .then(res => {
+      console.log(res);
+
+       dispatch({ type: FETCHING_CHARS_SUCCESS, payload: res.data.results });
+    })
+    .catch(err => {
+    //   dispatch({ type: FAILURE_CHARS_FAILURE, payload: err });
+    });
+};
+
+
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
