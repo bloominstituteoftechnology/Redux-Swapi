c31ca03ea0f77484c0f083c726f94182b28993f6
chrisgins <christiansgina@gmail.com>
2018-12-12 18:25:39 -0700

    lousy commits and things

--- src/index.js
+++ src/index.js
@@ -3,17 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware} from "redux";
 import rootReducer from "./reducers";
-// needed dependancies
-// applyMiddleware from redux
-// thunk from redux-thunk
-// logger from redux-logger
-// rootReducer from ./reducers
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+
+const middleware = applyMiddleware(logger, thunk);
 
 const store = createStore(
-  rootReducer
-  /* applyMiddleware goes here */
+  rootReducer,
+  middleware
 );
 
 ReactDOM.render(
