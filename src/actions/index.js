214c5a16e7f8d2318c0d401af1df7748473f4f71
David Situ <davidsitu626@gmail.com>
2018-12-12 16:04:59 -0800

    David Situ Star Wars names rendering completed

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,23 @@
 // we'll need axios
+import axios from 'axios';
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
-
+export const FETCH = 'FETCH';
+export const SUCCESS = 'SUCCESS';
+export const ERROR = 'ERROR';
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
-// remember that now we have controll over our thunk-based action creator
+// remember that now we have control over our thunk-based action creator
+export const fetchPeople = () => (dispatch) => {
+  dispatch({ type: FETCH }); //part of a way to start a fetch loading thing while waiting for load
+  axios
+    .get('https://swapi.co/api/people/')
+    .then(response => {
+      console.log(response)
+      dispatch({type: SUCCESS, payload: response.data.results});
+    })
+    .catch(err => {
+      dispatch({type: ERROR, payload: err});
+    });
+  };
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,13 +1,34 @@
-import /* we need our action types here*/ "../actions";
+import { FETCH, SUCCESS, ERROR } from '../actions';
+
 const initialState = {
-  characters: []
+  characters: [],
+  error: null,
+  fetching: false
   // Array characters, Boolean fetching, null error.
 };
-export const charsReducer = (state = initialState, action) => {
+export const charReducer = (state = initialState, action) => {
   switch (action.type) {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+    case FETCH:
+      return {
+        ...state,
+        fetching: true
+      };
+    case SUCCESS:
+      return {
+        ...state,
+        error: null,
+        fetching: false,
+        characters: action.payload
+      };
+    case ERROR:
+      return {
+        ...state,
+        fetching: false,
+        error: action.payload
+      };
     default:
       return state;
   }
