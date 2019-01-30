5115cb462191f8a7e7141bd29db9c221493d3e64
mcferrenm <mcferrenmax@gmail.com>
2019-01-30 15:45:00 -0500

    Add action variables and build out starWarsReducer switch

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,11 @@
-// we'll need axios
+import axios from "axios";
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
+
+export const FETCH_CHARS_START = "FETCH_CHARS_START";
+export const FETCH_CHARS_SUCCESS = "FETCH_CHARS_SUCCESS";
+export const FETCH_CHARS_FAILURE = "FETCH_CHARS_FAILURE";
 
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,13 +1,36 @@
-import /* we need our action types here*/ "../actions";
+import {
+  FETCH_CHARS_START,
+  FETCH_CHARS_SUCCESS,
+  FETCH_CHARS_FAILURE
+} from "../actions";
 const initialState = {
-  characters: []
+  characters: [],
   // Array characters, Boolean fetching, null error.
+  error: null,
+  isLoading: false
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+    case FETCH_CHARS_START:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case FETCH_CHARS_SUCCESS:
+      return {
+        ...state,
+        error: "",
+        isLoading: false
+      };
+    case FETCH_CHARS_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        isLoading: false
+      };
     default:
       return state;
   }
