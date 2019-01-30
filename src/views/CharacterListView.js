db81b1348686a6f2c9d524572c64143d13a77201
mcferrenm <mcferrenmax@gmail.com>
2019-01-30 17:16:57 -0500

    Add loading spinner if loading

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import Loader from "react-loader-spinner";
 
 import { CharacterList } from "../components";
 import { getChars } from "../actions";
@@ -15,7 +16,7 @@ class CharacterListView extends React.Component {
 
   render() {
     if (this.props.isLoading) {
-      // return something here to indicate that you are fetching data
+      <Loader type="Ball-Triangle" color="#00BFFF" height="90" width="60" />;
     }
     return (
       <div className="CharactersList_wrapper">
