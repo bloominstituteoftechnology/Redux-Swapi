4b972e78b5fffce62a3e1521ce5cb1145adff744
leiannel <leilouis112@gmail.com>
2019-01-30 17:02:21 -0500

    completed mapping to components

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { connect } from "react-redux";
-
+import Loader from "react-loader-spinner";
 import { CharacterList } from "../components";
 // import actions
-import { getData } from '../actions';
+import { getData } from "../actions";
 
 class CharacterListView extends React.Component {
   constructor(props) {
@@ -12,18 +12,18 @@ class CharacterListView extends React.Component {
 
   componentDidMount() {
     // call our action
-    this.props.getData()
+    this.props.getData();
   }
 
   render() {
-    
-
-    if (this.props.fetching) {
-      // return something here to indicate that you are fetching data
-    }
     return (
-      <div className="CharactersList_wrapper">
-        {/* <CharacterList characters={this.props.characters} /> */}
+      <div>
+        {this.props.fetching && (
+          <Loader type="Puff" color="#00BFFF" height="400" width="400" />
+        )}
+        <div className="CharactersList_wrapper">
+          {this.props.characters && (<CharacterList characters={this.props.characters} /> )}
+        </div>
       </div>
     );
   }
@@ -32,11 +32,11 @@ class CharacterListView extends React.Component {
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
 const mapStateToProps = state => {
-  console.log(state)
   return {
-    fetching: state.isLoading
-  }
-}
+    fetching: state.charsReducer.isLoading,
+    characters: state.charsReducer.characters.results
+  };
+};
 export default connect(
   mapStateToProps /* mapStateToProps replaces null here */,
   {
