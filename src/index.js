e58b2644c0272eea61e39ec3b297efd2d508b91c
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-02-05 20:24:18 -0500

    IMPORT APPLYMIDDLEWARE, THUNK, LOGGER, COOLREDUCER

--- src/index.js
+++ src/index.js
@@ -11,9 +11,16 @@ import rootReducer from "./reducers";
 // logger from redux-logger
 // rootReducer from ./reducers
 
+import { createStore, applyMiddleware } from "redux";
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { someCoolReducer } from './reducers';
+
 const store = createStore(
-  rootReducer
+  rootReducer,
+  someCoolReducer,
   /* applyMiddleware goes here */
+  applyMiddleware(thunk, logger);
 );
 
 ReactDOM.render(
