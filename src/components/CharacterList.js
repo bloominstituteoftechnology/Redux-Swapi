ac581ecb01e28f4cf804a2f71a309bd25b3a89c2
kingmetri <demetribethel@gmail.com>
2019-02-05 22:35:39 -0500

    Added actions to reducers w/ switch.

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {connect} from 'react-redux'//connect
+import {connect} from 'react-redux';//connect
 import {GetCharacters} from '../actions';
 import Character from "./Character";
 
