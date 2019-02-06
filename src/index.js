2c5ffc92e363eb09e16a58dfb2d245cb0d76e034
kingmetri <demetribethel@gmail.com>
2019-02-05 22:04:10 -0500

    Middleware Added.

--- src/index.js
+++ src/index.js
@@ -5,20 +5,23 @@ import App from "./App";
 import { Provider } from "react-redux";
 import rootReducer from "./reducers";
 import {applyMiddleware, createStore} from 'redux';
+import ReduxThunk from 'redux-thunk';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
 
 
 
-// needed dependancies
-// applyMiddleware from redux
+// needed dependancies good to go!
+// applyMiddleware from redux good to go!
 // thunk from redux-thunk good 2 go!
-// logger from redux-logger
-// rootReducer from ./reducers
+// logger from redux-logger good 2 go!
+// rootReducer from ./reducers good 2 go!
+
+const Middleware = applyMiddleware(ReduxThunk)(createStore);
 
 const store = createStore(
-  rootReducer
+  rootReducer, applyMiddleware(thunk, ...Middleware)
   /* applyMiddleware goes here */
 );
 
