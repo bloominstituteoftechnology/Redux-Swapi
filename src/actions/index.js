af95cfabe5c5cf7b4a98c6f3a330b735463aff44
Alando Appling <alandoappling@gmail.com>
2019-02-05 19:14:07 -0800

    Finished MVP

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,28 @@
+import axios from "axios";
+export const GET_SWAPI = "GET_SWAPI";
+export const ERROR = "ERROR";
+export const SUCCESS = "SUCCESS";
+
 // we'll need axios
+export function getSwapi() {
+  return dispatch => {
+    dispatch({ type: GET_SWAPI });
+    axios
+      .get("https://swapi.co/api/people/")
+      .then(res => {
+        dispatch({
+            type: SUCCESS,
+            payload: res.data.results
+        })
+      })
+      .catch(err => {
+        dispatch({
+          type: ERROR,
+          payload: "Characters Failed to Load"
+        });
+      });
+  };
+}
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
--- src/index.js
+++ src/index.js
@@ -1,24 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import thunk from "redux-thunk";
+import { applyMiddleware, createStore } from "redux";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
-import rootReducer from "./reducers";
-// needed dependancies
-// applyMiddleware from redux
-// thunk from redux-thunk
-// logger from redux-logger
-// rootReducer from ./reducers
+import App from './App'
+import Reducer from "./reducers";
+import "./index.css";
 
-const store = createStore(
-  rootReducer
-  /* applyMiddleware goes here */
-);
+const rootElement = document.getElementById("root");
+const store = createStore(Reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
+  rootElement
 );
