92de56723f4f0f1120a6e188fdc540941f6dfad8
iradsanchez <ira.d.sanchez@gmail.com>
2019-02-18 22:20:13 -0700

    Implement actions and reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,25 @@
-// we'll need axios
+import axios from "axios";
 
-// we'll need to create 3 different action types here.
-// one for fetching, one for success and one for failure
+export const FETCHING = "FETCHING";
+export const SUCCESS = "SUCCESS";
+export const FAILURE = "FAILURE";
 
-// our action creator will be a function that returns a function
-// the url to fetch characters from is `https://swapi.co/api/people/`
-// remember that now we have controll over our thunk-based action creator
+export function fetching() {
+  return dispatch => {
+    dispatch({ type: FETCHING });
+    axios
+      .get(`https://swapi.co/api/people/`)
+      .then(response => {
+        dispatch({
+          type: SUCCESS,
+          payload: response.data.results
+        });
+      })
+      .catch(error => {
+        dispatch({
+          type: FAILURE,
+          payload: error
+        });
+      });
+  };
+}
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,13 +1,32 @@
-import /* we need our action types here*/ "../actions";
+import { FETCHING, SUCCESS, FAILURE } from "../actions";
+
 const initialState = {
-  characters: []
-  // Array characters, Boolean fetching, null error.
+  characters: [],
+  fetching: false,
+  error: ""
 };
+
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+    case FETCHING:
+      return { ...state, fetching: true, characters: [], error: "" };
+    case SUCCESS:
+      return {
+        ...state,
+        fetching: false,
+        error: "",
+        characters: action.payload
+      };
+    case FAILURE:
+      return {
+        ...state,
+        fetching: false,
+        characters: [],
+        error: action.payload
+      };
     default:
       return state;
   }
