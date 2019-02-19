212b9041ca25313087f74b92edee6a88f2004255
iradsanchez <ira.d.sanchez@gmail.com>
2019-02-18 22:08:21 -0700

    Update Keiran on my status
    Project set up. applyMiddleware, thunk, and logger set up in index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
+import "./index.css";
+
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import rootReducer from "./reducers";
-// needed dependancies
-// applyMiddleware from redux
-// thunk from redux-thunk
-// logger from redux-logger
-// rootReducer from ./reducers
 
-const store = createStore(
-  rootReducer
-  /* applyMiddleware goes here */
-);
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+import logger from "redux-logger";
+
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <Provider store={store}>
