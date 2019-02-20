d8b0b47f8170f7a3528f8b1e4dd58edff183f061
thomasutsey <tomutsey@yahoo.com>
2019-02-19 22:00:09 -0500

    Add reducer data
    import actions
    
    write switch to handle each action and update state'

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,14 +1,24 @@
-import /* we need our action types here*/ "../actions";
+
+import {FETCH, SUCCESS, FAIL} from '../actions/index'
 const initialState = {
-  characters: []
-  // Array characters, Boolean fetching, null error.
+  characters: [],
+  fetch: false,
+  fail:""
+
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCH:
+    return Object.assign({},state,{fetch:true,characters:[],fail:""})
+    case SUCCESS:
+    return {...state,fetch:false,characters:action.payload,error:""}
+    case FAIL:
+    return {...state, fetch:false,characters:[], error: action.payload}
+    default:
+    return state;
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
-    default:
-      return state;
+    
   }
 };
