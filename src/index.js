49b50e7639bed5dbd02106ebe3f440b421a82806
thomasutsey <tomutsey@yahoo.com>
2019-02-19 20:40:45 -0500

    add dependecies to index.js
    add thunk and logger
    
    create store while implementing the apply middleware dependencies thunk and logger

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
+
 import React from "react";
 import ReactDOM from "react-dom";
+//Styling
 import "./index.css";
+// importing from app component
 import App from "./App";
+// importing redux dependencies
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+// importing the rootreducer which handles both index and starwars reducers
 import rootReducer from "./reducers";
-// needed dependancies
-// applyMiddleware from redux
-// thunk from redux-thunk
-// logger from redux-logger
-// rootReducer from ./reducers
 
+// create the store
 const store = createStore(
-  rootReducer
-  /* applyMiddleware goes here */
+  rootReducer, 
+  applyMiddleware (thunk, logger)
 );
 
+// wrap app with provider which gives redux and its dependencies access. 
 ReactDOM.render(
   <Provider store={store}>
     <App />
