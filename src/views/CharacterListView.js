c65930d5f74639918e59d2dec959d3ecab95581c
thomasutsey <tomutsey@yahoo.com>
2019-02-19 22:31:01 -0500

    pushing for blocker

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
+import {charData} from '../actions'
 // import actions
 
 class CharacterListView extends React.Component {
@@ -10,11 +11,12 @@ class CharacterListView extends React.Component {
   }
 
   componentDidMount() {
-    // call our action
+    this.props.dispatch(charData())
   }
 
   render() {
     if (this.props.fetching) {
+      return <div>Using the force...</div>
       // return something here to indicate that you are fetching data
     }
     return (
@@ -24,12 +26,16 @@ class CharacterListView extends React.Component {
     );
   }
 }
-
+const mapStateToProps = state =>{
+  return{
+    characters: state.charsReducer.characters,
+    fetch: state.charsReducer.fetch,
+    fail: state.charsReducer.error
+  }
+}
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
 export default connect(
-  null /* mapStateToProps replaces null here */,
-  {
-    /* action creators go here */
-  }
-)(CharacterListView);
+   mapStateToProps)(CharacterListView);
+ 
+
