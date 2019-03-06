da28877e206fa6c78a4ff50556d2c614ed8d8c35
Timothy Shores <timothymshores@gmail.com>
2019-03-06 17:12:08 -0600

    Imported dependencies into index.js

--- src/index.js
+++ src/index.js
@@ -7,9 +7,13 @@ import { createStore } from "redux";
 import rootReducer from "./reducers";
 // needed dependancies
 // applyMiddleware from redux
+import { applyMiddleware, createStore } from "redux";
 // thunk from redux-thunk
+import thunk from "redux-thunk";
 // logger from redux-logger
+import logger from "redux-logger";
 // rootReducer from ./reducers
+import charsReducer from "./reducers";
 
 const store = createStore(
   rootReducer
