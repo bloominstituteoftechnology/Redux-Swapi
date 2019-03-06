b1857eba0853091d6af157a21df762341442770e
Timothy Shores <timothymshores@gmail.com>
2019-03-06 17:17:33 -0600

    Add imports and initial state to starWarsReducer.js

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,7 +1,13 @@
-import /* we need our action types here*/ "../actions";
+import {
+  FETCHING_CHARACTERS,
+  FETCHING_CHARACTERS_SUCCESS,
+  FETCHING_CHARACTERS_FAILURE
+} from "../actions";
 const initialState = {
   characters: []
   // Array characters, Boolean fetching, null error.
+  fetching: false,
+  error: null
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
