0e4869562471f26e0fbcd043c5149cd376946e99
Timothy Shores <timothymshores@gmail.com>
2019-03-06 17:20:54 -0600

    Add switch statement to starWarsReducer.js

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -11,9 +11,17 @@ const initialState = {
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
-    // Fill me in with the important reducers
-    // action types should be FETCHING, SUCCESS and FAILURE
-    // your switch statement should handle all of these cases.
+    case FETCHING_CHARACTERS:
+      return { ...state, fetching: true };
+    case FETCHING_CHARACTERS_FAILURE:
+      return { ...state, fetching: false, error: action.payload };
+    case FETCHING_CHARACTERS_SUCCESS:
+      console.log(action.payload);
+      return {
+        ...state,
+        characters: [...state.characters, ...action.payload],
+        fetching: false
+      };
     default:
       return state;
   }
