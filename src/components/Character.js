266aa4107c7d455e87003b262e756adf67858cf3
Marc Thiongo <marcthuo@gmail.com>
2019-03-20 20:54:04 -0400

    styling

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,7 +1,34 @@
 import React from "react";
+import styled from 'styled-components';
+
+const CharsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 1px solid red;
+  border-radius: 5px;
+  color: doggerblue;
+`;
+
+const Title = styled.div`
+  font-size: 1em;
+`;
 
 const Character = props => {
-  return <li>{props.character.name}</li>;
+  return 
+    <CharsWrapper>
+    <li>
+      <Title>
+        {props.character.name}
+      </Title>  
+        {props.character.height}
+        {props.character.mass}
+        {props.character.gender}
+        {props.character.species}
+        {props.character.birth_year}
+        {props.character.films}
+        {props.character.starships}
+    </li>
+    </CharsWrapper>
 };
 
 export default Character;
