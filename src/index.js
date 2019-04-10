8c36c349d587e16b3d2315ffedcc1aa488c517e0
Lambda_School_Loaner_93 <lambda_school_loaner_93@Lambda-School-Loaner-93s-MacBook-Pro.local>
2019-04-10 18:48:52 -0400

    mvp met

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
-import rootReducer from "./reducers";
+import { createStore, applyMiddleware } from "redux";
+import thunk from 'redux-thunk';
+import charsReducer from "./reducers";
+import logger from 'redux-logger';
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
@@ -12,9 +14,12 @@ import rootReducer from "./reducers";
 // rootReducer from ./reducers
 
 const store = createStore(
-  rootReducer
+  charsReducer,
+  applyMiddleware(thunk,logger)
   /* applyMiddleware goes here */
+  
 );
+console.log(store)
 
 ReactDOM.render(
   <Provider store={store}>
