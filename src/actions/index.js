c23a9c0f63a1e698aa8370bb2170252e68f9583b
Dustin Snoap <afrobacon@gmail.com>
2019-04-15 19:21:50 -0400

    MVP

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,18 @@
-// we'll need axios
+import axios from 'axios'
 
-// we'll need to create 3 different action types here.
-// one for fetching, one for success and one for failure
+export const FETCH = 'FETCH'
+export const SUCCESSFUL_HUNT = 'SUCCESSFUL_HUNT'
+export const RETURNED_WITH_NOTHING = 'RETURNED_WITH_NOTHING'
 
-// our action creator will be a function that returns a function
-// the url to fetch characters from is `https://swapi.co/api/people/`
-// remember that now we have controll over our thunk-based action creator
+export const hunt = () => dispatch => {
+    console.log("made it here")
+    dispatch({type: FETCH})
+    axios
+        .get(`https://swapi.co/api/people/`)
+        .then(res => {
+            console.log(res.data.results)
+            dispatch({type: SUCCESSFUL_HUNT, payload: res.data.results})
+            }
+        )
+        .catch(err => dispatch({type: RETURNED_WITH_NOTHING, payload: err}))
+}
--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import rootReducer from "./reducers";
-// needed dependancies
-// applyMiddleware from redux
-// thunk from redux-thunk
-// logger from redux-logger
-// rootReducer from ./reducers
+//react
+import React from "react"
+import ReactDOM from "react-dom"
+//redux
+import {applyMiddleware, createStore} from "redux"
+import {Provider} from "react-redux"
+//middleware
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
+//local
+import App from "./App"
+import rootReducer from "./reducers"
+import "./index.css"
 
 const store = createStore(
-  rootReducer
-  /* applyMiddleware goes here */
-);
+  rootReducer,
+  applyMiddleware(thunk, logger)
+)
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById("root")
-);
+)
