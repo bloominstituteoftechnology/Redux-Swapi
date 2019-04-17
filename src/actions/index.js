87444f85442ef4764cb4b008f1717d9e6da612f7
kly001 <karenly1961@gmail.com>
2019-04-17 15:43:58 -0400

    starwarReducers Completed

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,12 @@
 // we'll need axios
-
+import axios from "axios";
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
 
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+export const FETCH_CHARACTER_START = "FETCH_CHARACTER_START";
+export const FETCH_CHARACTER_SUCCESS = "FETCH_CHARACTER_SUCCESS";
+export const FETCH_CHARACTER_FAILURE = "FETCH_CHARACTER_FAILURE";
+
