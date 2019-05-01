366050b91e7064fa9fb14ddfd6752fa5e9bd4e49
Sean Doyle <sdoylelambda@gmail.com>
2019-05-01 19:26:48 -0400

    last update.

--- src/components/Character.js
+++ src/components/Character.js
@@ -2,10 +2,14 @@ import React from "react";
 
 const Character = props => {
   return (
-  <li>{props.character.name}</li>
-  // <li>{props.character.height}</li>
-  // <li>{props.character.gender}</li>
-  // <li>{props.character.mass}</li>
+  <li>
+    <div>
+      <h4>{props.character.name}</h4>
+      <h4>{props.character.height}</h4>
+      <h4>{props.character.gender}</h4>
+      <h4>{props.character.mass}</h4>
+    </div>
+  </li>
   )
 };
 
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -2,13 +2,14 @@ import React from "react";
 import Character from "./Character";
 
 const CharacterList = props => {
+  console.log(props.characters)
   return (
     <ul>
-      {props.characters && props.characters.map(character => {
+      {/* {props.characters && props.characters.map(character => {
         return (
         <Character key={character.name} character={character} />
         );
-      })}
+      })} */}
     </ul>
   );
 };
