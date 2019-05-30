eabf4fc34226e9e79f55729b34de153dde218021
angel prats <angelpratz@gmail.com>
2019-05-30 10:39:54 -0400

    project started initial commit actions indexjs

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,3 +6,27 @@
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+
+import axios from 'axios'
+export const FETCHING_CHARS = 'FETCHING_CHARS';
+export const FETCHING_SUCCESS = 'FETCHING_SUCCESS';
+export const FETCHING_FAILURE = 'FETCHING_FAILURE';
+
+
+export const getChars = () => dispatch => {
+  dispatch({ type: FETCHING_CHARS });
+  axios
+    .get('https://swapi.co/api/people')
+    .then(({ data }) => {
+      dispatch({
+        type: FETCHING_SUCCESS,
+        data: data.results
+      });
+    })
+    .catch(error => {
+      dispatch({
+        type: FETCHING_FAILURE,
+        data: error
+      });
+    })
+}
