3447b95457ce2a83e7f25403fe13df5b14d878fc
Bryce-Soghigian <49734722+Bryce-Soghigian@users.noreply.github.com>
2019-06-19 16:39:04 -0500

    Update CharacterList.js

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,14 +1,15 @@
 import React from "react";
-
 import Character from "./Character";
 
 const CharacterList = props => {
   return (
-    <ul>
-      {props.characters.map(character => {
-        return <Character key={character.name} character={character} />;
-      })}
-    </ul>
+    <div>
+      <ul>
+        {props.characters.map(character => {
+          return <Character key={character.name} character={character} />;
+        })}
+      </ul>
+    </div>
   );
 };
 
