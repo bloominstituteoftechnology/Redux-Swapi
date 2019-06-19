2b0aa408db22abf6f7cca1844d2564fef597b859
shane-gooch <43436342+shane-gooch@users.noreply.github.com>
2019-06-19 14:02:14 -0700

    shanegooch

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
 import rootReducer from "./reducers";
 // needed dependancies
 // applyMiddleware from redux
@@ -11,8 +14,12 @@ import rootReducer from "./reducers";
 // logger from redux-logger
 // rootReducer from ./reducers
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+
 const store = createStore(
-  rootReducer
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk, logger))
   /* applyMiddleware goes here */
 );
 
