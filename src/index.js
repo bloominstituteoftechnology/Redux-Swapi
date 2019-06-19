fe27f4a308c3b7a5318193dd1ef112ff6bdfc339
Andrew Brudnak <brudnak@icloud.com>
2019-06-19 12:53:16 -0700

    Setting up the project 🛠
    = Ran “npm install” 🚀
    
    - Brought in required dependencies into
    index.js as the comments instructed 📄
    
    - applyMiddleware from redux
    
    - thunk from redux-thunk 🚚
    
    - logger from redux-logger
    
    - rootReducer from ./reducers

--- src/index.js
+++ src/index.js
@@ -1,24 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import rootReducer from "./reducers";
-// needed dependancies
-// applyMiddleware from redux
-// thunk from redux-thunk
-// logger from redux-logger
-// rootReducer from ./reducers
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
-const store = createStore(
-  rootReducer
-  /* applyMiddleware goes here */
-);
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
