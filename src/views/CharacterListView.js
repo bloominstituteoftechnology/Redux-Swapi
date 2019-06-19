edb1b0e263c1a27975e2ec0a2c415ff5ad8e4974
PascalUlor <pascalulor@yahoo.com>
2019-06-19 15:03:48 +0100

    fetch and render state to frontend

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -3,19 +3,23 @@ import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
 // import actions
+import { fetchData } from "../actions";
 
 class CharacterListView extends React.Component {
-  constructor() {
-    super();
-  }
+  // constructor() {
+  //   super();
+  // }
 
   componentDidMount() {
     // call our action
+    console.log("======", this.props);
+    this.props.fetchData();
   }
 
   render() {
     if (this.props.fetching) {
       // return something here to indicate that you are fetching data
+      return <div>Loading</div>
     }
     return (
       <div className="CharactersList_wrapper">
@@ -27,9 +31,15 @@ class CharacterListView extends React.Component {
 
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
+function mapStateToProps(state) {
+  return {
+    characters: state.people.characters
+  };
+}
 export default connect(
-  null /* mapStateToProps replaces null here */,
+  mapStateToProps /* mapStateToProps replaces null here */,
   {
     /* action creators go here */
+    fetchData
   }
 )(CharacterListView);
