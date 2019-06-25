2ad81befdf3d63c0282a0a80bb4b4749541e380a
BreyBatten <47111428+BreyBatten@users.noreply.github.com>
2019-06-25 13:04:47 -0400

    set up project, imported dependencies, set up store with rootReducer and applyMiddleware

--- src/index.js
+++ src/index.js
@@ -3,17 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { applyeMiddleware, createStore } from "redux";
 import rootReducer from "./reducers";
-// needed dependancies
-// applyMiddleware from redux
-// thunk from redux-thunk
-// logger from redux-logger
-// rootReducer from ./reducers
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+
+import rootReducer from "./reducers";
 
 const store = createStore(
-  rootReducer
-  /* applyMiddleware goes here */
+  rootReducer, applyMiddleware(thunk)
 );
 
 ReactDOM.render(
