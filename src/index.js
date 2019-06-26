76eec4464b183bfcb6901be32929c4eadada572f
unknown <jjamal95@gmail.com>
2019-06-25 22:17:14 -0500

    inital commit, index.js wired up, moving on to reducers

--- src/index.js
+++ src/index.js
@@ -5,14 +5,20 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import rootReducer from "./reducers";
+import {applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
 // logger from redux-logger
 // rootReducer from ./reducers
 
+const middleware = applyMiddleware(logger, thunk)
+
 const store = createStore(
-  rootReducer
+  rootReducer,middleware
   /* applyMiddleware goes here */
 );
 
