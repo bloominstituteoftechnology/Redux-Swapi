704bf27d8496c930e9eaa9655b80dbd738cb50f0
unknown <jjamal95@gmail.com>
2019-06-27 22:20:16 -0500

    finished mvp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
-
+import axios from "axios"
 import { CharacterListView } from "./views";
 import "./styles/App.css";
 
 class App extends Component {
+  componentDidMount() {
+    axios
+    .get(`https://swapi.co/api/people/`)
+    .then(response => {
+        console.log(response);
+    })
+  }
+
   render() {
     return <CharacterListView />;
   }
