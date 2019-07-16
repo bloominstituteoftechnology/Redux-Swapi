5f5d013c7078e7645db7c5b225840057c15b90bc
anthonyvigz <anthonyvigz@gmail.com>
2019-07-16 15:43:37 -0400

    imported logger

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { Provider } from "react-redux";
 import { applyMiddleware, createStore } from "redux";
 import rootReducer from "./reducers";
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 // needed dependancies
 // applyMiddleware from redux
@@ -15,7 +16,7 @@ import thunk from 'redux-thunk';
 
 const store = createStore(
   rootReducer,
-  applyMiddleware(thunk)
+  applyMiddleware(thunk, logger)
   /* applyMiddleware goes here */
 );
 
