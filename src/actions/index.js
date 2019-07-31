eadc1b17c1000b3a76e9bd6c504ad6bf25bce5ab
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-07-31 12:07:34 -0700

    Cleaning up old code from new knowledge

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,21 @@
+import axios from "axios";
+import { dispatch } from "rxjs/internal/observable/pairs";
+export const FETCH_START = "FETCH_START";
+export const FETCH_SUCCESS = "FETCH_SUCCESS";
+export const FETCH_FAILURE = "FETCH_FAILURE";
+
+export const getPeople = () => dispatch => {
+    dispatch({type: FETCH_START})
+
+    axios
+        .get("http://swapi.co/api/people")
+        .then(res => {
+            console.log(res.data)
+            dispatch({type: FETCH_SUCCESS, payload: res.data})
+        })
+        .catch(err => dispatch({type: FETCH_FAILURE, payload: err}))
+}
+
 // we'll need axios
 
 // we'll need to create 3 different action types here.
